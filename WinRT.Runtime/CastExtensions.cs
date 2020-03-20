2dc16eada3ace9f79a95917b67cc6b31b2531b31
Jeremy Koritzinsky <jekoritz@microsoft.com>
2020-03-20 10:41:11 -0700

    Add doc comment for CastExtensions.As.

--- WinRT.Runtime/CastExtensions.cs
+++ WinRT.Runtime/CastExtensions.cs
@@ -7,6 +7,18 @@ namespace WinRT
 {
     public static class CastExtensions
     {
+        /// <summary>
+        /// Cast a WinRT object to an interface type it implements in its implementation
+        /// even if it doesn't implement it in metadata.
+        /// </summary>
+        /// <typeparam name="TInterface">The interface type to cast to.</typeparam>
+        /// <param name="value">The object.</param>
+        /// <returns>
+        /// If <typeparamref name="TInterface"/> is <see cref="object"/>, returns the "default" wrapper for this WinRT object that implements all of the types that this object implements in metadata.
+        /// If <paramref name="value"/> implements <typeparamref name="TInterface"/> in metadata, casts <paramref name="value"/> to <typeparamref name="TInterface"/>.
+        /// Otherwise, creates a new wrapper of the underlying WinRT object that implements <typeparamref name="TInterface"/>.
+        /// </returns>
+        /// <exception cref="ArgumentException">Thrown if the runtime type of <paramref name="value"/> is not a projected type (if the object is a managed object).</exception>
         public static TInterface As<TInterface>(this object value)
         {
             IObjectReference GetRefForObject()
