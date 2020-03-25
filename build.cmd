19d65b83318a03dbfee2add1de5c1e9bcf84a87f
Scott Jones <sjones@microsoft.com>
2020-03-25 11:56:05 -0700

    update build.cmd to support prerelease .net 5 sdk

--- build.cmd
+++ build.cmd
@@ -2,6 +2,21 @@
 
 setlocal ENABLEDELAYEDEXPANSION
 
+rem Add per-user dotnet path to find pre-release versions
+set path=%LocalAppData%\Microsoft\dotnet;%path%
+
+rem User expected to provide global.json with allowPrerelease=true
+if not exist %~dp0global.json (
+  echo global.json not found, creating one to allowPrelease for unit test project builds
+  echo { > global.json
+  echo   "sdk": { >> global.json
+  echo     "version": "5.0.100-preview.3.20168.4", >> global.json
+  echo     "rollForward": "patch", >> global.json
+  echo     "allowPrerelease": true >> global.json
+  echo   } >> global.json
+  echo } >> global.json
+)
+
 set cswinrt_platform=%1
 set cswinrt_configuration=%2
 set cswinrt_version=%3
@@ -28,20 +43,34 @@ if /I "%cswinrt_configuration%" equ "all" (
 )
 
 if "%cswinrt_configuration%"=="" (
-  set cswinrt_configuration=Debug
+  set cswinrt_configuration=Release
 )
 
+:build
 echo Building cswinrt for %cswinrt_platform% %cswinrt_configuration%
 msbuild cswinrt.sln /p:platform=%cswinrt_platform%;configuration=%cswinrt_configuration%;GenerateTestProjection=true
 
+:test
 rem Build/Run xUnit tests, generating xml output report for Azure Devops reporting, via XunitXml.TestLogger NuGet
 echo Running cswinrt unit tests for %cswinrt_platform% %cswinrt_configuration%
-if %cswinrt_platform%==x86 (
-  set program_files="%ProgramFiles(x86)%"
-) else (
-  set program_files="%ProgramFiles%"
+set dotnet_exe="%LocalAppData%\Microsoft\dotnet\dotnet.exe"
+if not exist %dotnet_exe% (
+  if %cswinrt_platform%==x86 (
+    set dotnet_exe="%ProgramFiles(x86)%\dotnet\dotnet.exe"
+  ) else (
+    set dotnet_exe="%ProgramFiles%\dotnet\dotnet.exe"
+  )
 )
-%program_files%\dotnet\dotnet.exe test --no-build --logger xunit;LogFilePath=%~dp0test_%cswinrt_version%.xml unittest/UnitTest.csproj /nologo /m /p:platform=%cswinrt_platform%;configuration=%cswinrt_configuration%
+%dotnet_exe% test --no-build --logger xunit;LogFilePath=%~dp0test_%cswinrt_version%.xml unittest/UnitTest.csproj /nologo /m /p:platform=%cswinrt_platform%;configuration=%cswinrt_configuration%
+if ErrorLevel 1 (
+  echo.
+  echo ERROR: Unit test failed, skipping NuGet pack
+  exit /b !ErrorLevel!
+)
 
-set cswinrt_bin_dir=%~dp0_build\%cswinrt_platform%\%cswinrt_configuration%\cswinrt\bin
-nuget pack nuget/Microsoft.Windows.CsWinRT.nuspec -Properties cswinrt_exe=%cswinrt_bin_dir%\cswinrt.exe -Version %cswinrt_version% -OutputDirectory %cswinrt_bin_dir% -NonInteractive -Verbosity Detailed
+:package
+set cswinrt_bin_dir=%~dp0_build\%cswinrt_platform%\%cswinrt_configuration%\cswinrt\bin\
+set cswinrt_exe=%cswinrt_bin_dir%cswinrt.exe
+set netstandard2_runtime=%~dp0WinRT.Runtime\bin\%cswinrt_configuration%\netstandard2.0\WinRT.Runtime.dll
+set netcoreapp5_runtime=%~dp0WinRT.Runtime\bin\%cswinrt_configuration%\netcoreapp5.0\WinRT.Runtime.dll
+nuget pack nuget/Microsoft.Windows.CsWinRT.nuspec -Properties cswinrt_exe=%cswinrt_exe%;netstandard2_runtime=%netstandard2_runtime%;netcoreapp5_runtime=%netcoreapp5_runtime% -Version %cswinrt_version% -OutputDirectory %cswinrt_bin_dir% -NonInteractive -Verbosity Detailed
