0c0d9e1277ba2dc2d55210819327716c0770d469
Jeremy Koritzinsky <jekoritz@microsoft.com>
2020-04-27 14:55:28 -0700

    Provide IReference<TDelegate> for delegate types.

--- WinUI/WinUIDesktopSample/MainPage.xaml.cs
+++ WinUI/WinUIDesktopSample/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Input;
 
 namespace WinUIDesktopSample
 {
@@ -16,6 +17,12 @@ namespace WinUIDesktopSample
         public MainPage()
         {
             InitializeComponent();
+            this.AddHandler(UIElement.TappedEvent, new TappedEventHandler(Foo_PointerTapped), true /*handledEventsToo*/);
+        }
+
+        private void Foo_PointerTapped(object sender, TappedRoutedEventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
