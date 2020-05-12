2c569ef829ad8af6e0bda2ab9ea0a8ec192f7966
Scott Jones <sjones@microsoft.com>
2020-05-12 14:56:30 -0700

    dynamic generated types -> Object (#268)
    * dynamic generated types -> Object
    
    * debugging thru with Alex, it appears that dynamic types should use ExpandoObject
    
    * PR feedback
    
    * typo

--- WinRT.Runtime/TypeNameSupport.cs
+++ WinRT.Runtime/TypeNameSupport.cs
@@ -31,6 +31,12 @@ namespace WinRT
         /// <returns>A tuple containing the resolved type and the index of the end of the resolved type name.</returns>
         public static (Type type, int remaining) FindTypeByName(ReadOnlySpan<char> runtimeClassName)
         {
+            // Assume that anonymous types are expando objects, whether declared 'dynamic' or not.
+            // It may be necessary to detect otherwise and return System.Object.
+            if(runtimeClassName.StartsWith("<>f__AnonymousType".AsSpan()))
+            {
+                return (typeof(System.Dynamic.ExpandoObject), 0);
+            }
             var (genericTypeName, genericTypes, remaining) = ParseGenericTypeName(runtimeClassName);
             return (FindTypeByNameCore(genericTypeName, genericTypes), remaining);
         }
