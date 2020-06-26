15ed71a1c70154c1d195577e8808fb3335b3b1a2
Manodasan Wignarajah <mawign@microsoft.com>
2020-06-26 08:06:27 -0700

    Adding BenchmarkDotNet for benchmarking and adding bencmarking for cswinrt projection on .netcore 2.0, 3.1, 5 and benchmarking for winmd projection on net core 3.1.

---
+++ Benchmarks/Program.cs
@@ -0,0 +1,96 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Toolchains.CsProj;
+using BenchmarkDotNet.Toolchains.DotNetCli;
+using BenchmarkDotNet.Toolchains;
+using BenchmarkDotNet.Loggers;
+using BenchmarkDotNet.Characteristics;
+using System.IO;
+
+namespace Benchmarks
+{
+    public class Program
+    {
+        static void Main(string[] args) => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, new CustomConfig().Config);
+
+        private class CustomConfig : Attribute, IConfigSource
+        {
+            public IConfig Config { get; } = DefaultConfig.Instance;
+
+            public CustomConfig()
+            {
+                // Test CsWinRT projection
+                var job = Job.Default
+                    .WithPlatform(BenchmarkDotNet.Environments.Platform.X64)
+                    .WithArguments(
+                        new Argument[] {
+                            new MsBuildArgument("/p:platform=x64"),
+                            new MsBuildArgument("/p:MsAppxPackageTargets=%temp%\\EmptyMsAppxPackage.Targets")
+                        }
+                    ).AsDefault();
+
+                // Test WinMD support
+#if NETCOREAPP3_1
+                // BenchmarkDotNet will rebuild the project with a project reference when this exe is ran and
+                // in the same folder as it we have the application manifest binplaced which we want to embed in the new exe.
+                string manifestFile = Path.Combine(
+                    Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location),
+                    "Benchmarks.manifest");
+
+                var winmdJob = Job.Default
+                    .WithPlatform(BenchmarkDotNet.Environments.Platform.X64)
+                    .WithToolchain(new NetCore3ToolChainWithNativeExecution())
+                    .WithArguments(
+                        new Argument[] {
+                            new MsBuildArgument("/p:platform=x64"),
+                            new MsBuildArgument("/p:ApplicationManifest=" + manifestFile),
+                            new MsBuildArgument("/p:BenchmarkWinmdSupport=true")
+                        }
+                    )
+                    .WithId("WinMD NetCoreApp31");
+
+                // Optimizer needs to be diabled as it errors on WinMDs
+                Config = Config.WithOption(ConfigOptions.DisableOptimizationsValidator, true)
+                        .AddJob(winmdJob);
+#else
+                Config = Config.AddJob(job);
+#endif
+            }
+        }
+
+        // Custom tool chain for building the benchmark with WinMDs as we need to execute the
+        // exe version of the benchmark rather than the dll version which runs under dotnet cli.
+        // This is becuase we need to be able to embed a side by side manifest for reg free winrt
+        // and to enable COM to find the WinMDs.
+        private class NetCore3ToolChainWithNativeExecution : Toolchain
+        {
+            public NetCore3ToolChainWithNativeExecution()
+                : base("netcoreapp3.1-native",
+                      new CsProjGeneratorWithNativeExe(NetCoreAppSettings.NetCoreApp31),
+                      CsProjCoreToolchain.NetCoreApp31.Builder,
+                      new Executor())
+            {
+            }
+
+            public override bool IsSupported(BenchmarkCase benchmarkCase, ILogger logger, IResolver resolver)
+            {
+                return CsProjCoreToolchain.NetCoreApp31.IsSupported(benchmarkCase, logger, resolver);
+            }
+        }
+
+        private class CsProjGeneratorWithNativeExe : CsProjGenerator
+        {
+            public CsProjGeneratorWithNativeExe(NetCoreAppSettings settings)
+                :base(settings.TargetFrameworkMoniker, settings.CustomDotNetCliPath, settings.PackagesPath, settings.RuntimeFrameworkVersion)
+            {
+            }
+
+            protected override string GetExecutableExtension()
+            {
+                return ".exe";
+            }
+        }
+    }
+}
---
+++ Benchmarks/QueryInterface.cs
@@ -0,0 +1,78 @@
+﻿using BenchmarkComponent;
+using BenchmarkDotNet.Attributes;
+using Windows.ApplicationModel.Chat;
+
+namespace Benchmarks
+{
+    public class QueryInterfacePerf
+    {
+        ClassWithMultipleInterfaces instance;
+        ChatMessage message;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            instance = new ClassWithMultipleInterfaces();
+            message = new ChatMessage();
+        }
+
+        [Benchmark]
+        public int QueryDefaultInterface()
+        {
+            return instance.Property1;
+        }
+
+        [Benchmark]
+        public int QueryNonDefaultInterface()
+        {
+            return instance.IntProperty1;
+        }
+
+        [Benchmark]
+        public bool QueryNonDefaultInterface2()
+        {
+            return instance.BoolProperty1;
+        }
+
+        [Benchmark]
+        public void QueryDefaultInterfaceSetProperty()
+        {
+            instance.Property1 = 4;
+        }
+
+        [Benchmark]
+        public void QueryNonDefaultInterfaceSetProperty()
+        {
+            instance.IntProperty1 = 4;
+        }
+
+        [Benchmark]
+        public bool QuerySDKDefaultInterface()
+        {
+            return message.IsForwardingDisabled;
+        }
+
+        [Benchmark]
+        public bool QuerySDKNonDefaultInterface()
+        {
+            return message.IsSeen;
+        }
+
+        // The following 2 benchmarks try to benchmark the time taken for the first call
+        // rather than the mean time over several calls.  It has the overhead of the object
+        // construction, but it can be used to track regressions to performance.
+        [Benchmark]
+        public int ConstructAndQueryDefaultInterfaceFirstCall()
+        {
+            ClassWithMultipleInterfaces instance2 = new ClassWithMultipleInterfaces();
+            return instance2.Property1;
+        }
+
+        [Benchmark]
+        public int ConstructAndQueryNonDefaultInterfaceFirstCall()
+        {
+            ClassWithMultipleInterfaces instance2 = new ClassWithMultipleInterfaces();
+            return instance2.IntProperty1;
+        }
+    }
+}
