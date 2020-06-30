bb7b7832855a620b3a1fb6bb6c042bc944c67acf
Scott Jones <sjones@microsoft.com>
2020-06-29 17:15:34 -0700

    update to .net 5 sdk preview 7, and a few other dependencies

--- build.cmd
+++ build.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-set CsWinRTNet5SdkVersion=5.0.100-preview.5.20279.10
+set CsWinRTNet5SdkVersion=5.0.100-preview.7.20329.1
 
 :dotnet
 rem Install required .NET 5 SDK version and add to environment
