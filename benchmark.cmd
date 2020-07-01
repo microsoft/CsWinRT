8d8c3976375e2265f5b7fe6e004dab10fb8558b6
Manodasan Wignarajah <mawign@microsoft.com>
2020-07-01 15:26:50 -0700

    Fix absolute paths

--- benchmark.cmd
+++ benchmark.cmd
@@ -1,2 +1,2 @@
 msbuild Benchmarks\Benchmarks.csproj -t:restore -t:build /p:platform=x64 /p:configuration=release
-dotnet S:\CsWinRT\Benchmarks\bin\x64\Release\netcoreapp2.0\Benchmarks.dll -filter * --runtimes netcoreapp2.0 netcoreapp3.1 netcoreapp5.0
+dotnet %~dp0Benchmarks\bin\x64\Release\netcoreapp2.0\Benchmarks.dll -filter * --runtimes netcoreapp2.0 netcoreapp3.1 netcoreapp5.0
