47e7b73e8bd83c0cdd939d97a49e36239efa6025
UJJWAL CHADHA <ujchadha@microsoft.com>
2020-07-20 17:45:42 -0400

    Added Reflection Performance Benchmarks

---
+++ Benchmarks/ReflectionPerf.cs
@@ -0,0 +1,35 @@
+﻿using BenchmarkComponent;
+using BenchmarkDotNet.Attributes;
+
+namespace Benchmarks
+{
+    [MemoryDiagnoser]
+    public class ReflectionPerf
+    {
+        ClassWithMarshalingRoutines instance;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            instance = new ClassWithMarshalingRoutines();
+        }
+
+        [Benchmark]
+        public void ExecuteComWrappersCreateKeyValuePairsFactory()
+        {
+            var value = instance.DefaultKeyValuePairProperty;
+        }
+
+        [Benchmark]
+        public void ExecuteComWrappersCreateArrayFactory()
+        {
+            var value = instance.DefaultArrayProperty;
+        }
+
+        [Benchmark]
+        public void ExecuteComWrappersCreateNullableFactory()
+        {
+            var value = instance.DefaultNullableProperty;
+        }
+    }
+}
