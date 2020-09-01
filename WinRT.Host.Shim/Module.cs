9ff4b8c27dcf6f65f9cff44580b09f04ba20ced2
Scott Jones <sjones@microsoft.com>
2020-08-31 18:42:52 -0700

    PR feedback - isolate all but winrt.runtime.dll (per-component ALC)

--- WinRT.Host.Shim/Module.cs
+++ WinRT.Host.Shim/Module.cs
@@ -7,6 +7,9 @@ using System.IO;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+#if !NETSTANDARD2_0
+using System.Runtime.Loader;
+#endif
 using System.Text;
 using Windows.Foundation;
 using WinRT;
@@ -20,6 +23,68 @@ namespace WinRT.Host
 
         public unsafe delegate int GetActivationFactoryDelegate(IntPtr hstrTargetAssembly, IntPtr hstrRuntimeClassId, IntPtr* activationFactory);
 
+#if NETSTANDARD2_0
+        public static class ActivationLoader
+        {
+            public static Assembly LoadAssembly(string targetAssembly) => Assembly.LoadFrom(targetAssembly);
+        }
+#else
+        class ActivationLoader : AssemblyLoadContext
+        {
+            private AssemblyDependencyResolver _resolver;
+
+            public static Assembly LoadAssembly(string targetAssembly)
+            {
+                var loader = new ActivationLoader(targetAssembly);
+                return loader.LoadFromAssemblyPath(targetAssembly);
+            }
+
+            private ActivationLoader(string path)
+            {
+                _resolver = new AssemblyDependencyResolver(path);
+                AssemblyLoadContext.Default.Resolving += (AssemblyLoadContext assemblyLoadContext, AssemblyName assemblyName) =>
+                {
+                    // Consolidate all WinRT.Runtime loads to the default ALC, or failing that, the first shim ALC 
+                    if (assemblyName.Name == "WinRT.Runtime")
+                    {
+                        string assemblyPath = _resolver.ResolveAssemblyToPath(assemblyName);
+                        if (assemblyPath != null)
+                        {
+                            return LoadFromAssemblyPath(assemblyPath);
+                        }
+                    }
+                    return null;
+                };
+            }
+
+
+            protected override Assembly Load(AssemblyName assemblyName)
+            {
+                if (assemblyName.Name != "WinRT.Runtime")
+                {
+                    string assemblyPath = _resolver.ResolveAssemblyToPath(assemblyName);
+                    if (assemblyPath != null)
+                    {
+                        return LoadFromAssemblyPath(assemblyPath);
+                    }
+                }
+
+                return null;
+            }
+
+            protected override IntPtr LoadUnmanagedDll(string unmanagedDllName)
+            {
+                string libraryPath = _resolver.ResolveUnmanagedDllToPath(unmanagedDllName);
+                if (libraryPath != null)
+                {
+                    return LoadUnmanagedDllFromPath(libraryPath);
+                }
+
+                return IntPtr.Zero;
+            }
+        }
+#endif
+
         public static unsafe int GetActivationFactory(IntPtr hstrTargetAssembly, IntPtr hstrRuntimeClassId, IntPtr* activationFactory)
         {
             *activationFactory = IntPtr.Zero;
@@ -29,8 +94,7 @@ namespace WinRT.Host
 
             try
             {
-                // TODO: this may be risky - alternatives to ensure same ALC as shim?
-                var assembly = Assembly.LoadFrom(targetAssembly);
+                var assembly = ActivationLoader.LoadAssembly(targetAssembly);
                 var type = assembly.GetType("WinRT.Module");
                 if (type != null)
                 {
@@ -55,3 +119,4 @@ namespace WinRT.Host
         }
     }
 }
+
