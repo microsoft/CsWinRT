9190ef4f1fb846440134c7207db4ecfa6f1f69f6
Scott Jones <sjones@microsoft.com>
2020-09-15 08:47:31 -0700

    [DO NOT MERGE] Restore "Update to .NET 5 Preview 8 and update compiler toolset package" (#400)
    * Revert "Revert "Update to .NET 5 Preview 8 and update compiler toolset package (#388)" (#399)"
    
    This reverts commit 598ec082a36359e60e3ac30263d9e4dea351499a.
    
    * fixed a number of build/test issues
    
    * Use VS 16.8 Build Tools if necessary
    
    * check nuget
    
    * cswinrt_build_only
    
    * requirements

--- WinRT.Runtime/Projections/ICommand.cs
+++ WinRT.Runtime/Projections/ICommand.cs
@@ -121,8 +121,8 @@ namespace ABI.System.Windows.Input
     internal sealed unsafe class CanExecuteChangedEventSource : EventSource<global::System.EventHandler>
     {
         internal CanExecuteChangedEventSource(IObjectReference obj,
-            delegate* stdcall<global::System.IntPtr, global::System.IntPtr, out global::WinRT.EventRegistrationToken, int> addHandler,
-            delegate* stdcall<global::System.IntPtr, global::WinRT.EventRegistrationToken, int> removeHandler)
+            delegate* unmanaged[Stdcall]<global::System.IntPtr, global::System.IntPtr, out global::WinRT.EventRegistrationToken, int> addHandler,
+            delegate* unmanaged[Stdcall]<global::System.IntPtr, global::WinRT.EventRegistrationToken, int> removeHandler)
             : base(obj, addHandler, removeHandler)
         {
         }
@@ -146,13 +146,13 @@ namespace ABI.System.Windows.Input
         {
             internal IInspectable.Vftbl IInspectableVftbl;
             private void* _add_CanExecuteChanged_0;
-            public delegate* stdcall<IntPtr, IntPtr, out global::WinRT.EventRegistrationToken, int> add_CanExecuteChanged_0 { get => (delegate* stdcall<IntPtr, IntPtr, out global::WinRT.EventRegistrationToken, int>)_add_CanExecuteChanged_0; set => _add_CanExecuteChanged_0 = value; }
+            public delegate* unmanaged[Stdcall]<IntPtr, IntPtr, out global::WinRT.EventRegistrationToken, int> add_CanExecuteChanged_0 { get => (delegate* unmanaged[Stdcall]<IntPtr, IntPtr, out global::WinRT.EventRegistrationToken, int>)_add_CanExecuteChanged_0; set => _add_CanExecuteChanged_0 = value; }
             private void* _remove_CanExecuteChanged_1;
-            public delegate* stdcall<IntPtr, global::WinRT.EventRegistrationToken, int> remove_CanExecuteChanged_1 { get => (delegate* stdcall<IntPtr, global::WinRT.EventRegistrationToken, int>)_remove_CanExecuteChanged_1; set => _remove_CanExecuteChanged_1 = value; }
+            public delegate* unmanaged[Stdcall]<IntPtr, global::WinRT.EventRegistrationToken, int> remove_CanExecuteChanged_1 { get => (delegate* unmanaged[Stdcall]<IntPtr, global::WinRT.EventRegistrationToken, int>)_remove_CanExecuteChanged_1; set => _remove_CanExecuteChanged_1 = value; }
             private void* _CanExecute_2;
-            public delegate* stdcall<IntPtr, IntPtr, out byte, int> CanExecute_2 { get => (delegate* stdcall<IntPtr, IntPtr, out byte, int>)_CanExecute_2; set => _CanExecute_2 = value; }
+            public delegate* unmanaged[Stdcall]<IntPtr, IntPtr, out byte, int> CanExecute_2 { get => (delegate* unmanaged[Stdcall]<IntPtr, IntPtr, out byte, int>)_CanExecute_2; set => _CanExecute_2 = value; }
             private void* _Execute_3;
-            public delegate* stdcall<IntPtr, IntPtr, int> Execute_3 { get => (delegate* stdcall<IntPtr, IntPtr, int>)_Execute_3; set => _Execute_3 = value; }
+            public delegate* unmanaged[Stdcall]<IntPtr, IntPtr, int> Execute_3 { get => (delegate* unmanaged[Stdcall]<IntPtr, IntPtr, int>)_Execute_3; set => _Execute_3 = value; }
 
             private static readonly Vftbl AbiToProjectionVftable;
             public static readonly IntPtr AbiToProjectionVftablePtr;
