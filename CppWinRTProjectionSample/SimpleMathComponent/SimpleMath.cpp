3b0df9d271ec0a7cdd511f39fb695861a090cfce
Angela Zhang <angzhang@microsoft.com>
2020-09-22 15:14:45 -0400

    adding sample files

---
+++ CppWinRTProjectionSample/SimpleMathComponent/SimpleMath.cpp
@@ -0,0 +1,25 @@
+﻿#include "pch.h"
+#include "SimpleMath.h"
+#include "SimpleMath.g.cpp"
+
+namespace winrt::SimpleMathComponent::implementation
+{
+	double SimpleMath::add(double firstNumber, double secondNumber)
+	{
+		return firstNumber + secondNumber;
+	}
+	double SimpleMath::subtract(double firstNumber, double secondNumber)
+	{
+		return firstNumber - secondNumber;
+	}
+	double SimpleMath::multiply(double firstNumber, double secondNumber)
+	{
+		return firstNumber * secondNumber;
+	}
+	double SimpleMath::divide(double firstNumber, double secondNumber)
+	{
+		if (0 == secondNumber)
+			return -1;
+		return firstNumber / secondNumber;
+	}
+}
