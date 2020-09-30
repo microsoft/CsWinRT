cac5a23df1fc61756b3928c5ba9a403b5f75615a
UJJWAL CHADHA <ujchadha@microsoft.com>
2020-09-30 17:31:29 -0400

    Update testwinrt and clean up redundant code

--- get_testwinrt.cmd
+++ get_testwinrt.cmd
@@ -12,7 +12,7 @@ git checkout -f master
 if ErrorLevel 1 popd & exit /b !ErrorLevel!
 git fetch -f
 if ErrorLevel 1 popd & exit /b !ErrorLevel!
-git reset -q --hard e058f2fc16e4812a16b5453da7ef4d65b3cf0bfe
+git reset -q --hard 5cc3911373ac6ceac3aec2d5983fec0155d5e4e3
 if ErrorLevel 1 popd & exit /b !ErrorLevel!
 echo Restoring Nuget
 ..\.nuget\nuget.exe restore
