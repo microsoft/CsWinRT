ab6a5eca27af85d7ddba0bd0f09c2829d5925aa0
Scott Jones <sjones@microsoft.com>
2020-11-10 22:59:33 -0800

    Use custom marshalers when type is erased with MarshalInspectable<object> (#568)
    * Add generic marshaling support for Uri
    
    * generalized for all custom marshalers
    
    Co-authored-by: Manodasan Wignarajah <mawign@microsoft.com>

--- UnitTest/TestComponentCSharp_Tests.cs
+++ UnitTest/TestComponentCSharp_Tests.cs
@@ -624,6 +624,11 @@ namespace UnitTest
         [Fact]
         public void TestObjectCasting()
         {
+            object expected_uri = new Uri("http://aka.ms/cswinrt");
+            TestObject.ObjectProperty = expected_uri;
+            Assert.Equal(expected_uri, TestObject.UriProperty);
+            Assert.Equal(expected_uri, TestObject.ObjectProperty);
+
             var expected = new KeyValuePair<string, string>("key", "value");
             TestObject.ObjectProperty = expected;
             var out_pair = (KeyValuePair<string, string>)TestObject.ObjectProperty;
