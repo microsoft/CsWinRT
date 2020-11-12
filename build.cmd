66da8e1d1eb84cb962a1d0fbcea688a1e2838eff
Manodasan Wignarajah <mawign@microsoft.com>
2020-11-11 22:52:25 -0800

    Update to .NET 5 RTM SDK. (#579)

--- build.cmd
+++ build.cmd
@@ -1,7 +1,7 @@
 @echo off
 if /i "%cswinrt_echo%" == "on" @echo on
 
-set CsWinRTNet5SdkVersion=5.0.100-rc.1.20454.5
+set CsWinRTNet5SdkVersion=5.0.100
 
 :dotnet
 rem Install required .NET 5 SDK version and add to environment
