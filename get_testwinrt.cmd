9401a7992645bb4500271d31fa8e861fd19e0184
Ujjwal Chadha <66971578+ujjwalchadha@users.noreply.github.com>
2020-11-20 14:24:33 -0500

    Update get_testwinrt.cmd

--- get_testwinrt.cmd
+++ get_testwinrt.cmd
@@ -12,7 +12,7 @@ git checkout -f master
 if ErrorLevel 1 popd & exit /b !ErrorLevel!
 git fetch -f
 if ErrorLevel 1 popd & exit /b !ErrorLevel!
-git reset -q --hard 5df065fd3bccf314079e39e1bf2644fd239615c1
+git reset -q --hard 5024e7d7f3d9a0062b8d171cd83752248489c36d
 if ErrorLevel 1 popd & exit /b !ErrorLevel!
 echo Restoring Nuget
 ..\.nuget\nuget.exe restore
