7ceab90d8f8ada6bf9399a1470649358d4f4b3ef
Manodasan Wignarajah <mawign@microsoft.com>
2020-12-30 13:57:28 -0800

    Add WinUI test app to test composable types.

---
+++ src/Authoring/AuthoringWinUITest/AuthoringWinUITest/MainWindow.xaml.cpp
@@ -0,0 +1,43 @@
+﻿#include "pch.h"
+#include "MainWindow.xaml.h"
+#if __has_include("MainWindow.g.cpp")
+#include "MainWindow.g.cpp"
+#endif
+
+using namespace winrt;
+using namespace Microsoft::UI::Xaml;
+using namespace AuthoringSample;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace winrt::AuthoringWinUITest::implementation
+{
+    MainWindow::MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    int32_t MainWindow::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainWindow::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainWindow::myButton_Click(IInspectable const&, RoutedEventArgs const&)
+    {
+        myButton().Content(box_value(L"Clicked"));
+
+        ButtonUtils buttonUtils;
+        auto defaultButton = buttonUtils.GetButton();
+        auto customButton = buttonUtils.GetCustomButton();
+//        EXPECT_EQ(customButton.GetVariant(), 3);
+
+        CustomButton button;
+//        EXPECT_EQ(button.GetVariant(), 3);
+    }
+}
