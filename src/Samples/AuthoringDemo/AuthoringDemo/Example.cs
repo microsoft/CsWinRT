ac799d87bc8460d3493acea0f32be2acd76f72d1
Angela Zhang <angzhang@microsoft.com>
2021-01-22 12:16:01 -0800

    adding authoring sample

---
+++ src/Samples/AuthoringDemo/AuthoringDemo/Example.cs
@@ -0,0 +1,12 @@
+﻿namespace AuthoringDemo
+{
+    public sealed class Example
+    {
+        public int SampleProperty { get; set; }
+
+        public static string SayHello()
+        {
+            return "Hello from your C# WinRT component";
+        }
+    }
+}
---
+++ src/Samples/AuthoringDemo/CppConsoleApp/main.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include "iostream";
+
+using namespace winrt;
+using namespace Windows::Foundation;
+
+int main()
+{
+    init_apartment();
+
+    AuthoringDemo::Example ex;
+    ex.SampleProperty(42);
+    std::wcout << ex.SampleProperty() << std::endl;
+    std::wcout << ex.SayHello().c_str() << std::endl;
+
+    AuthoringDemo::FolderEnumeration folderEnumerator;
+    folderEnumerator.GetFilesAndFoldersAsync().get();
+    std::wcout << folderEnumerator.AllFiles().c_str() << std::endl;
+}
