fd9d6acb4f6d4c2460fb4a75e94a6326ba986afc
Joshua Larkin <jlarkin@microsoft.com>
2021-01-22 07:53:02 -0800

    update gitignore, add analyzer release files, update unittests for new diagnostic identifer (wme -> cswinrt)

---
+++ src/Authoring/WinRT.SourceGenerator/AnalyzerReleases.Unshipped.md
@@ -0,0 +1,33 @@
+﻿; Unshipped analyzer release
+; https://github.com/dotnet/roslyn-analyzers/blob/master/src/Microsoft.CodeAnalysis.Analyzers/ReleaseTrackingAnalyzers.Help.md
+
+### New Rules
+Rule ID | Category | Severity | Notes
+--------|----------|----------|-------
+CsWinRT1000 | Usage | Error | WinRTRules
+CsWinRT1001 | Usage | Error | WinRTRules
+CsWinRT1002 | Usage | Error | WinRTRules
+CsWinRT1003 | Usage | Error | WinRTRules
+CsWinRT1004 | Usage | Error | WinRTRules
+CsWinRT1005 | Usage | Error | WinRTRules
+CsWinRT1006 | Usage | Error | WinRTRules
+CsWinRT1007 | Usage | Error | WinRTRules
+CsWinRT1008 | Usage | Error | WinRTRules
+CsWinRT1009 | Usage | Error | WinRTRules
+CsWinRT1010 | Usage | Error | WinRTRules
+CsWinRT1011 | Usage | Error | WinRTRules
+CsWinRT1012 | Usage | Error | WinRTRules
+CsWinRT1013 | Usage | Error | WinRTRules
+CsWinRT1014 | Usage | Error | WinRTRules
+CsWinRT1015 | Usage | Error | WinRTRules
+CsWinRT1016 | Usage | Error | WinRTRules
+CsWinRT1017 | Usage | Error | WinRTRules
+CsWinRT1018 | Usage | Error | WinRTRules
+CsWinRT1019 | Usage | Error | WinRTRules
+CsWinRT1020 | Usage | Error | WinRTRules
+CsWinRT1021 | Usage | Error | WinRTRules
+CsWinRT1022 | Usage | Error | WinRTRules
+CsWinRT1023 | Usage | Error | WinRTRules
+CsWinRT1024 | Usage | Error | WinRTRules
+CsWinRT1025 | Usage | Error | WinRTRules
+CsWinRT1026 | Usage | Error | WinRTRules
--- src/Tests/DiagnosticTests/UnitTesting.cs
+++ src/Tests/DiagnosticTests/UnitTesting.cs
@@ -47,19 +47,20 @@ namespace DiagnosticTests
         /// from the cswinrt source generator based on the given source code</summary>
         /// <param name="source"></param>
         [Test, TestCaseSource(nameof(ValidCases))] 
-        public void CheckNoDiagnostic(string source)
+        private void CheckNoDiagnostic(string source)
         { 
             Compilation compilation = CreateCompilation(source);
             RunGenerators(compilation, out var diagnosticsFound,  new Generator.SourceGenerator());
-            var WinRTDiagnostics = diagnosticsFound.Where(diag => diag.Id.StartsWith("WME"));
-            string builder = "";
-            foreach (var d in WinRTDiagnostics)
-            {
-                builder += d.Descriptor.Description + "\n";
-            }
+            
+            var WinRTDiagnostics = diagnosticsFound.Where(diag => diag.Id.StartsWith("CsWinRT"));
             if (WinRTDiagnostics.Count() != 0)
             {
-                throw new System.Exception("Expected no diagnostics. But found:" + builder);
+                string foundDiagnostics = "";
+                foreach (var d in WinRTDiagnostics)
+                {
+                    foundDiagnostics += d.Descriptor.Description + "\n";
+                }
+                throw new System.Exception("Expected no diagnostics. But found:" + foundDiagnostics);
             }
         }
 
@@ -68,21 +69,21 @@ namespace DiagnosticTests
         /// it checks that a diagnostic with the same description was raised by the source generator 
         /// </summary> 
         [Test, TestCaseSource(nameof(InvalidCases))]
-        public void CodeHasDiagnostic(string testCode, DiagnosticDescriptor rule)
+        private void CodeHasDiagnostic(string testCode, DiagnosticDescriptor rule)
         { 
             Compilation compilation = CreateCompilation(testCode);
             RunGenerators(compilation, out var diagnosticsFound,  new Generator.SourceGenerator());
             HashSet<DiagnosticDescriptor> diagDescsFound = MakeDiagnosticSet(diagnosticsFound);
-            string builder = "";
             if (!diagDescsFound.Contains(rule))
             {
-                foreach (var d in diagDescsFound)
-                {
-                    builder += d.Description + "\n";
-                }
                 if (diagDescsFound.Count != 0)
                 {
-                    throw new System.Exception("Didn't find the expected diagnostic, found:\n" + builder);
+                    string foundDiagnostics = "";
+                    foreach (var d in diagDescsFound)
+                    {
+                        foundDiagnostics += d.Description + "\n";
+                    }
+                    throw new System.Exception("Didn't find the expected diagnostic, found:\n" + foundDiagnostics);
                 }
                 else
                 { 
