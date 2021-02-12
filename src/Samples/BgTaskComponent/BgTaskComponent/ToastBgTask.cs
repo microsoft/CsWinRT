8e1ce8523c7cf36064c289a1ea25076ca2f2067e
angelazhangmsft <angzhang@microsoft.com>
2021-02-12 10:26:56 -0800

    Add background task component sample (#736)
    * bg task works with hacks
    
    * Create README.md
    
    * edits
    
    * Update README.md
    
    * edits
    
    * get solution configs right
    
    * fix register bgtask logic
    
    * add runtimeidentifiers for cmdline build
    
    * define tfm property for refactoring, remove sdk tool pkg
    
    * update readme with wapproj changes
    
    * Update README.md

---
+++ src/Samples/BgTaskComponent/BgTaskComponent/ToastBgTask.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using Windows.ApplicationModel.Background;
+using Windows.Data.Xml.Dom;
+using Windows.UI.Notifications;
+
+namespace BgTaskComponent
+{
+    public sealed class ToastBgTask: IBackgroundTask
+    {
+
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            Debug.WriteLine("Background " + taskInstance.Task.Name + " Starting...");
+
+            // Perform the background task.
+            SendToast();
+        }
+
+        private static void SendToast()
+        {
+            XmlDocument toastXml = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastImageAndText02);
+            XmlNodeList textElements = toastXml.GetElementsByTagName("text");
+            textElements[0].AppendChild(toastXml.CreateTextNode("A toast example"));
+            textElements[1].AppendChild(toastXml.CreateTextNode("You've changed timezones!"));
+            ToastNotification notification = new(toastXml);
+            ToastNotificationManager.CreateToastNotifier().Show(notification);
+        }
+    }
+}
