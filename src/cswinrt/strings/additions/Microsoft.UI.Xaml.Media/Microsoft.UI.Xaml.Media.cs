a696f3bd856913fab3cdf88e857367a1c9ee87ff
Manodasan Wignarajah <mawign@microsoft.com>
2021-02-22 16:07:35 -0800

    Add missing source metadata for WindowsRuntimeType attribute (#744)
    * Add source metadata for couple types that were missed.
    
    * Temporary fix source metadata for color via generator.
    
    * Fix issue where generated methods for static properties and events weren't marked as static.

--- src/cswinrt/strings/additions/Microsoft.UI.Xaml.Media/Microsoft.UI.Xaml.Media.cs
+++ src/cswinrt/strings/additions/Microsoft.UI.Xaml.Media/Microsoft.UI.Xaml.Media.cs
@@ -3,7 +3,7 @@ namespace Microsoft.UI.Xaml.Media
 {
     using Windows.Foundation;
 
-    [global::WinRT.WindowsRuntimeType]
+    [global::WinRT.WindowsRuntimeType("Microsoft.UI")]
     [StructLayout(LayoutKind.Sequential)]
     public struct Matrix : IFormattable
     {
