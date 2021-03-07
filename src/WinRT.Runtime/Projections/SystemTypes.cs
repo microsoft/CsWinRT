bb335625ba0e846e63e6bbafb9b05de8290a2a42
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2021-03-07 23:09:09 +0800

    Use .NET Core 3.1 compatible marshaling for DateTimeOffset.

--- src/WinRT.Runtime/Projections/SystemTypes.cs
+++ src/WinRT.Runtime/Projections/SystemTypes.cs
@@ -55,6 +55,11 @@ namespace ABI.System
         // 'System.DateTimeOffset.Ticks' is relative to 01/01/0001
         public long UniversalTime;
 
+        // Numer of ticks counted between 0001-01-01, 00:00:00 and 1601-01-01, 00:00:00.
+        // You can get this through:  (new DateTimeOffset(1601, 1, 1, 0, 0, 1, TimeSpan.Zero)).Ticks;
+        private const long ManagedUtcTicksAtNativeZero = 504911232000000000;
+        // DO NOT use ToFileTime/FromFileTime, which don't support negative UniversalTime.
+
         public struct Marshaler
         {
             public DateTimeOffset __abi;
@@ -62,14 +67,14 @@ namespace ABI.System
 
         public static Marshaler CreateMarshaler(global::System.DateTimeOffset value)
         {
-            return new Marshaler { __abi = new DateTimeOffset { UniversalTime = value.ToFileTime() } };
+            return new Marshaler { __abi = new DateTimeOffset { UniversalTime = value.Ticks - ManagedUtcTicksAtNativeZero } };
         }
 
         public static DateTimeOffset GetAbi(Marshaler m) => m.__abi;
 
         public static global::System.DateTimeOffset FromAbi(DateTimeOffset value)
         {
-            return global::System.DateTimeOffset.FromFileTime(value.UniversalTime);
+            return new global::System.DateTimeOffset(value.UniversalTime + ManagedUtcTicksAtNativeZero, global:: System.TimeSpan.Zero);
         }
 
         public static unsafe void CopyAbi(Marshaler arg, IntPtr dest) =>
