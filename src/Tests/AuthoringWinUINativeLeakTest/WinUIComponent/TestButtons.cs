40539f1b14494dfcfb08c9d1f89a6095355fdcb8
Angela Zhang <angzhang@microsoft.com>
2021-03-24 20:44:32 -0700

    adding the component

---
+++ src/Tests/AuthoringWinUINativeLeakTest/WinUIComponent/TestButtons.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.UI.Xaml.Controls;
+
+namespace WinUIComponent
+{
+    public static class TestButtons
+    {
+        public static WeakReference weakRefCustomButton = null;
+        public static WeakReference weakRefRegularButton = null;
+        public static bool isAliveCustomButton { get { return weakRefCustomButton.IsAlive; } }
+        public static bool isAliveRegularButton { get { return weakRefRegularButton.IsAlive; } }
+
+        public static CustomButton GetCustomButton()
+        {
+            var customButton = new CustomButton();
+            weakRefCustomButton = new WeakReference(customButton);
+            return customButton;
+        }
+
+        public static Button GetRegularButton()
+        {
+            Button button = new Button
+            {
+                Content = "Button"
+            };
+            weakRefRegularButton = new WeakReference(button);
+            return button;
+        }
+    }
+}
