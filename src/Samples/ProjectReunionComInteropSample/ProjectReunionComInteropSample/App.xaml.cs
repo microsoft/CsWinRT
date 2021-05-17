5eff8ff317a05d1cdb1ba40f56154d5ccb2f7b04
Angela Zhang <angzhang@microsoft.com>
2021-05-17 14:06:02 -0700

    adding winui sample

---
+++ src/Samples/ProjectReunionComInteropSample/ProjectReunionComInteropSample/App.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media;
+using Windows.Web.Http;
+
+namespace WinUIComInteropSample
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+        }
+
+        Window myWindow;
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
+        {
+            var window = new MainWindow();
+            window.Activate();
+
+            myWindow = window;
+        }
+
+    }
+
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            //WinRT.ComWrappersSupport.InitializeComWrappers();
+
+            Application.Start((e) => new App());
+        }
+    }
+}
---
+++ src/Samples/ProjectReunionComInteropSample/ProjectReunionComInteropSample/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Media;
+using Windows.Media.Core;
+using Windows.Media.Playback;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
+using Windows.UI.Core;
+using WinRT;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace WinUIComInteropSample
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        private async void userConsentVerifierButton_Click(object sender, RoutedEventArgs e)
+        {
+            var hwnd = ((Object)this).As<WinRT.Interop.IWindowNative>().WindowHandle;
+            await Windows.Security.Credentials.UI.UserConsentVerifierInterop.RequestVerificationForWindowAsync(hwnd, "Test Message");;
+        }
+
+        private async void filePickerButton_Click(object sender, RoutedEventArgs e)
+        {
+            // casting to (Object) is a workaround - cswinrt issue #485 with .As<> 
+            var hwnd = ((object)this).As<WinRT.Interop.IWindowNative>().WindowHandle;
+            var folderPicker = new FolderPicker();
+            var initializeWithWindow = ((object)folderPicker).As<WinRT.Interop.IInitializeWithWindow>();
+            initializeWithWindow.Initialize(hwnd);
+            folderPicker.FileTypeFilter.Add("*");
+
+            var folder = await folderPicker.PickSingleFolderAsync();
+            //filePickerButton.Content = folder != null ? folder.Path : string.Empty;
+        }
+    }
+}
