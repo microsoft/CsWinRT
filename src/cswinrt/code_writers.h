dc5694df97fa3f87b2c7a73e3039c26bac4ac3b6
UJJWAL CHADHA <ujchadha@microsoft.com>
2021-06-17 14:48:21 -0700

    added out param default to event source handlers

--- src/cswinrt/code_writers.h
+++ src/cswinrt/code_writers.h
@@ -498,6 +498,18 @@ namespace cswinrt
         w.write("default(%)", bind<write_projection_type>(semantics));
     }
 
+    void write_event_out_defaults(writer& w, method_signature const& methodSig)
+    {
+        for (auto&& param : methodSig.params())
+        {
+            if (get_param_category(param) == param_category::out || get_param_category(param) == param_category::receive_array)
+            {
+                w.write("\n% = default(%);", bind<write_parameter_name>(param), bind<write_projection_type>(get_type_semantics(param.second->Type())));
+                break;
+            }
+        }
+    }
+
     void write_event_invoke_args(writer& w, method_signature const& methodSig)
     {
         w.write("%", bind_list<write_parameter_name_with_modifier>(", ", methodSig.params()));
@@ -6471,7 +6483,7 @@ handler = (%) =>
 {
 var localDel = _event;
 if (localDel == null)
-{
+{%
 return %;
 }
 %localDel.Invoke(%);
@@ -6488,6 +6500,7 @@ return handler;
                     eventTypeCode,
                     bind<write_event_source_type_name>(eventTypeSemantics), 
                     bind<write_event_invoke_params>(invokeMethodSig),
+                    bind<write_event_out_defaults>(invokeMethodSig),
                     bind<write_event_invoke_return_default>(invokeMethodSig),
                     bind<write_event_invoke_return>(invokeMethodSig),
                     bind<write_event_invoke_args>(invokeMethodSig));
