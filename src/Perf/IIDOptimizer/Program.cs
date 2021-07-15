dc4385d78679569f7255956c2c004bfe3ab1032b
Joshua Larkin <70237359+j0shuams@users.noreply.github.com>
2021-07-15 16:49:35 -0700

    Don't patch twice

--- src/Perf/IIDOptimizer/Program.cs
+++ src/Perf/IIDOptimizer/Program.cs
@@ -1,10 +1,13 @@
 ﻿using Mono.Cecil;
+using Mono.Cecil.Cil;
 using System;
 using System.IO;
 using System.CommandLine;
 using System.CommandLine.Invocation;
 using System.Threading.Tasks;
 using System.Collections.Generic;
+
+using System.Linq;
 
 namespace GuidPatch
 {
@@ -18,7 +21,16 @@ namespace GuidPatch
                 argumentType: typeof(string), 
                 arity: ArgumentArity.ExactlyOne 
             );
-        
+
+        static readonly Option _outputDir =
+            new Option
+            (
+                alias: "--outputDirectory",
+                description: "The directory to save the patched .dll to.",
+                argumentType: typeof(string),
+                arity: ArgumentArity.ExactlyOne
+            );
+
         static readonly Option _references =
             new Option
             (
@@ -33,33 +45,50 @@ namespace GuidPatch
             var rootCommand = new RootCommand { };
             // friendlier option names 
             _targetAssembly.AddAlias("--target");
+            _outputDir.AddAlias("--outdir");
             _references.AddAlias("--refs");
 
             rootCommand.AddOption(_targetAssembly);
+            rootCommand.AddOption(_outputDir);
             rootCommand.AddOption(_references);
 
-            rootCommand.Handler = CommandHandler.Create<string, IEnumerable<FileInfo>>(GuidPatch);
+            rootCommand.Handler = CommandHandler.Create<string, string, IEnumerable<FileInfo>>(GuidPatch);
             await rootCommand.InvokeAsync(args);            
         }
 
-        private static int GuidPatch(string targetAssembly, IEnumerable<FileInfo> references)
+        private static int GuidPatch(string targetAssembly, string outputDirectory, IEnumerable<FileInfo> references)
         {
             var resolver = new ReferenceAssemblyResolver(references);
             try 
             {
                 AssemblyDefinition winRTRuntimeAssembly = resolver.Resolve(new AssemblyNameReference("WinRT.Runtime", default));
                    
-                Directory.CreateDirectory("obj\\IIDOptimizer"); 
-                
-                var guidPatcher = new GuidPatcher(
-                    targetAssembly, 
-                    resolver,
-                    winRTRuntimeAssembly);
+                var readerParameters = new ReaderParameters(ReadingMode.Deferred)
+                {
+                    ReadWrite = true,
+                    InMemory = true,
+                    AssemblyResolver = resolver,
+                    ThrowIfSymbolsAreNotMatching = false,
+                    SymbolReaderProvider = new DefaultSymbolReaderProvider(false),
+                    ApplyWindowsRuntimeProjections = false,
+                    ReadSymbols = true
+                };
+
+                var targetAssemblyDefinition = AssemblyDefinition.ReadAssembly(targetAssembly, readerParameters);
+
+                if (targetAssemblyDefinition.MainModule.Types.Any(typeDef => typeDef.Name == "<GuidPatcherImplementationDetails>"))
+                {
+                    Console.WriteLine("Target assembly has already been patched. Exiting early as there is no work to do.");
+                    return -2;
+                }
+
+                var guidPatcher = new GuidPatcher(winRTRuntimeAssembly, targetAssemblyDefinition);
 
                 int numPatches = guidPatcher.ProcessAssembly(); 
 
-                guidPatcher.SaveAssembly(guidPatcher.OptimizerDir); 
+                guidPatcher.SaveAssembly(outputDirectory);
 
+                Console.WriteLine($"Saved patched .dll to {outputDirectory}");
                 Console.WriteLine($"{numPatches} IID calculations/fetches patched"); 
                 return 0; 
             }
