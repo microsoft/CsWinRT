08209f5954e7dd58a90290a3883f4db3f035ebec
Manodasan Wignarajah <mawign@microsoft.com>
2021-09-02 16:50:16 -0700

    Add test projection with a set property that references a get property from another projection.

---
+++ src/Tests/TestComponentCSharp/AnotherAssembly.SetPropertyClass.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "AnotherAssembly.SetPropertyClass.g.h"
+
+namespace winrt::TestComponentCSharp::AnotherAssembly::implementation
+{
+    struct SetPropertyClass : SetPropertyClassT<SetPropertyClass>
+    {
+        SetPropertyClass() = default;
+
+        void ReadWriteProperty(int32_t value);
+        int32_t ReadWriteProperty();
+
+        int32_t readWriteValue;
+    };
+}
