4fdfdbb4b722f591d045cb304677e9182531abdd
Joshua Larkin <70237359+j0shuams@users.noreply.github.com>
2021-10-04 23:40:30 -0700

    class

--- src/Samples/TestEmbedded/C++ Components/Beta/Class.h
+++ src/Samples/TestEmbedded/C++ Components/Beta/Class.h
@@ -1,10 +1,6 @@
 #pragma once
 #include "Class.g.h"
 
-// Note: Remove this static_assert after copying these generated source files to your project.
-// This assertion exists to avoid compiling these generated source files directly.
-static_assert(false, "Do not compile generated C++/WinRT source files directly");
-
 namespace winrt::Beta::implementation
 {
     struct Class : ClassT<Class>
