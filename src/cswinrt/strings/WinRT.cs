01ee662f33ebb104fe1deeb30c697e9477d6c748
Joshua Larkin <70237359+j0shuams@users.noreply.github.com>
2021-10-28 08:55:00 -0700

    +

--- src/cswinrt/strings/WinRT.cs
+++ src/cswinrt/strings/WinRT.cs
@@ -114,7 +114,7 @@ namespace WinRT
                 return false;
             }
         }
-+
+
         private static unsafe bool TryCreate(string fileName, out DllModule module)
         {
             // Explicitly look for module in the same directory as this one, and
@@ -851,9 +851,7 @@ namespace WinRT
 #pragma warning restore 0436
         internal static void InitalizeProjection()
         {
-#if !EMBED
             ComWrappersSupport.RegisterProjectionAssembly(typeof(ProjectionInitializer).Assembly);
-#endif
         }
     }
 }
