44a2eb5374ea7135e7a85693750f56812f3e7817
Ujjwal Chadha <ujchadha@microsoft.com>
2021-10-28 14:28:34 -0700

    Fix commit id

--- src/get_testwinrt.cmd
+++ src/get_testwinrt.cmd
@@ -14,7 +14,7 @@ git checkout -f master
 if ErrorLevel 1 popd & exit /b !ErrorLevel!
 git fetch -f
 if ErrorLevel 1 popd & exit /b !ErrorLevel!
-git reset -q --hard e610d3a3cfabdb96bc5666aacedbcdb027e02d50
+git reset -q --hard 62c15ff0014e8c4e9e6a8bc2446909146337c685
 if ErrorLevel 1 popd & exit /b !ErrorLevel!
 echo Restoring Nuget
 %this_dir%.nuget\nuget.exe restore
