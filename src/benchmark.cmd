4810a988551a4d5af029adf100d77df4df0aa0c5
Manodasan Wignarajah <mawign@microsoft.com>
2021-10-29 11:41:24 -0700

    Add cmd

--- src/benchmark.cmd
+++ src/benchmark.cmd
@@ -1,2 +1,4 @@
 msbuild Benchmarks\Benchmarks.csproj -t:restore -t:build /p:platform=x64 /p:configuration=release /p:solutiondir=%~dp0 /p:IsDotnetBuild=false
-dotnet %~dp0Benchmarks\bin\x64\Release\netcoreapp2.0\Benchmarks.dll -filter * --runtimes netcoreapp2.0 netcoreapp3.1 netcoreapp5.0
+rem dotnet %~dp0Benchmarks\bin\x64\Release\netcoreapp2.0\Benchmarks.dll -filter * --runtimes netcoreapp2.0 netcoreapp3.1 netcoreapp5.0
+rem dotnet %~dp0Benchmarks\bin\x64\Release\netcoreapp2.0\Benchmarks.dll -p ETW -filter Benchmarks.QueryInterfacePerf.GetVector Benchmarks.QueryInterfacePerf.GetVectorInt Benchmarks.ReflectionPerf.ExecuteMarshalingForCustomObject --runtimes netcoreapp5.0
+dotnet %~dp0Benchmarks\bin\x64\Release\netcoreapp2.0\Benchmarks.dll -p ETW -filter Benchmarks.ReflectionPerf.IntEventSource2* --runtimes netcoreapp5.0
