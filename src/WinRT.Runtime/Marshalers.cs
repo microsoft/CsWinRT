6ca62675dad0c6747d03865f0699a8a09cffd2a5
Joshua Larkin <70237359+j0shuams@users.noreply.github.com>
2022-01-19 11:25:57 -0800

    abstraction

--- src/WinRT.Runtime/Marshalers.cs
+++ src/WinRT.Runtime/Marshalers.cs
@@ -1167,27 +1167,7 @@ namespace WinRT
 
         public static IObjectReference CreateMarshaler(T o, bool unwrapObject = true)
         {
-            if (o is null)
-            {
-                return null;
-            }
-
-            if (unwrapObject && ComWrappersSupport.TryUnwrapObject(o, out var objRef))
-            {
-                return objRef.As<IInspectable.Vftbl>(IInspectable.IID);
-            }
-            var publicType = o.GetType();
-            Type helperType = Projections.FindCustomHelperTypeMapping(publicType, true);
-            if (helperType != null)
-            {
-                var parms = new[] { Expression.Parameter(typeof(object), "arg") };
-                var createMarshaler = Expression.Lambda<Func<object, IObjectReference>>(
-                    Expression.Call(helperType.GetMethod("CreateMarshaler", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static), 
-                        new[] { Expression.Convert(parms[0], publicType) }), parms).Compile();
-                return createMarshaler(o);
-            }
-
-            return ComWrappersSupport.CreateCCWForObject<IInspectable.Vftbl>(o, IInspectable.IID);
+            return CreateMarshaler<IInspectable.Vftbl>(o, IInspectable.IID, unwrapObject);
         }
 
         public static IntPtr GetAbi(IObjectReference objRef) =>
