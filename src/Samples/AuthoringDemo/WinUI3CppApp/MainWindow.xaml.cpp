b01c69d2adbed55ae82c63522f31ee12e0b06a50
angelazhangmsft <angzhang@microsoft.com>
2022-01-31 16:22:11 -0800

    Update authoring samples to .NET 6 (#1066)
    * update authoring samples to .NET 6
    
    update samples and cleanup some of the docs
    
    * Update authoring.md
    
    * Update authoring.md
    
    * Update authoring.md
    
    * add packaged cpp app
    
    * address feedback and delete readme.txt

---
+++ src/Samples/AuthoringDemo/WinUI3CppApp/MainWindow.xaml.cpp
@@ -0,0 +1,25 @@
+﻿#include "pch.h"
+#include "MainWindow.xaml.h"
+#if __has_include("MainWindow.g.cpp")
+#include "MainWindow.g.cpp"
+#endif
+
+using namespace winrt;
+using namespace Microsoft::UI::Xaml;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace winrt::WinUI3CppApp::implementation
+{
+    MainWindow::MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    void MainWindow::sayHelloButton_Click(IInspectable const&, RoutedEventArgs const&)
+    {
+        AuthoringDemo::Example ex;
+        sayHelloButton().Content(box_value(ex.SayHello()));
+    }
+}
