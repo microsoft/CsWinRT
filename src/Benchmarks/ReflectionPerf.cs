1d693015739ac782aa45ca944da99ae559239d3b
Manodasan Wignarajah <mawign@microsoft.com>
2022-02-06 02:17:06 -0800

    Benchmarks to test concurrent dictionary performance

--- src/Benchmarks/ReflectionPerf.cs
+++ src/Benchmarks/ReflectionPerf.cs
@@ -1,6 +1,7 @@
 ﻿using BenchmarkComponent;
 using BenchmarkDotNet.Attributes;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 
 namespace Benchmarks
@@ -11,6 +12,19 @@ namespace Benchmarks
         ClassWithMarshalingRoutines instance;
         IDictionary<String, WrappedClass> instanceDictionary;
         ManagedObjectWithInterfaces managedObject;
+        ConcurrentDictionary<Type, bool> ttest1;
+        ConcurrentDictionary<Type, Type> ttest2;
+        ConcurrentDictionary<Type, bool> ttest3;
+        ConcurrentDictionary<Type, string> ttest4;
+        ConcurrentDictionary<Type, TypeCache> ttest5;
+        ConcurrentDictionary<Type, TypeCache2> ttest6;
+        ConcurrentDictionary<Type, bool> ttest7;
+        ConcurrentDictionary<Type, Type> ttest8;
+        ConcurrentDictionary<Type, bool> ttest9;
+        ConcurrentDictionary<Type, string> ttest10;
+        ConcurrentDictionary<Type, TypeCache> ttest11;
+        ConcurrentDictionary<Type, TypeCache2> ttest12;
+
 
         [GlobalSetup]
         public void Setup()
@@ -18,6 +32,18 @@ namespace Benchmarks
             instance = new ClassWithMarshalingRoutines();
             instanceDictionary = instance.ExistingDictionary;
             managedObject = new ManagedObjectWithInterfaces();
+            ttest1 = new();
+            ttest2 = new();
+            ttest3 = new();
+            ttest4 = new();
+            ttest5 = new();
+            ttest6 = new();
+            ttest7 = new();
+            ttest8 = new();
+            ttest9 = new();
+            ttest10 = new();
+            ttest11= new();
+            ttest12 = new();
         }
 
         [Benchmark]
@@ -296,5 +322,384 @@ namespace Benchmarks
         {
             return new WeakReference<ClassWithMarshalingRoutines>(instance);
         }
+
+        private Type[] GetTypes()
+        {
+            return new Type [] { 
+                typeof(ClassWithMarshalingRoutines),
+                typeof(int), 
+                typeof(ReflectionPerf), 
+                typeof(System.Uri), 
+                typeof(double), 
+                typeof(ManagedObjectWithInterfaces), 
+                typeof(ManagedComposableObjectWithInterfaces), 
+                typeof(ManagedEvents),
+                typeof(object),
+                typeof(ReflectionPerf),
+                typeof(System.Uri),
+                typeof(double),
+                typeof(double),
+                typeof(ManagedObjectWithInterfaces),
+                typeof(ManagedComposableObjectWithInterfaces),
+                typeof(BlittableStruct),
+                typeof(NonBlittable),
+                typeof(ClassWithMarshalingRoutines),
+                typeof(int),
+                typeof(ReflectionPerf),
+                typeof(System.Uri),
+                typeof(double),
+                typeof(ManagedObjectWithInterfaces),
+                typeof(ManagedComposableObjectWithInterfaces),
+                typeof(ClassWithMarshalingRoutines),
+                typeof(int),
+                typeof(ReflectionPerf),
+                typeof(System.Uri),
+                typeof(double),
+                typeof(ManagedObjectWithInterfaces),
+                typeof(BlittableStruct),
+                typeof(NonBlittable),
+                typeof(ClassWithMarshalingRoutines),
+            };
+        }
+
+        private sealed class TypeCache
+        {
+            private readonly Type type;
+            private bool isLengthGreaterThan4Set;
+            private bool lengthGreaterThan4;
+            private bool isBaseTypeSet;
+            private Type baseType;
+            private bool isLengthLessThan4;
+            private bool lengthLessThan4;
+            private bool isName;
+            private string name;
+
+            public TypeCache(Type type)
+            {
+                this.type = type;
+            }
+
+            private static bool MakeLengthGreaterThan4(Type type)
+            {
+                return type.FullName.Length > 4;
+            }
+
+            private bool SetLengthGreaterThan4()
+            {
+                lengthGreaterThan4 = MakeLengthGreaterThan4(type);
+                isLengthGreaterThan4Set = true;
+                return lengthGreaterThan4;
+            }
+
+            public bool LengthGreaterThan4 => isLengthGreaterThan4Set ? lengthGreaterThan4 : SetLengthGreaterThan4();
+
+            private static Type MakeBaseType(Type type)
+            {
+                return type.BaseType;
+            }
+
+            private Type SetBaseType()
+            {
+                baseType = MakeBaseType(type);
+                isBaseTypeSet = true;
+                return baseType;
+            }
+
+            public Type BaseType => isBaseTypeSet ? baseType : SetBaseType();
+
+            private static bool MakeLengthLessThan4(Type type)
+            {
+                return type.FullName.Length < 4;
+            }
+
+            private bool SetLengthLessThan4()
+            {
+                lengthLessThan4 = MakeLengthLessThan4(type);
+                isLengthLessThan4 = true;
+                return lengthLessThan4;
+            }
+
+            public bool LengthLessThan4 => isLengthLessThan4 ? lengthLessThan4 : SetLengthLessThan4();
+
+            private static string MakeName(Type type)
+            {
+                return type.Name;
+            }
+
+            private string SetName()
+            {
+                name = MakeName(type);
+                isName = true;
+                return name;
+            }
+
+            public string Name => isName ? name : SetName();
+        }
+
+        private sealed class TypeCache2
+        {
+            private readonly Type type;
+            private volatile bool isLengthGreaterThan4Set;
+            private volatile bool lengthGreaterThan4;
+            private volatile bool isBaseTypeSet;
+            private volatile Type baseType;
+            private volatile bool isLengthLessThan4;
+            private volatile bool lengthLessThan4;
+            private volatile bool isName;
+            private volatile string name;
+
+            public TypeCache2(Type type)
+            {
+                this.type = type;
+            }
+
+            private static bool MakeLengthGreaterThan4(Type type)
+            {
+                return type.FullName.Length > 4;
+            }
+
+            private bool SetLengthGreaterThan4()
+            {
+                lengthGreaterThan4 = MakeLengthGreaterThan4(type);
+                isLengthGreaterThan4Set = true;
+                return lengthGreaterThan4;
+            }
+
+            public bool LengthGreaterThan4 => isLengthGreaterThan4Set ? lengthGreaterThan4 : SetLengthGreaterThan4();
+
+            private static Type MakeBaseType(Type type)
+            {
+                return type.BaseType;
+            }
+
+            private Type SetBaseType()
+            {
+                baseType = MakeBaseType(type);
+                isBaseTypeSet = true;
+                return baseType;
+            }
+
+            public Type BaseType => isBaseTypeSet ? baseType : SetBaseType();
+
+            private static bool MakeLengthLessThan4(Type type)
+            {
+                return type.FullName.Length < 4;
+            }
+
+            private bool SetLengthLessThan4()
+            {
+                lengthLessThan4 = MakeLengthLessThan4(type);
+                isLengthLessThan4 = true;
+                return lengthLessThan4;
+            }
+
+            public bool LengthLessThan4 => isLengthLessThan4 ? lengthLessThan4 : SetLengthLessThan4();
+
+            private static string MakeName(Type type)
+            {
+                return type.Name;
+            }
+
+            private string SetName()
+            {
+                name = MakeName(type);
+                isName = true;
+                return name;
+            }
+
+            public string Name => isName ? name : SetName();
+        }
+
+        [Benchmark]
+        public object Test1()
+        {
+            Type[] types = GetTypes();
+            ConcurrentDictionary<Type, bool> test1 = new();
+            ConcurrentDictionary<Type, Type> test2 = new();
+            ConcurrentDictionary<Type, bool> test3 = new();
+            ConcurrentDictionary<Type, string> test4 = new();
+            foreach (Type type in types)
+            {
+                _ = test1.GetOrAdd(type, (type) => type.FullName.Length > 4);
+                _ = test2.GetOrAdd(type, (type) => type.BaseType);
+                _ = test3.GetOrAdd(type, (type) => type.FullName.Length < 4);
+                _ = test4.GetOrAdd(type, (type) => type.Name);
+            }
+            return null;
+        }
+
+        [Benchmark]
+        public object Test2()
+        {
+            Type[] types = GetTypes();
+            ConcurrentDictionary<Type, TypeCache> test1 = new();
+            foreach (Type type in types)
+            {
+                var typeCache = test1.GetOrAdd(type, (type) => new(type));
+                _ = typeCache.LengthGreaterThan4;
+                _ = typeCache.BaseType;
+                _ = typeCache.LengthLessThan4;
+                _ = typeCache.Name;
+            }
+            return null;
+        }
+
+        [Benchmark]
+        public object Test3()
+        {
+            Type[] types = GetTypes();
+            ConcurrentDictionary<Type, TypeCache2> test1 = new();
+            foreach (Type type in types)
+            {
+                var typeCache = test1.GetOrAdd(type, (type) => new(type));
+                _ = typeCache.LengthGreaterThan4;
+                _ = typeCache.BaseType;
+                _ = typeCache.LengthLessThan4;
+                _ = typeCache.Name;
+            }
+            return null;
+        }
+
+        [Benchmark]
+        public object Test33()
+        {
+            Type[] types = GetTypes();
+            ConcurrentDictionary<Type, TypeCache2> test1 = new();
+            foreach (Type type in types)
+            {
+                _ = test1.GetOrAdd(type, (type) => new(type)).LengthGreaterThan4;
+                _ = test1.GetOrAdd(type, (type) => new(type)).BaseType;
+                _ = test1.GetOrAdd(type, (type) => new(type)).LengthLessThan4;
+                _ = test1.GetOrAdd(type, (type) => new(type)).Name;
+            }
+            return null;
+        }
+
+        [Benchmark]
+        public object Test4()
+        {
+            Type[] types = GetTypes();
+            foreach (Type type in types)
+            {
+                _ = ttest1.GetOrAdd(type, (type) => type.FullName.Length > 4);
+                _ = ttest2.GetOrAdd(type, (type) => type.BaseType);
+                _ = ttest3.GetOrAdd(type, (type) => type.FullName.Length < 4);
+                _ = ttest4.GetOrAdd(type, (type) => type.Name);
+            }
+            return null;
+        }
+
+        [Benchmark]
+        public object Test5()
+        {
+            Type[] types = GetTypes();
+            foreach (Type type in types)
+            {
+                var typeCache = ttest5.GetOrAdd(type, (type) => new(type));
+                _ = typeCache.LengthGreaterThan4;
+                _ = typeCache.BaseType;
+                _ = typeCache.LengthLessThan4;
+                _ = typeCache.Name;
+            }
+            return null;
+        }
+
+        [Benchmark]
+        public object Test6()
+        {
+            Type[] types = GetTypes();
+            foreach (Type type in types)
+            {
+                var typeCache = ttest6.GetOrAdd(type, (type) => new(type));
+                _ = typeCache.LengthGreaterThan4;
+                _ = typeCache.BaseType;
+                _ = typeCache.LengthLessThan4;
+                _ = typeCache.Name;
+            }
+            return null;
+        }
+        [Benchmark]
+        public object Test66()
+        {
+            Type[] types = GetTypes();
+            foreach (Type type in types)
+            {
+                _ = ttest6.GetOrAdd(type, (type) => new(type)).LengthGreaterThan4;
+                _ = ttest6.GetOrAdd(type, (type) => new(type)).BaseType;
+                _ = ttest6.GetOrAdd(type, (type) => new(type)).LengthLessThan4;
+                _ = ttest6.GetOrAdd(type, (type) => new(type)).Name;
+            }
+            return null;
+        }
+
+        [Benchmark]
+        public object Test7()
+        {
+            Type[] types = GetTypes();
+            foreach (Type type in types)
+            {
+                _ = ttest7.GetOrAdd(type, (type) => type.FullName.Length > 4);
+                _ = ttest8.GetOrAdd(type, (type) => type.BaseType);
+                _ = ttest9.GetOrAdd(type, (type) => type.FullName.Length < 4);
+                _ = ttest10.GetOrAdd(type, (type) => type.Name);
+            }
+
+            ttest7.Clear();
+            ttest8.Clear();
+            ttest9.Clear();
+            ttest10.Clear();
+            return null;
+        }
+
+        [Benchmark]
+        public object Test8()
+        {
+            Type[] types = GetTypes();
+            foreach (Type type in types)
+            {
+                var typeCache = ttest11.GetOrAdd(type, (type) => new(type));
+                _ = typeCache.LengthGreaterThan4;
+                _ = typeCache.BaseType;
+                _ = typeCache.LengthLessThan4;
+                _ = typeCache.Name;
+            }
+
+            ttest11.Clear();
+            return null;
+        }
+
+        [Benchmark]
+        public object Test9()
+        {
+            Type[] types = GetTypes();
+            foreach (Type type in types)
+            {
+                var typeCache = ttest12.GetOrAdd(type, (type) => new(type));
+                _ = typeCache.LengthGreaterThan4;
+                _ = typeCache.BaseType;
+                _ = typeCache.LengthLessThan4;
+                _ = typeCache.Name;
+            }
+
+            ttest12.Clear();
+            return null;
+        }
+
+        [Benchmark]
+        public object Test99()
+        {
+            Type[] types = GetTypes();
+            foreach (Type type in types)
+            {
+                _ = ttest12.GetOrAdd(type, (type) => new(type)).LengthGreaterThan4;
+                _ = ttest12.GetOrAdd(type, (type) => new(type)).BaseType;
+                _ = ttest12.GetOrAdd(type, (type) => new(type)).LengthLessThan4;
+                _ = ttest12.GetOrAdd(type, (type) => new(type)).Name;
+            }
+
+            ttest12.Clear();
+            return null;
+        }
+
     }
 }
