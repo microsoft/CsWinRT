65e791255a0ffa8bf4ac3d0bfa9dea46235f76bd
Manodasan Wignarajah <mawign@microsoft.com>
2022-02-07 20:09:56 -0800

    Update variables

--- src/build.cmd
+++ src/build.cmd
@@ -2,7 +2,7 @@
 if /i "%cswinrt_echo%" == "on" @echo on
 
 set CsWinRTBuildNetSDKVersion=6.0.100-rc.2.21505.57
-set CsWinRTNet5SdkVersion=5.0.402
+set CsWinRTNet5SdkVersion=5.0.404
 set this_dir=%~dp0
 
 :dotnet
