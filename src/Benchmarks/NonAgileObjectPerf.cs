9782d5f7317fd119e78903d8f32c9e34ab681a0d
Manodasan Wignarajah <mawign@microsoft.com>
2022-02-09 00:47:01 -0800

    Add benchark for non agile objects

---
+++ src/Benchmarks/NonAgileObjectPerf.cs
@@ -0,0 +1,61 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Benchmarks
+{
+    [MemoryDiagnoser]
+    public class NonAgileObjectPerf
+    {
+        AutoResetEvent createObject;
+        AutoResetEvent exitThread;
+        AutoResetEvent objectCreated;
+        Thread staThread;
+        private Windows.UI.Popups.PopupMenu nonAgileObject;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            createObject = new AutoResetEvent(false);
+            exitThread = new AutoResetEvent(false);
+            objectCreated = new AutoResetEvent(false);
+            staThread = new Thread(new ThreadStart(ObjectAllocationLoop));
+            staThread.SetApartmentState(ApartmentState.STA);
+            staThread.Start();
+        }
+
+        [GlobalCleanup]
+        public void Cleanup()
+        {
+            exitThread.Set();
+            createObject.Set();
+        }
+
+        private void ObjectAllocationLoop()
+        {
+            while (createObject.WaitOne() && !exitThread.WaitOne(1))
+            {
+                createObject.Reset();
+                nonAgileObject = new Windows.UI.Popups.PopupMenu();
+                _ = nonAgileObject.Commands.Count;
+                objectCreated.Set();
+            }
+        }
+
+        private int CallObject()
+        {
+            return nonAgileObject.Commands.Count;
+        }
+
+        [Benchmark]
+        public void ConstructAndQueryNonAgileObject()
+        {
+            createObject.Set();
+            objectCreated.WaitOne();
+            CallObject();
+            objectCreated.Reset();
+        }
+    }
+}
