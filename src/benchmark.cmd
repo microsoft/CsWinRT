9b5b463ba695da5cfefc85cc2891a83d6fa46d7e
Manodasan Wignarajah <mawign@microsoft.com>
2022-02-14 20:29:54 -0800

    Attempt fix

--- src/benchmark.cmd
+++ src/benchmark.cmd
@@ -1,2 +1,3 @@
+msbuild TestWinRT\Test.sln -t:restore -t:build /p:platform=x64 /p:configuration=release /p:solutiondir=%~dp0 /p:IsDotnetBuild=false
 msbuild Benchmarks\Benchmarks.csproj -t:restore -t:build /p:platform=x64 /p:configuration=release /p:solutiondir=%~dp0 /p:IsDotnetBuild=false
 dotnet %~dp0Benchmarks\bin\x64\Release\netcoreapp2.0\Benchmarks.dll -filter * --runtimes netcoreapp2.0 netcoreapp3.1 netcoreapp5.0
