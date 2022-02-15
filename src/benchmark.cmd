5d57cf567eb65bd75756d3124c624a8c34ce7342
Manodasan Wignarajah <mawign@microsoft.com>
2022-02-14 21:25:48 -0800

    Restore packages

--- src/benchmark.cmd
+++ src/benchmark.cmd
@@ -1,3 +1,4 @@
-msbuild TestWinRT\Test.sln -t:restore -t:build /p:platform=x64 /p:configuration=release /p:solutiondir=%~dp0 /p:IsDotnetBuild=false
+nuget restore TestWinRT\Test.sln
+nuget restore cswinrt.sln
 msbuild Benchmarks\Benchmarks.csproj -t:restore -t:build /p:platform=x64 /p:configuration=release /p:solutiondir=%~dp0 /p:IsDotnetBuild=false
 dotnet %~dp0Benchmarks\bin\x64\Release\netcoreapp2.0\Benchmarks.dll -filter * --runtimes netcoreapp2.0 netcoreapp3.1 netcoreapp5.0
