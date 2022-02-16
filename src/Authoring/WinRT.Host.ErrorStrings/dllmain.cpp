63284184b37bb689206df1ded892e4428b120a54
Joshua Larkin <70237359+j0shuams@users.noreply.github.com>
2022-02-16 10:33:40 -0800

    simplify"

--- src/Authoring/WinRT.Host.ErrorStrings/dllmain.cpp
+++ src/Authoring/WinRT.Host.ErrorStrings/dllmain.cpp
@@ -1,6 +1,7 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "pch.h"
 
+__declspec(dllexport)
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
                        LPVOID lpReserved
--- src/Authoring/WinRT.Host.ErrorStrings/resource.h
+++ src/Authoring/WinRT.Host.ErrorStrings/resource.h
@@ -1,12 +1,47 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by WinRT.Host.ErrorStrings1.rc
+// Used by WinRT.Host.ErrorStrings.rc
+//
+#define InvalidArgFailure               106
+#define CoreHostLibLoadFailure          107
+#define CoreHostLibMissingFailure       108
+#define CoreHostEntryPointFailure       109
+#define CoreHostCurHostFindFailure      110
+#define CoreClrResolveFailure           111
+#define CoreClrBindFailure              112
+#define CoreClrInitFailure              113
+#define CoreClrExeFailure               114
+#define ResolverInitFailure             115
+#define ResolverResolveFailure          116
+#define LibHostCurExeFindFailure        117
+#define LibHostInitFailure              118
+#define LibHostSdkFindFailure           119
+#define LibHostInvalidArgs              120
+#define InvalidConfigFile               121
+#define AppArgNotRunnable               122
+#define AppHostExeNotBoundFailure       123
+#define FrameworkMissingFailure         124
+#define HostApiFailed                   125
+#define HostApiBufferTooSmall           126
+#define LibHostUnknownCommand           127
+#define LibHostAppRootFindFailure       128
+#define SdkResolverResolveFailure       129
+#define FrameworkCompatFailure          130
+#define FrameworkCompatRetry            131
+#define BundleExtractionFailure         132
+#define BundleExtractionIOError         133
+#define LibHostDuplicateProperty        134
+#define HostApiUnsupportedVersion       135
+#define HostInvalidState                136
+#define HostPropertyNotFound            137
+#define CoreHostIncompatibleConfig      138
+#define HostApiUnsupportedScenario      139
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_RESOURCE_VALUE        107
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
