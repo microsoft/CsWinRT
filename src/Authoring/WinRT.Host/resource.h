1b4465499ed3b467dc74b3eda67cb9366b02dada
Joshua Larkin <70237359+j0shuams@users.noreply.github.com>
2022-03-08 13:22:51 -0800

    winrthost with mui

---
+++ src/Authoring/WinRT.Host/resource.h
@@ -0,0 +1,49 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by WinRTHostErrorStrings.rc
+//
+#define InvalidArgFailureStr            101
+#define CoreHostLibLoadFailureStr       102
+#define CoreHostLibMissingFailureStr    103
+#define CoreHostEntryPointFailureStr    104
+#define CoreHostCurHostFindFailureStr   105
+#define CoreClrResolveFailureStr        106
+#define CoreClrBindFailureStr           107
+#define CoreClrInitFailureStr           108
+#define CoreClrExeFailureStr            109
+#define ResolverInitFailureStr          110
+#define ResolverResolveFailureStr       111
+#define LibHostCurExeFindFailureStr     112
+#define LibHostInitFailureStr           113
+#define LibHostSdkFindFailureStr        114
+#define LibHostInvalidArgsStr           115
+#define InvalidConfigFileStr            116
+#define AppArgNotRunnableStr            117
+#define AppHostExeNotBoundFailureStr    118
+#define FrameworkMissingFailureStr      119
+#define HostApiFailedStr                120
+#define HostApiBufferTooSmallStr        121
+#define LibHostUnknownCommandStr        122
+#define LibHostAppRootFindFailureStr    123
+#define SdkResolverResolveFailureStr    124
+#define FrameworkCompatFailureStr       125
+#define FrameworkCompatRetryStr         126
+#define BundleExtractionFailureStr      127
+#define BundleExtractionIOErrorStr      128
+#define LibHostDuplicatePropertyStr     129
+#define HostApiUnsupportedVersionStr    130
+#define HostInvalidStateStr             131
+#define HostPropertyNotFoundStr         132
+#define CoreHostIncompatibleConfigStr   133
+#define HostApiUnsupportedScenarioStr   134
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
