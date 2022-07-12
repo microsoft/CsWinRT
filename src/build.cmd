917d69865386135acbd37bac9d266adfdae80dac
Manodasan Wignarajah <mawign@microsoft.com>
2022-07-12 13:25:27 -0700

    Fix build break

--- src/build.cmd
+++ src/build.cmd
@@ -140,8 +140,8 @@ if ErrorLevel 1 (
   exit /b !ErrorLevel!
 )
 
-if not %cswinrt_platform%==arm and not %cswinrt_platform%==arm64 (
-  echo Publishing function tests for %cswinrt_platform% %cswinrt_configuration%
+if "%cswinrt_platform%" NEQ "arm" and "%cswinrt_platform%" NEQ "arm64" (
+  echo Publishing functional tests for %cswinrt_platform% %cswinrt_configuration%
   for %%a in (%cswinrt_functional_tests%) do (
     echo Publishing %%a
     call :exec %msbuild_path%msbuild.exe /t:restore /t:publish %cswinrt_build_params% /p:platform=%cswinrt_platform%;configuration=%cswinrt_configuration%;VersionNumber=%cswinrt_version_number%;VersionString=%cswinrt_version_string%;AssemblyVersionNumber=%cswinrt_assembly_version%;GenerateTestProjection=true;BaselineAllAPICompatError=%cswinrt_baseline_breaking_compat_errors%;BaselineAllMatchingRefApiCompatError=%cswinrt_baseline_assembly_version_compat_errors% /p:solutiondir=%this_dir% %this_dir%Tests\FunctionalTests\%%a\%%a.csproj
@@ -247,6 +247,8 @@ for %%a in (%cswinrt_functional_tests%) do (
     exit /b !ErrorLevel!
   )
 )
+
+if "%cswinrt_label%"=="functionaltest" goto :eof
 
 :package
 rem We set the properties of the CsWinRT.nuspec here, and pass them as the -Properties option when we call `nuget pack`
