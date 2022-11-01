342b186afb5cececb5ad6528bcc85aed12ee5908
Scott Jones <sjones@microsoft.com>
2022-10-31 18:00:13 -0700

    Remove all error reporting from projection and to client code like microsoft.ui.xaml.dll

--- src/cswinrt/code_writers.h
+++ src/cswinrt/code_writers.h
@@ -5257,7 +5257,7 @@ try
 }
 catch (Exception __exception__)
 {
-global::WinRT.ExceptionHelpers.ReportUnhandledError(__exception__);
+global::WinRT.ExceptionHelpers.SetErrorInfo(__exception__);
 return global::WinRT.ExceptionHelpers.GetHRForException(__exception__);
 }
 return 0;)",
@@ -5461,7 +5461,6 @@ return 0;
 }
 catch (Exception __ex)
 {
-global::WinRT.ExceptionHelpers.ReportUnhandledError(__ex);
 return __ex.HResult;
 }
 })",
@@ -5493,7 +5492,6 @@ return 0;
 }
 catch (Exception __ex)
 {
-global::WinRT.ExceptionHelpers.ReportUnhandledError(__ex);
 return __ex.HResult;
 }
 })",
