2005e9e6077d5816528480d4732dced99842971d
Manodasan Wignarajah <mawign@microsoft.com>
2023-09-08 22:41:37 -0700

    Initial changes to make vtable generation trimming safe for AOT (#1352)
    * Add source generator to generate vtable interfaces.
    
    * Update .NET SDK version
    
    * Updates to vtable source generator
    
    * Minor change
    
    * Support for custom mapped types and covariance
    
    * Fix issues with vtable generation
    
    * Remove attributes that seem to have not worked.
    
    * Fix trimming issue with WindowsRuntimeBuffer
    
    * Fix API compat check
    
    * Update .NET SDK version
    
    * PR feedback
    
    * Fix x86 build due to failing to load source generator.
    
    * Update src/Authoring/WinRT.SourceGenerator/AotOptimizer.cs
    
    Co-authored-by: Sergio Pedri <sergio0694@live.com>
    
    ---------
    
    Co-authored-by: Sergio Pedri <sergio0694@live.com>

---
+++ src/Authoring/WinRT.SourceGenerator/AotOptimizer.cs
@@ -0,0 +1,205 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using WinRT.SourceGenerator;
+
+namespace Generator
+{
+    [Generator]
+    public class WinRTAotSourceGenerator : IIncrementalGenerator
+    {
+        public void Initialize(IncrementalGeneratorInitializationContext context)
+        {
+            var vtableAttributesToAdd = context.SyntaxProvider.CreateSyntaxProvider(
+                static (n, _) => NeedVtableAttribute(n),
+                static (n, _) => GetVtableAttributeToAdd(n));
+
+            context.RegisterImplementationSourceOutput(vtableAttributesToAdd, GenerateVtableAttributes);
+        }
+
+        // Restrict to non-projected classes which can be instantiated
+        // and are partial allowing to add attributes.
+        private static bool NeedVtableAttribute(SyntaxNode node)
+        {
+            return node is ClassDeclarationSyntax declaration &&
+                !declaration.Modifiers.Any(m => m.IsKind(SyntaxKind.StaticKeyword) || m.IsKind(SyntaxKind.AbstractKeyword)) &&
+                declaration.Modifiers.Any(m => m.IsKind(SyntaxKind.PartialKeyword)) &&
+                !GeneratorHelper.IsWinRTType(declaration); // Making sure it isn't an RCW we are projecting.
+        }
+
+        private static VtableAttribute GetVtableAttributeToAdd(GeneratorSyntaxContext context)
+        {
+            var symbol = context.SemanticModel.GetDeclaredSymbol(context.Node as ClassDeclarationSyntax);
+
+            HashSet<string> interfacesToAddToVtable = new();
+            foreach (var iface in symbol.Interfaces)
+            {
+                AddInterfaceAndCompatibleInterfacesToVtable(iface);
+
+                foreach (var baseIface in iface.AllInterfaces)
+                {
+                    AddInterfaceAndCompatibleInterfacesToVtable(baseIface);
+                }
+            }
+
+            bool hasWinRTExposedBaseType = false;
+            var baseType = symbol.BaseType;
+            while (baseType != null)
+            {
+                if (GeneratorHelper.IsWinRTType(baseType) ||
+                    baseType.Interfaces.Any(GeneratorHelper.IsWinRTType))
+                {
+                    hasWinRTExposedBaseType = true;
+                    break;
+                }
+
+                baseType = baseType.BaseType;
+            }
+
+            return new VtableAttribute(
+                symbol.ContainingNamespace.ToDisplayString(),
+                symbol.ContainingNamespace.IsGlobalNamespace,
+                symbol.Name,
+                interfacesToAddToVtable.ToImmutableArray(),
+                hasWinRTExposedBaseType);
+
+            void AddInterfaceAndCompatibleInterfacesToVtable(INamedTypeSymbol iface)
+            {
+                if (GeneratorHelper.IsWinRTType(iface))
+                {
+                    interfacesToAddToVtable.Add(iface.ToDisplayString());
+                }
+
+                if (iface.IsGenericType && TryGetCompatibleWindowsRuntimeTypesForVariantType(iface, null, out var compatibleIfaces))
+                {
+                    foreach (var compatibleIface in compatibleIfaces)
+                    {
+                        interfacesToAddToVtable.Add(compatibleIface.ToDisplayString());
+                    }
+                }
+            }
+        }
+
+        private static bool TryGetCompatibleWindowsRuntimeTypesForVariantType(INamedTypeSymbol type, Stack<INamedTypeSymbol> typeStack, out IList<INamedTypeSymbol> compatibleTypes)
+        {
+            compatibleTypes = null;
+
+            // Out of all the C# interfaces which are valid WinRT interfaces and
+            // support covariance, they all only have one generic parameter,
+            // so scoping to only handle that.
+            if (type is not { IsGenericType: true, TypeParameters: [{ Variance: VarianceKind.Out, IsValueType: false }] })
+            {
+                return false;
+            }
+
+            var definition = type.OriginalDefinition;
+            if (!GeneratorHelper.IsWinRTType(definition))
+            {
+                return false;
+            }
+
+            if (typeStack == null)
+            {
+                typeStack = new Stack<INamedTypeSymbol>();
+            }
+            else
+            {
+                if (typeStack.Contains(type))
+                {
+                    return false;
+                }
+            }
+            typeStack.Push(type);
+
+            HashSet<ITypeSymbol> compatibleTypesForGeneric = new(SymbolEqualityComparer.Default);
+
+            if (GeneratorHelper.IsWinRTType(type.TypeArguments[0]))
+            {
+                compatibleTypesForGeneric.Add(type.TypeArguments[0]);
+            }
+
+            foreach (var iface in type.TypeArguments[0].AllInterfaces)
+            {
+                if (GeneratorHelper.IsWinRTType(iface))
+                {
+                    compatibleTypesForGeneric.Add(iface);
+                }
+
+                if (iface.IsGenericType
+                    && TryGetCompatibleWindowsRuntimeTypesForVariantType(iface, typeStack, out var compatibleIfaces))
+                {
+                    compatibleTypesForGeneric.UnionWith(compatibleIfaces);
+                }
+            }
+
+            var baseType = type.TypeArguments[0].BaseType;
+            while (baseType != null)
+            {
+                if (GeneratorHelper.IsWinRTType(baseType))
+                {
+                    compatibleTypesForGeneric.Add(baseType);
+                }
+                baseType = baseType.BaseType;
+            }
+
+            typeStack.Pop();
+
+            compatibleTypes = new List<INamedTypeSymbol>(compatibleTypesForGeneric.Count);
+            foreach (var compatibleType in compatibleTypesForGeneric)
+            {
+                compatibleTypes.Add(definition.Construct(compatibleType));
+            }
+
+            return true;
+        }
+
+        private static void GenerateVtableAttributes(SourceProductionContext sourceProductionContext, VtableAttribute vtableAttribute)
+        {
+            // Even if no interfaces are on the vtable, as long as the base type is a WinRT type,
+            // we want to put the attribute as the attribute is the opt-in for the source generated
+            // vtable generation.
+            if (vtableAttribute.Interfaces.Any() || vtableAttribute.HasWinRTExposedBaseType)
+            {
+                var vtableEntries = string.Join(", ", vtableAttribute.Interfaces.Select((@interface) => $@"typeof({@interface})"));
+                
+                StringBuilder source = new();
+                if (!vtableAttribute.IsGlobalNamespace)
+                {
+                    source.Append($$"""
+                        namespace {{vtableAttribute.Namespace}}
+                        {
+                        """);
+                }
+
+                source.Append($$"""
+                    [global::WinRT.WinRTExposedType({{vtableEntries}})]
+                    partial class {{vtableAttribute.ClassName}}
+                    {
+                    }
+                    """);
+
+                if (!vtableAttribute.IsGlobalNamespace)
+                {
+                    source.Append($@"}}");
+                }
+
+                string prefix = vtableAttribute.IsGlobalNamespace ? "" : $"{vtableAttribute.Namespace}.";
+                sourceProductionContext.AddSource($"{prefix}{vtableAttribute.ClassName}.WinRTVtable.g.cs", source.ToString());
+            }
+        }
+    }
+
+    internal sealed record VtableAttribute(
+        string Namespace,
+        bool IsGlobalNamespace,
+        string ClassName,
+        EquatableArray<string> Interfaces,
+        bool HasWinRTExposedBaseType);
+}
--- src/Authoring/WinRT.SourceGenerator/Helper.cs
+++ src/Authoring/WinRT.SourceGenerator/Helper.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -121,5 +122,154 @@ namespace Generator
             }
             return Path.Combine(context.GetGeneratedFilesDir(), fileName + ".winmd");
         }
+    }
+
+    static class GeneratorHelper
+    {
+        private static bool IsFundamentalType(ISymbol type)
+        {
+            if (type is INamedTypeSymbol namedTypeSymbol)
+            {
+                switch (namedTypeSymbol.SpecialType)
+                {
+                    case SpecialType.System_Boolean:
+                    case SpecialType.System_String:
+                    case SpecialType.System_Single:
+                    case SpecialType.System_Double:
+                    case SpecialType.System_UInt16:
+                    case SpecialType.System_UInt32:
+                    case SpecialType.System_UInt64:
+                    case SpecialType.System_Int16:
+                    case SpecialType.System_Int32:
+                    case SpecialType.System_Int64:
+                    case SpecialType.System_Char:
+                    case SpecialType.System_Byte:
+                    case SpecialType.System_Object:
+                        return true; 
+                }
+            }
+
+            return type.ToDisplayString() == "System.Guid";
+        }
+
+        public static bool IsWinRTType(ISymbol type)
+        {
+            bool isProjectedType = type.GetAttributes().
+                Any(attribute => string.CompareOrdinal(attribute.AttributeClass.Name, "WindowsRuntimeTypeAttribute") == 0) ||
+                IsFundamentalType(type);
+
+            if (!isProjectedType & type.ContainingNamespace != null)
+            {
+                isProjectedType = MappedCSharpTypes.ContainsKey(string.Join(".", type.ContainingNamespace.ToDisplayString(), type.MetadataName));
+            }
+
+            // Ensure all generic parameters are WinRT types.
+            if (isProjectedType && type is INamedTypeSymbol namedType && namedType.IsGenericType && !namedType.IsDefinition)
+            {
+                isProjectedType = namedType.TypeArguments.All(IsWinRTType);
+            }
+            return isProjectedType;
+        }
+
+        public static bool IsWinRTType(MemberDeclarationSyntax node)
+        {
+            bool isProjectedType = node.AttributeLists.SelectMany(list => list.Attributes).
+                Any(attribute => string.CompareOrdinal(attribute.Name.NormalizeWhitespace().ToFullString(), "WindowsRuntimeTypeAttribute") == 0);
+            return isProjectedType;
+        }
+
+        public readonly struct MappedType
+        {
+            private readonly string @namespace;
+            private readonly string name;
+            private readonly string assembly;
+            private readonly bool isSystemType;
+            private readonly bool isValueType;
+            private readonly Func<ISymbol, (string, string, string, bool, bool)> multipleMappingFunc;
+
+            public MappedType(string @namespace, string name, string assembly, bool isValueType = false)
+            {
+                this.@namespace = @namespace;
+                this.name = name;
+                this.assembly = assembly;
+                isSystemType = string.CompareOrdinal(this.assembly, "mscorlib") == 0;
+                this.isValueType = isValueType;
+                multipleMappingFunc = null;
+            }
+
+            public MappedType(Func<ISymbol, (string, string, string, bool, bool)> multipleMappingFunc)
+            {
+                @namespace = null;
+                name = null;
+                assembly = null;
+                isSystemType = false;
+                isValueType = false;
+                this.multipleMappingFunc = multipleMappingFunc;
+            }
+
+            public (string, string, string, bool, bool) GetMapping(ISymbol containingType = null)
+            {
+                return multipleMappingFunc != null ?
+                    multipleMappingFunc(containingType) : (@namespace, name, assembly, isSystemType, isValueType);
+            }
+        }
+
+        // Based on whether System.Type is used in an attribute declaration or elsewhere, we need to choose the correct custom mapping
+        // as attributes don't use the TypeName mapping.
+        internal static (string, string, string, bool, bool) GetSystemTypeCustomMapping(ISymbol containingSymbol)
+        {
+            bool isDefinedInAttribute =
+                containingSymbol != null &&
+                    string.CompareOrdinal((containingSymbol as INamedTypeSymbol).BaseType?.ToString(), "System.Attribute") == 0;
+            return isDefinedInAttribute ?
+                ("System", "Type", "mscorlib", true, false) :
+                ("Windows.UI.Xaml.Interop", "TypeName", "Windows.Foundation.UniversalApiContract", false, true);
+        }
+
+        // This should be in sync with the reverse mapping from WinRT.Runtime/Projections.cs and cswinrt/helpers.h.
+        public static readonly Dictionary<string, MappedType> MappedCSharpTypes = new(StringComparer.Ordinal)
+        {
+            { "System.DateTimeOffset", new MappedType("Windows.Foundation", "DateTime", "Windows.Foundation.FoundationContract", true) },
+            { "System.Exception", new MappedType("Windows.Foundation", "HResult", "Windows.Foundation.FoundationContract", true) },
+            { "System.EventHandler`1", new MappedType("Windows.Foundation", "EventHandler`1", "Windows.Foundation.FoundationContract", true) },
+            { "System.FlagsAttribute", new MappedType("System", "FlagsAttribute", "mscorlib" ) },
+            { "System.IDisposable", new MappedType("Windows.Foundation", "IClosable", "Windows.Foundation.FoundationContract") },
+            { "System.IServiceProvider", new MappedType("Microsoft.UI.Xaml", "IXamlServiceProvider", "Microsoft.UI") },
+            { "System.Nullable`1", new MappedType("Windows.Foundation", "IReference`1", "Windows.Foundation.FoundationContract" ) },
+            { "System.Object", new MappedType("System", "Object", "mscorlib" ) },
+            { "System.TimeSpan", new MappedType("Windows.Foundation", "TimeSpan", "Windows.Foundation.FoundationContract", true) },
+            { "System.Uri", new MappedType("Windows.Foundation", "Uri", "Windows.Foundation.FoundationContract") },
+            { "System.ComponentModel.DataErrorsChangedEventArgs", new MappedType("Microsoft.UI.Xaml.Data", "DataErrorsChangedEventArgs", "Microsoft.UI") },
+            { "System.ComponentModel.INotifyDataErrorInfo", new MappedType("Microsoft.UI.Xaml.Data", "INotifyDataErrorInfo", "Microsoft.UI") },
+            { "System.ComponentModel.INotifyPropertyChanged", new MappedType("Microsoft.UI.Xaml.Data", "INotifyPropertyChanged", "Microsoft.UI") },
+            { "System.ComponentModel.PropertyChangedEventArgs", new MappedType("Microsoft.UI.Xaml.Data", "PropertyChangedEventArgs", "Microsoft.UI") },
+            { "System.ComponentModel.PropertyChangedEventHandler", new MappedType("Microsoft.UI.Xaml.Data", "PropertyChangedEventHandler", "Microsoft.UI") },
+            { "System.Windows.Input.ICommand", new MappedType("Microsoft.UI.Xaml.Input", "ICommand", "Microsoft.UI") },
+            { "System.Collections.IEnumerable", new MappedType("Microsoft.UI.Xaml.Interop", "IBindableIterable", "Microsoft.UI") },
+            { "System.Collections.IList", new MappedType("Microsoft.UI.Xaml.Interop", "IBindableVector", "Microsoft.UI") },
+            { "System.Collections.Specialized.INotifyCollectionChanged", new MappedType("Microsoft.UI.Xaml.Interop", "INotifyCollectionChanged", "Microsoft.UI") },
+            { "System.Collections.Specialized.NotifyCollectionChangedAction", new MappedType("Microsoft.UI.Xaml.Interop", "NotifyCollectionChangedAction", "Microsoft.UI") },
+            { "System.Collections.Specialized.NotifyCollectionChangedEventArgs", new MappedType("Microsoft.UI.Xaml.Interop", "NotifyCollectionChangedEventArgs", "Microsoft.UI") },
+            { "System.Collections.Specialized.NotifyCollectionChangedEventHandler", new MappedType("Microsoft.UI.Xaml.Interop", "NotifyCollectionChangedEventHandler", "Microsoft.UI") },
+            { "WinRT.EventRegistrationToken", new MappedType("Windows.Foundation", "EventRegistrationToken", "Windows.Foundation.FoundationContract", true) },
+            { "System.AttributeTargets", new MappedType("Windows.Foundation.Metadata", "AttributeTargets", "Windows.Foundation.FoundationContract", true) },
+            { "System.AttributeUsageAttribute", new MappedType("Windows.Foundation.Metadata", "AttributeUsageAttribute", "Windows.Foundation.FoundationContract") },
+            { "System.Numerics.Matrix3x2", new MappedType("Windows.Foundation.Numerics", "Matrix3x2", "Windows.Foundation.FoundationContract", true) },
+            { "System.Numerics.Matrix4x4", new MappedType("Windows.Foundation.Numerics", "Matrix4x4", "Windows.Foundation.FoundationContract", true) },
+            { "System.Numerics.Plane", new MappedType("Windows.Foundation.Numerics", "Plane", "Windows.Foundation.FoundationContract", true) },
+            { "System.Numerics.Quaternion", new MappedType("Windows.Foundation.Numerics", "Quaternion", "Windows.Foundation.FoundationContract", true) },
+            { "System.Numerics.Vector2", new MappedType("Windows.Foundation.Numerics", "Vector2", "Windows.Foundation.FoundationContract", true) },
+            { "System.Numerics.Vector3", new MappedType("Windows.Foundation.Numerics", "Vector3", "Windows.Foundation.FoundationContract", true) },
+            { "System.Numerics.Vector4", new MappedType("Windows.Foundation.Numerics", "Vector4", "Windows.Foundation.FoundationContract", true) },
+            { "System.Type", new MappedType(GetSystemTypeCustomMapping) },
+            { "System.Collections.Generic.IEnumerable`1", new MappedType("Windows.Foundation.Collections", "IIterable`1", "Windows.Foundation.FoundationContract") },
+            { "System.Collections.Generic.IEnumerator`1", new MappedType("Windows.Foundation.Collections", "IIterator`1", "Windows.Foundation.FoundationContract") },
+            { "System.Collections.Generic.KeyValuePair`2", new MappedType("Windows.Foundation.Collections", "IKeyValuePair`2", "Windows.Foundation.FoundationContract") },
+            { "System.Collections.Generic.IReadOnlyDictionary`2", new MappedType("Windows.Foundation.Collections", "IMapView`2", "Windows.Foundation.FoundationContract") },
+            { "System.Collections.Generic.IDictionary`2", new MappedType("Windows.Foundation.Collections", "IMap`2", "Windows.Foundation.FoundationContract") },
+            { "System.Collections.Generic.IReadOnlyList`1", new MappedType("Windows.Foundation.Collections", "IVectorView`1", "Windows.Foundation.FoundationContract") },
+            { "System.Collections.Generic.IList`1", new MappedType("Windows.Foundation.Collections", "IVector`1", "Windows.Foundation.FoundationContract") },
+            { "Windows.UI.Color", new MappedType("Windows.UI", "Color", "Windows.Foundation.UniversalApiContract", true) },
+        };
     }
 }
