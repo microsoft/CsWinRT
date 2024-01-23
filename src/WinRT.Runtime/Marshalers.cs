04088f10cf314369e0c8545d8d89de7bcd1fd5e2
Sergio Pedri <sergio0694@live.com>
2024-01-23 12:25:16 +0100

    Stop using ThreadLocal<T>, use [ThreadStatic] (#1460)

--- src/WinRT.Runtime/Marshalers.cs
+++ src/WinRT.Runtime/Marshalers.cs
@@ -4,7 +4,6 @@
 using System;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
-using System.Linq.Expressions;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
