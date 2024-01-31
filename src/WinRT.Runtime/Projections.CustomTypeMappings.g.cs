13efd95243a6c0eeabb1cbe515d69124978b5ab1
Sergio Pedri <sergio0694@live.com>
2024-01-31 16:39:23 +0100

    Switch RegisterCustomTypeToHelperTypeMapping too

--- src/WinRT.Runtime/Projections.CustomTypeMappings.g.cs
+++ src/WinRT.Runtime/Projections.CustomTypeMappings.g.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+#if NET
+
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -315,5 +317,47 @@ namespace WinRT
         public static void RegisterEventHandlerMapping() => RegisterCustomAbiTypeMapping(
             typeof(EventHandler),
             typeof(ABI.System.EventHandler));
+
+        /// <summary>Registers the custom ABI type mapping for the <see cref="IMap{K, V}"/> type.</summary>
+        public static void RegisterIMapOpenGenericMapping() => RegisterCustomTypeToHelperTypeMapping(
+            typeof(IMap<,>),
+            typeof(ABI.System.Collections.Generic.IDictionary<,>));
+
+        /// <summary>Registers the custom ABI type mapping for the <see cref="IVector{T}"/> type.</summary>
+        public static void RegisterIVectorOpenGenericMapping() => RegisterCustomTypeToHelperTypeMapping(
+            typeof(IVector<>),
+            typeof(ABI.System.Collections.Generic.IList<>));
+
+        /// <summary>Registers the custom ABI type mapping for the <see cref="IMapView{K, V}"/> type.</summary>
+        public static void RegisterIMapViewOpenGenericMapping() => RegisterCustomTypeToHelperTypeMapping(
+            typeof(IMapView<,>),
+            typeof(ABI.System.Collections.Generic.IReadOnlyDictionary<,>));
+
+        /// <summary>Registers the custom ABI type mapping for the <see cref="IVectorView{T}"/> type.</summary>
+        public static void RegisterIVectorViewOpenGenericMapping() => RegisterCustomTypeToHelperTypeMapping(
+            typeof(IVectorView<>),
+            typeof(ABI.System.Collections.Generic.IReadOnlyList<>));
+
+        /// <summary>Registers the custom ABI type mapping for the <see cref="IBindableVector"/> type.</summary>
+        public static void RegisterIBindableVectorMapping() => RegisterCustomTypeToHelperTypeMapping(
+            typeof(IBindableVector),
+            typeof(ABI.System.Collections.IList));
+
+        /// <summary>Registers the custom ABI type mapping for the <see cref="ICollection{T}"/> type.</summary>
+        public static void RegisterICollectionOpenGenericMapping() => RegisterCustomTypeToHelperTypeMapping(
+            typeof(ICollection<>),
+            typeof(ABI.System.Collections.Generic.ICollection<>));
+
+        /// <summary>Registers the custom ABI type mapping for the <see cref="IReadOnlyCollection{T}"/> type.</summary>
+        public static void RegisterIReadOnlyCollectionOpenGenericMapping() => RegisterCustomTypeToHelperTypeMapping(
+            typeof(IReadOnlyCollection<>),
+            typeof(ABI.System.Collections.Generic.IReadOnlyCollection<>));
+
+        /// <summary>Registers the custom ABI type mapping for the <see cref="ICollection"/> type.</summary>
+        public static void RegisterICollectionMapping() => RegisterCustomTypeToHelperTypeMapping(
+            typeof(ICollection),
+            typeof(ABI.System.Collections.ICollection));
     }
 }
+
+#endif
