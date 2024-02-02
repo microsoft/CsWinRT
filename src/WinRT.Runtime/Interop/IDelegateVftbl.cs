dd5e6cddc372ec80c6ef8c449581c5904e607703
Sergio Pedri <sergio0694@live.com>
2024-02-02 11:19:44 +0100

    Fix comment

--- src/WinRT.Runtime/Interop/IDelegateVftbl.cs
+++ src/WinRT.Runtime/Interop/IDelegateVftbl.cs
@@ -7,7 +7,7 @@ namespace WinRT.Interop
 {
     // This is internal both in the embedded case, but also if we're on modern .NET,
     // because projections only use this vftbl type downlevel, and running older
-    // projections against a .NET version of CsWinRT is not supported.
+    // projections against a newer version of CsWinRT is not supported.
 #if EMBED || NET
     internal
 #else 
