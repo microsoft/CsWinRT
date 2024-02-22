82150c58cbe773cf3a7fa1d64712fd027ea33472
Manodasan Wignarajah <mawign@microsoft.com>
2024-02-22 13:27:27 -0800

    Address issues discovered after fixing AOT warnings (#1511)
    * Fix build breaks from AOT warning fixes
    
    * Improvements to GUID helper type lookup to avoid needing instantiated generic type
    
    * Move around where helper type is initialized to handle more scenarios
    
    * Remove commented code

--- src/WinRT.Runtime/Projections/IList.net5.cs
+++ src/WinRT.Runtime/Projections/IList.net5.cs
@@ -159,6 +159,8 @@ namespace ABI.System.Collections.Generic
     {
         unsafe static IListMethods()
         {
+            ComWrappersSupport.RegisterHelperType(typeof(global::System.Collections.Generic.IList<T>), typeof(global::ABI.System.Collections.Generic.IList<T>));
+
             // Early return to ensure things are trimmed correctly on NAOT.
             // See https://github.com/dotnet/runtime/blob/main/docs/design/tools/illink/feature-checks.md.
             if (!RuntimeFeature.IsDynamicCodeCompiled)
@@ -626,6 +628,8 @@ namespace ABI.System.Collections.Generic
                 return false;
             }
 
+            ComWrappersSupport.RegisterHelperType(typeof(global::System.Collections.Generic.IReadOnlyList<T>), typeof(global::ABI.System.Collections.Generic.IReadOnlyList<T>));
+
             return true;
         }
 
@@ -654,25 +658,20 @@ namespace ABI.System.Collections.Generic
                 new IList_Delegates.ReplaceAll_11(Do_Abi_ReplaceAll_11)
             };
 
-            var abiToProjectionVftablePtr = (IntPtr)NativeMemory.AllocZeroed((nuint)(sizeof(IInspectable.Vftbl) + sizeof(IntPtr) * 12));
-            *(IInspectable.Vftbl*)abiToProjectionVftablePtr = IInspectable.Vftbl.AbiToProjectionVftable;
-            ((IntPtr*)abiToProjectionVftablePtr)[6] = Marshal.GetFunctionPointerForDelegate(DelegateCache[0]);
-            ((IntPtr*)abiToProjectionVftablePtr)[7] = Marshal.GetFunctionPointerForDelegate(DelegateCache[1]);
-            ((IntPtr*)abiToProjectionVftablePtr)[8] = Marshal.GetFunctionPointerForDelegate(DelegateCache[2]);
-            ((IntPtr*)abiToProjectionVftablePtr)[9] = Marshal.GetFunctionPointerForDelegate(DelegateCache[3]);
-            ((IntPtr*)abiToProjectionVftablePtr)[10] = Marshal.GetFunctionPointerForDelegate(DelegateCache[4]);
-            ((IntPtr*)abiToProjectionVftablePtr)[11] = Marshal.GetFunctionPointerForDelegate(DelegateCache[5]);
-            ((IntPtr*)abiToProjectionVftablePtr)[12] = Marshal.GetFunctionPointerForDelegate(DelegateCache[6]);
-            ((IntPtr*)abiToProjectionVftablePtr)[13] = Marshal.GetFunctionPointerForDelegate(DelegateCache[7]);
-            ((IntPtr*)abiToProjectionVftablePtr)[14] = Marshal.GetFunctionPointerForDelegate(DelegateCache[8]);
-            ((IntPtr*)abiToProjectionVftablePtr)[15] = Marshal.GetFunctionPointerForDelegate(DelegateCache[9]);
-            ((IntPtr*)abiToProjectionVftablePtr)[16] = Marshal.GetFunctionPointerForDelegate(DelegateCache[10]);
-            ((IntPtr*)abiToProjectionVftablePtr)[17] = Marshal.GetFunctionPointerForDelegate(DelegateCache[11]);
-
-            if (!IListMethods<T>.TryInitCCWVtable(abiToProjectionVftablePtr))
-            {
-                NativeMemory.Free((void*)abiToProjectionVftablePtr);
-            }
+            InitCcw(
+                (delegate* unmanaged[Stdcall]<IntPtr, uint, TAbi*, int>)Marshal.GetFunctionPointerForDelegate(DelegateCache[0]),
+                (delegate* unmanaged[Stdcall]<IntPtr, uint*, int>)Marshal.GetFunctionPointerForDelegate(DelegateCache[1]),
+                (delegate* unmanaged[Stdcall]<IntPtr, IntPtr*, int>)Marshal.GetFunctionPointerForDelegate(DelegateCache[2]),
+                (delegate* unmanaged[Stdcall]<IntPtr, TAbi, uint*, byte*, int>)Marshal.GetFunctionPointerForDelegate(DelegateCache[3]),
+                (delegate* unmanaged[Stdcall]<IntPtr, uint, TAbi, int>)Marshal.GetFunctionPointerForDelegate(DelegateCache[4]),
+                (delegate* unmanaged[Stdcall]<IntPtr, uint, TAbi, int>)Marshal.GetFunctionPointerForDelegate(DelegateCache[5]),
+                (delegate* unmanaged[Stdcall]<IntPtr, uint, int>)Marshal.GetFunctionPointerForDelegate(DelegateCache[6]),
+                (delegate* unmanaged[Stdcall]<IntPtr, TAbi, int>)Marshal.GetFunctionPointerForDelegate(DelegateCache[7]),
+                (delegate* unmanaged[Stdcall]<IntPtr, int>)Marshal.GetFunctionPointerForDelegate(DelegateCache[8]),
+                (delegate* unmanaged[Stdcall]<IntPtr, int>)Marshal.GetFunctionPointerForDelegate(DelegateCache[9]),
+                (delegate* unmanaged[Stdcall]<IntPtr, uint, int, IntPtr, uint*, int>)Marshal.GetFunctionPointerForDelegate(DelegateCache[10]),
+                (delegate* unmanaged[Stdcall]<IntPtr, int, IntPtr, int>)Marshal.GetFunctionPointerForDelegate(DelegateCache[11])
+            );
         }
 
         private static unsafe int Do_Abi_GetAt_0(void* thisPtr, uint index, TAbi* __return_value__)
