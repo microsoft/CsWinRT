69f7da59b70498d21d62512f0feb43df5749cec2
Manodasan Wignarajah <mawign@microsoft.com>
2024-02-23 11:05:46 -0800

    Fix merge

--- src/WinRT.Runtime/Context.cs
+++ src/WinRT.Runtime/Context.cs
@@ -58,5 +58,10 @@ namespace WinRT
             }
 #endif
         }
+
+        public static void DisposeContextCallback(IntPtr contextCallbackPtr)
+        {
+            MarshalInspectable<object>.DisposeAbi(contextCallbackPtr);
+        }
     }
 }
