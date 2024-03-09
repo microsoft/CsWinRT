9b59cbaf12ea6583adb04f3c8a086ea866c04f68
Johan Laanstra <jlaans@microsoft.com>
2024-03-08 18:02:07 -0800

    Provide ActivationHandler to hook/mock activation and to match C++/WinRT's winrt_activation_handler.

--- src/WinRT.Runtime/ActivationFactory.cs
+++ src/WinRT.Runtime/ActivationFactory.cs
@@ -197,8 +197,27 @@ namespace WinRT
 #endif
     static class ActivationFactory
     {
+#if NET
+        /// <summary>
+        /// This provides a hook into activation to hook/mock activation of WinRT types.
+        /// </summary>
+        public static Func<string, Guid, IntPtr> ActivationHandler { get; set; }
+#endif
+
         public static IObjectReference Get(string typeName)
         {
+#if NET
+            // Check hook first
+            if (ActivationHandler != null)
+            {
+                var factoryFromhandler = GetFromActivationHandler(typeName, IID.IID_IActivationFactory);
+                if (factoryFromhandler != null)
+                {
+                    return factoryFromhandler;
+                }
+            }
+#endif
+
             // Prefer the RoGetActivationFactory HRESULT failure over the LoadLibrary/etc. failure
             int hr;
             ObjectReference<IUnknownVftbl> factory;
@@ -236,6 +255,18 @@ namespace WinRT
         public static ObjectReference<I> Get<I>(string typeName, Guid iid)
 #endif
         {
+#if NET
+            // Check hook first
+            if (ActivationHandler != null)
+            {
+                var factoryFromhandler = GetFromActivationHandler(typeName, iid);
+                if (factoryFromhandler != null)
+                {
+                    return factoryFromhandler;
+                }
+            }
+#endif
+
             // Prefer the RoGetActivationFactory HRESULT failure over the LoadLibrary/etc. failure
             int hr;
 #if NET
@@ -283,5 +314,30 @@ namespace WinRT
                 }
             }
         }
+
+#if NET
+        private static IObjectReference GetFromActivationHandler(string typeName, Guid iid)
+        {
+            var activationHandler = ActivationHandler;
+            if (activationHandler != null)
+            {
+                IntPtr instancePtr = IntPtr.Zero;
+                try
+                {
+                    instancePtr = activationHandler(typeName, iid);
+                    if (instancePtr != IntPtr.Zero)
+                    {
+                        return ObjectReference<IUnknownVftbl>.Attach(ref instancePtr);
+                    }
+                }
+                finally
+                {
+                    MarshalInspectable<object>.DisposeAbi(instancePtr);
+                }
+            }
+
+            return null;
+        }
+#endif
     }
 }
