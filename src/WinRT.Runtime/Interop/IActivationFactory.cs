251f8408cfe718617a256f044c18533174bf2e6f
Sergio Pedri <sergio0694@live.com>
2024-04-30 15:57:48 -0700

    Don't use 'IActivationFactory.Vftbl' on .NET

--- src/WinRT.Runtime/Interop/IActivationFactory.cs
+++ src/WinRT.Runtime/Interop/IActivationFactory.cs
@@ -142,17 +142,23 @@ namespace ABI.WinRT.Interop
                 return 0;
             }
         }
-        internal static ObjectReference<Vftbl> FromAbi(IntPtr thisPtr) => ObjectReference<Vftbl>.FromAbi(thisPtr, IID.IID_IActivationFactory);
 
         public static implicit operator IActivationFactory(IObjectReference obj) => (obj != null) ? new IActivationFactory(obj) : null;
+#if NET
+        protected readonly ObjectReference<IUnknownVftbl> _obj;
+#else
         protected readonly ObjectReference<Vftbl> _obj;
+#endif
         public IObjectReference ObjRef { get => _obj; }
         public IntPtr ThisPtr => _obj.ThisPtr;
 
-        public IActivationFactory(IObjectReference obj) : this(obj.As<Vftbl>(IID.IID_IActivationFactory)) { }
-        internal IActivationFactory(ObjectReference<Vftbl> obj)
+        public IActivationFactory(IObjectReference obj)
         {
-            _obj = obj;
+#if NET
+            _obj = obj.As<IUnknownVftbl>(IID.IID_IActivationFactory);
+#else
+            _obj = obj.As<Vftbl>(IID.IID_IActivationFactory);
+#endif
         }
 
         public unsafe IntPtr ActivateInstance()
@@ -160,7 +166,12 @@ namespace ABI.WinRT.Interop
             IntPtr __retval = default;
             try
             {
+#if NET
+                IntPtr thisPtr = ThisPtr;
+                Marshal.ThrowExceptionForHR(((delegate* unmanaged[Stdcall]<IntPtr, IntPtr*, int>)(*(void***)thisPtr)[6])(thisPtr, &__retval));
+#else
                 global::WinRT.ExceptionHelpers.ThrowExceptionForHR(_obj.Vftbl.ActivateInstance_0(ThisPtr, &__retval));
+#endif
                 return __retval;
             }
             finally
