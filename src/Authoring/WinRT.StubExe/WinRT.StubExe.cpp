e8c5d6c514882b737a1d1fb8d64ce741d0092130
Sergio Pedri <sergio0694@live.com>
2024-06-15 14:17:16 -0700

    Add pch.h/pch.cpp to fix build

--- src/Authoring/WinRT.StubExe/WinRT.StubExe.cpp
+++ src/Authoring/WinRT.StubExe/WinRT.StubExe.cpp
@@ -1,3 +1,17 @@
+#include "pch.h"
+
+#if _M_IX86
+#pragma comment(lib, "libs/CustomNativeMain-x86.lib")
+#elif _M_X64
+#pragma comment(lib, "libs/CustomNativeMain-x64.lib")
+#elif _M_ARM
+#pragma comment(lib, "libs/CustomNativeMain-arm.lib")
+#elif _M_ARM64
+#pragma comment(lib, "libs/CustomNativeMain-arm64.lib")
+#else
+#error Invalid CPU architecture (only x86, x64, arm and arm64 are supported)
+#endif
+
 extern "C" int __managed__Main(int argc, wchar_t* argv[]);
 
 int __cdecl wmain(int argc, wchar_t* argv[])
