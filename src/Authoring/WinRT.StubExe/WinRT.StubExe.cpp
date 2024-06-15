59282ea19770e1650297ed446a494a01c468e444
Sergio Pedri <sergio0694@live.com>
2024-06-15 14:45:39 -0700

    Remove .lib files, switch to 'LoadLibrary'

--- src/Authoring/WinRT.StubExe/WinRT.StubExe.cpp
+++ src/Authoring/WinRT.StubExe/WinRT.StubExe.cpp
@@ -1,20 +1,42 @@
 #include "pch.h"
+#include <windows.h>
 
-#if _M_IX86
-#pragma comment(lib, "libs/CustomNativeMain-x86.lib")
-#elif _M_X64
-#pragma comment(lib, "libs/CustomNativeMain-x64.lib")
-#elif _M_ARM
-#pragma comment(lib, "libs/CustomNativeMain-arm.lib")
-#elif _M_ARM64
-#pragma comment(lib, "libs/CustomNativeMain-arm64.lib")
-#else
-#error Invalid CPU architecture (only x86, x64, arm and arm64 are supported)
-#endif
-
-extern "C" int __managed__Main(int argc, wchar_t* argv[]);
+typedef int (*__managed__Main)(int, wchar_t*[]);
 
 int __cdecl wmain(int argc, wchar_t* argv[])
 {
-    return __managed__Main(argc, argv);
+    wchar_t fileName[MAX_PATH];
+
+    // Get the path of the current .exe file (it will be renamed to match the app name)
+    if (!GetModuleFileNameW(NULL, fileName, MAX_PATH))
+    {
+        return GetLastError();
+    }
+
+    int fileNameLength = lstrlenW(fileName);
+
+    // Replace the extension (.exe -> .dll)
+    memcpy(
+        /* _Dst */ &fileName[fileNameLength - 3 /* "exe" */],
+        /* _Src */ L"dll",
+        /* _Size */ sizeof(wchar_t) * 3 /* "dll" */);
+
+    // Load the .dll for the app
+    HMODULE hModule = LoadLibraryW(fileName);
+
+    if (!hModule)
+    {
+        return GetLastError();
+    }
+
+    // Get the custom main from the native .dll (hardcoded to "__managed__Main")
+    FARPROC pEntryPoint = GetProcAddress(hModule, "__managed__Main");
+
+    if (!pEntryPoint)
+    {
+        return GetLastError();
+    }
+
+    // Jump to the custom entry point in the implementation .dll
+    return ((__managed__Main)pEntryPoint)(argc, argv);
 }
