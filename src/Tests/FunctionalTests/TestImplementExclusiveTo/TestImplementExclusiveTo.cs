9d36c332edc05c21cbb90c06c3e60a648f70127c
Jevan Saks <jevansa@microsoft.com>
2024-07-10 21:55:13 -0700

    Adding functional test too

---
+++ src/Tests/FunctionalTests/TestImplementExclusiveTo/TestImplementExclusiveTo.cs
@@ -0,0 +1,11 @@
+﻿using TestComponentCSharp.TestPublicExclusiveTo;
+
+namespace TestLibrary
+{
+    public sealed class TestClass : INonUniqueClass, IRegularInterface, INonUniqueClassFactory
+    {
+        int Type { get; } => throw new NotImplementedException();
+        string Path { get; } => throw new NotImplementedException();
+        int StaticProperty { get; } => throw new NotImplementedException();
+    }
+}
