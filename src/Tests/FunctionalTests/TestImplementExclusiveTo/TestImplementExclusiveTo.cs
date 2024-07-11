d3f0791223cfe79772b02d8163d10e3db8032d9a
Jevan Saks <jevansa@microsoft.com>
2024-07-11 10:22:55 -0700

    Fix warning

--- src/Tests/FunctionalTests/TestImplementExclusiveTo/TestImplementExclusiveTo.cs
+++ src/Tests/FunctionalTests/TestImplementExclusiveTo/TestImplementExclusiveTo.cs
@@ -3,7 +3,7 @@ using TestComponentCSharp.TestPublicExclusiveTo;
 
 namespace TestImplementExclusiveTo
 {
-    public sealed class TestClass : INonUniqueClass, IRegularInterface, INonUniqueClassFactory
+    public sealed partial class TestClass : INonUniqueClass, IRegularInterface, INonUniqueClassFactory
     {
         public int Type => throw new NotImplementedException();
         public string Path => throw new NotImplementedException();
