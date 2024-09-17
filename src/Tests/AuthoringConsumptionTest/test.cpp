7c6f08f91ba43f1cb4be737d86f340144a91a1ea
Sergio Pedri <sergio0694@live.com>
2024-09-17 18:36:14 +0200

    Fix typo

--- src/Tests/AuthoringConsumptionTest/test.cpp
+++ src/Tests/AuthoringConsumptionTest/test.cpp
@@ -731,7 +731,7 @@ TEST(AuthoringTest, CustomInterfaceGuid)
     ICustomInterfaceGuid customInterface;
 
     IID customInterfaceIid;
-    check_result(IIDFromString(L"{26D8EE57-8B1B-46F4-A4F9-8C6DEEEAF53A}", &customInterfaceIid));
+    check_hresult(IIDFromString(L"{26D8EE57-8B1B-46F4-A4F9-8C6DEEEAF53A}", &customInterfaceIid));
     check_hresult(customInterfaceClassUnknown->QueryInterface(customInterfaceIid, reinterpret_cast<void**>(winrt::put_abi(customInterface))));
 
     EXPECT_EQ(customInterface.HelloWorld(), L"Hello World!");
