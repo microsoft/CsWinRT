70f81b174953b7daa386dda147b730c0ca352ecc
Sergio Pedri <sergio0694@live.com>
2025-02-26 10:18:17 -0800

    Add 'WindowsRuntimeImports'

---
+++ src/WinRT.Runtime2/InteropServices/Platform/WindowsRuntimeImports.cs
@@ -0,0 +1,84 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+namespace WindowsRuntime.InteropServices;
+
+/// <summary>
+/// Native imports for fundamental COM and Windows Runtime APIs.
+/// </summary>
+internal static unsafe partial class WindowsRuntimeImports
+{
+    /// <see href="https://learn.microsoft.com/windows/win32/api/combaseapi/nf-combaseapi-cocreateinstance"/>
+    [LibraryImport("api-ms-win-core-com-l1-1-0.dll")]
+    public static partial HRESULT CoCreateInstance(Guid* rclsid, void* pUnkOuter, uint dwClsContext, Guid* riid, void** ppv);
+
+    /// <see href="https://learn.microsoft.com/windows/win32/api/combaseapi/nf-combaseapi-coincrementmtausage"/>
+    [LibraryImport("api-ms-win-core-com-l1-1-0.dll")]
+    public static unsafe partial HRESULT CoIncrementMTAUsage(CO_MTA_USAGE_COOKIE* cookie);
+
+    /// <see href="https://learn.microsoft.com/windows/win32/api/combaseapi/nf-combaseapi-codecrementmtausage"/>
+    [LibraryImport("api-ms-win-core-com-l1-1-0.dll")]
+    public static partial HRESULT CoDecrementMTAUsage(CO_MTA_USAGE_COOKIE cookie);
+
+    /// <see href="https://learn.microsoft.com/windows/win32/api/roapi/nf-roapi-rogetactivationfactory"/>
+    [LibraryImport("api-ms-win-core-winrt-l1-1-0.dll")]
+    [SupportedOSPlatform("windows6.2")]
+    public static partial HRESULT RoGetActivationFactory(HSTRING activatableClassId, Guid* iid, void** factory);
+
+    /// <see href="https://learn.microsoft.com/windows/win32/api/winstring/nf-winstring-windowscreatestring"/>
+    [LibraryImport("api-ms-win-core-winrt-string-l1-1-0.dll")]
+    [SupportedOSPlatform("windows6.2")]
+    public static partial HRESULT WindowsCreateString(char* sourceString, uint length, HSTRING* @string);
+
+    /// <see href="https://learn.microsoft.com/windows/win32/api/winstring/nf-winstring-windowscreatestringreference"/>
+    [LibraryImport("api-ms-win-core-winrt-string-l1-1-0.dll")]
+    [SupportedOSPlatform("windows6.2")]
+    public static partial HRESULT WindowsCreateStringReference(char* sourceString, uint length, /* TODO */ void* hstringHeader, HSTRING* @string);
+
+    /// <see href="https://learn.microsoft.com/windows/win32/api/winstring/nf-winstring-windowsdeletestring"/>
+    [LibraryImport("api-ms-win-core-winrt-string-l1-1-0.dll")]
+    [SupportedOSPlatform("windows6.2")]
+    public static partial HRESULT WindowsDeleteString(HSTRING @string);
+
+    /// <see href="https://learn.microsoft.com/windows/win32/api/winstring/nf-winstring-windowsgetstringrawbuffer"/>
+    [LibraryImport("api-ms-win-core-winrt-string-l1-1-0.dll")]
+    [SupportedOSPlatform("windows6.2")]
+    public static partial char* WindowsGetStringRawBuffer(HSTRING @string, uint* length);
+
+    /// <see href="https://learn.microsoft.com/windows/win32/api/combaseapi/nf-combaseapi-rogetagilereference"/>
+    [LibraryImport("api-ms-win-core-com-l1-1-1.dll")]
+    [SupportedOSPlatform("windows6.3")]
+    public static partial HRESULT RoGetAgileReference(AgileReferenceOptions options, Guid* riid, void* pUnk, void** ppAgileReference);
+
+    /// <see href="https://learn.microsoft.com/windows/win32/api/combaseapi/nf-combaseapi-cogetcontexttoken"/>
+    [LibraryImport("api-ms-win-core-com-l1-1-0.dll")]
+    public static partial HRESULT CoGetContextToken(nuint* pToken);
+
+    /// <see href="https://learn.microsoft.com/windows/win32/api/combaseapi/nf-combaseapi-cogetobjectcontext"/>
+    [LibraryImport("api-ms-win-core-com-l1-1-0.dll")]
+    public static partial HRESULT CoGetObjectContext(Guid* riid, void** ppv);
+
+    /// <see href="https://learn.microsoft.com/windows/win32/api/oleauto/nf-oleauto-seterrorinfo"/>
+    [LibraryImport("oleaut32.dll")]
+    public static partial HRESULT SetErrorInfo(uint dwReserved, void* perrinfo);
+
+    /// <see href="https://learn.microsoft.com/windows/win32/api/combaseapi/nf-combaseapi-cocreatefreethreadedmarshaler"/>
+    [LibraryImport("api-ms-win-core-com-l1-1-0.dll")]
+    public static partial HRESULT CoCreateFreeThreadedMarshaler(void* punkOuter, void** ppunkMarshal);
+
+    /// <see href="https://learn.microsoft.com/windows/win32/api/libloaderapi/nf-libloaderapi-loadlibraryexw"/>
+    [LibraryImport("kernel32", SetLastError = true)]
+    public static partial HMODULE LoadLibraryExW(char* lpLibFileName, HANDLE hFile, uint dwFlags);
+
+    /// <see href="https://learn.microsoft.com/windows/win32/api/libloaderapi/nf-libloaderapi-getprocaddress"/>
+    [LibraryImport("kernel32", SetLastError = true)]
+    public static partial void* GetProcAddress(HMODULE hModule, sbyte* lpProcName);
+
+    /// <see href="https://learn.microsoft.com/windows/win32/api/libloaderapi/nf-libloaderapi-freelibrary"/>
+    [LibraryImport("kernel32", SetLastError = true)]
+    public static partial int FreeLibrary(HMODULE hLibModule);
+}
