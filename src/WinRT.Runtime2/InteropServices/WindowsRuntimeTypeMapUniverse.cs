1460062ba5853599d7d0b8f413389d93e96ce390
Sergio Pedri <sergio0694@live.com>
2025-02-25 17:08:15 -0800

    Create WindowsRuntimeTypeMapUniverse.cs

---
+++ src/WinRT.Runtime2/InteropServices/WindowsRuntimeTypeMapUniverse.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Runtime.InteropServices;
+
+namespace WindowsRuntime.InteropServices;
+
+/// <summary>
+/// The <see cref="ITypeMapUniverse"/> placeholder for all Windows Runtime types, and marshalled types.
+/// </summary>
+public sealed class WindowsRuntimeTypeMapUniverse : ITypeMapUniverse;
