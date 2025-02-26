c866a6979caa5614e2ed3e495dd76ffeec7da742
Sophia Chen <sophia.six.chen@gmail.com>
2025-02-25 17:21:11 -0800

    changes for the guid stuff

--- src/Authoring/WinRT.SourceGenerator/WinRTTypeWriter.cs
+++ src/Authoring/WinRT.SourceGenerator/WinRTTypeWriter.cs
@@ -1612,7 +1612,7 @@ namespace Generator
                 byteArray[12],
                 byteArray[13],
                 byteArray[14],
-                byteArray[15]
+                byteArray[15] 
             };
 
             AddCustomAttributes("Windows.Foundation.Metadata.GuidAttribute", types, arguments, parentHandle);
@@ -2454,7 +2454,15 @@ namespace Generator
                 }
 
                 AddDefaultVersionAttribute(typeDefinitionHandle, GetVersion(classSymbol, true));
-                AddGuidAttribute(typeDefinitionHandle, interfaceName);
+                // bookmarking
+                Logger.Log("HelloWorld: the interfaceName is " + interfaceName);
+                Logger.Log("HelloWorld: Class Symbol is " + classSymbol.ToString());
+                if (!GetProjectionsDefaultInterfaceGuid(classSymbol, typeDefinitionHandle, classDeclaration))
+                {
+                    Logger.Log("adding a new guid attribute");
+                    AddGuidAttribute(typeDefinitionHandle, interfaceName);
+                }
+
                 AddExclusiveToAttribute(typeDefinitionHandle, classSymbol.ToString());
                 AddOverloadAttributeForInterfaceMethods(typeDeclaration);
 
@@ -2716,6 +2724,75 @@ namespace Generator
             }
         }
 
+        public bool GetProjectionsDefaultInterfaceGuid(INamedTypeSymbol symbol, TypeDefinitionHandle handle, TypeDeclaration classDeclaration)
+        {
+            bool IsWinRTType(ISymbol symbol, TypeMapper mapper)
+            {
+                if (symbol is INamedTypeSymbol namedType)
+                {
+                    if (namedType.TypeKind == TypeKind.Interface)
+                    {
+                        // Interfaces which are allowed to be implemented on authored types but
+                        // aren't WinRT interfaces.
+                        return !ImplementedInterfacesWithoutMapping.Contains(QualifiedName(namedType));
+                    }
+
+                    return namedType.SpecialType != SpecialType.System_Object &&
+                           namedType.SpecialType != SpecialType.System_Enum &&
+                           namedType.SpecialType != SpecialType.System_ValueType &&
+                           namedType.SpecialType != SpecialType.System_Delegate &&
+                           namedType.SpecialType != SpecialType.System_MulticastDelegate;
+                }
+
+                // In an authoring component, diagnostics prevents you from using non-WinRT types
+                // by the time we get to here.
+                return true;
+            }
+
+            
+            Logger.Log("HelloWorld: Start of our logic");
+            Logger.Log("handle is " + handle.ToString());
+            Guid guid = new Guid();
+
+            var baseAbstractClass = symbol.BaseType;
+            if (baseAbstractClass != null && baseAbstractClass.IsAbstract && IsWinRTType(baseAbstractClass, mapper))
+            {
+                Logger.Log("HelloWorld: Inside the flag condition");
+                Logger.Log("The abstract base class is " + baseAbstractClass);
+
+                if (classDeclaration.DefaultInterface != null)
+                {
+                    Logger.Log("HelloWorld: There is a default class class " + classDeclaration.DefaultInterface);
+
+                    // Get the guid of the projections class 
+                    var projectedAttr = baseAbstractClass.BaseType.GetAttributes().FirstOrDefault(attr => attr.AttributeClass?.Name == "ProjectedRuntimeClassAttribute");
+
+                    if (projectedAttr != null && projectedAttr.ConstructorArguments.Length == 1 && projectedAttr.ConstructorArguments[0].Value is INamedTypeSymbol projectedType)
+                    {
+                        Logger.Log("HelloWorld: There is an attribute named ProjectRuntimeClass");
+                        projectedType.TryGetAttributeWithType(Model.Compilation.GetTypeByMetadataName("System.Runtime.InteropServices.GuidAttribute"), out AttributeData guidAttr);
+                        if (guidAttr != null && guidAttr.ConstructorArguments.Length == 1)
+                        {
+                            Logger.Log("HelloWorld: There is an attribute named GUID. Just added the GUID.");
+                            string guidString = guidAttr.ConstructorArguments[0].Value as string;
+                            Logger.Log("The guid is " + guidString);
+                            if (!string.IsNullOrEmpty(guidString) && Guid.TryParse(guidString, out Guid parsedGuid))
+                            {
+                                Logger.Log("The guid is parsed");
+                                guid = parsedGuid;
+
+                                AddGuidAttribute(handle, guid);
+                                Logger.Log("The guid is added");
+
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
         public void GenerateWinRTExposedClassAttributes(GeneratorExecutionContext context)
         {
             bool IsWinRTType(ISymbol symbol, TypeMapper mapper)
