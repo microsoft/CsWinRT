89f59a54a9b339e14d7e054dd4c11b319a45e89e
Sergio Pedri <sergio0694@live.com>
2025-02-26 11:32:41 -0800

    Add 'WindowsRuntimeObject' draft

---
+++ src/WinRT.Runtime2/WindowsRuntimeObject.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.Marshalling;
+
+namespace WindowsRuntime;
+
+/// <summary>
+/// The base class for all projected Windows Runtime types.
+/// </summary>
+/// <remarks>
+/// This type should only be used as a base type by generated projected types.
+/// </remarks>
+public abstract class WindowsRuntimeObject :
+    IDynamicInterfaceCastable,
+    IUnmanagedVirtualMethodTableProvider,
+    ICustomQueryInterface
+{
+    protected WindowsRuntimeObject(object nativeObject)
+    {
+        ArgumentNullException.ThrowIfNull(nativeObject);
+    }
+
+    internal object NativeObject { get; }
+
+    /// <inheritdoc/>
+    RuntimeTypeHandle IDynamicInterfaceCastable.GetInterfaceImplementation(RuntimeTypeHandle interfaceType)
+    {
+        throw null!;
+    }
+
+    /// <inheritdoc/>
+    bool IDynamicInterfaceCastable.IsInterfaceImplemented(RuntimeTypeHandle interfaceType, bool throwIfNotImplemented)
+    {
+        return false;
+    }
+
+    /// <inheritdoc/>
+    VirtualMethodTableInfo IUnmanagedVirtualMethodTableProvider.GetVirtualMethodTableInfoForKey(Type type)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc/>
+    CustomQueryInterfaceResult ICustomQueryInterface.GetInterface(ref Guid iid, out nint ppv)
+    {
+        return GetInterface(ref iid, out ppv);
+    }
+
+    /// <inheritdoc cref="ICustomQueryInterface.GetInterface"/>
+    protected abstract CustomQueryInterfaceResult GetInterface(ref Guid iid, out nint ppv);
+}
