33b35d96ffaa690106ac13cdb71d19fe68131b71
Sergio Pedri <sergio0694@live.com>
2025-02-26 18:22:56 -0800

    Add 'IInspectableVftbl' and 'TrustLeve'

---
+++ src/WinRT.Runtime2/InteropServices/Vtables/IInspectableVftbl.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+
+#pragma warning disable CS0649
+
+namespace WindowsRuntime.InteropServices;
+
+/// <summary>
+/// Binding type for the <c>IInspectable</c> interface vtable.
+/// </summary>
+/// <see href="https://learn.microsoft.com/windows/win32/api/inspectable/nn-inspectable-iinspectable"/>
+internal unsafe struct IInspectableVftbl
+{
+    public delegate* unmanaged[MemberFunction]<void*, Guid*, void**, HRESULT> QueryInterface;
+    public delegate* unmanaged[MemberFunction]<void*, uint> AddRef;
+    public delegate* unmanaged[MemberFunction]<void*, uint> Release;
+    public delegate* unmanaged[MemberFunction]<void*, uint*, Guid**, int> GetIids;
+    public delegate* unmanaged[MemberFunction]<void*, HSTRING*, int> GetRuntimeClassName;
+    public delegate* unmanaged[MemberFunction]<void*, TrustLevel*, int> GetTrustLevel;
+}
