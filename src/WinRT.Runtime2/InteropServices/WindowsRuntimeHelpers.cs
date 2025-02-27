7bdd79b3cda26d05baf72ec4ca404842b61c50a4
Sergio Pedri <sergio0694@live.com>
2025-02-27 15:39:24 -0800

    Add 'WindowsRuntimeHelpers'

---
+++ src/WinRT.Runtime2/InteropServices/WindowsRuntimeHelpers.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.Runtime.CompilerServices;
+
+namespace WindowsRuntime.InteropServices;
+
+/// <summary>
+/// Interop helpers for interacting with Windows Runtime types.
+/// </summary>
+public static unsafe class WindowsRuntimeHelpers
+{
+    /// <summary>
+    /// Allocates an initializes an <c>IInspectable</c> vtable that's associated with a given type.
+    /// </summary>
+    /// <param name="type">The type associated with the allocated memory.</param>
+    /// <param name="count">The number of total entries in the resulting vtable.</param>
+    /// <returns>The resulting <c>IInspectable</c> vtable.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="type"/> is <see langword="null"/>.</exception>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown if <paramref name="count"/> is less than <c>6</c> (the size of <c>IInspectable</c>).</exception>
+    public static void** AllocateTypeAssociatedInspectableVtable(Type type, int count)
+    {
+        ArgumentOutOfRangeException.ThrowIfLessThan(count, 6);
+
+        void** vftbl = (void**)RuntimeHelpers.AllocateTypeAssociatedMemory(type, sizeof(void*) * count);
+
+        Unsafe.Copy(vftbl, in *(IInspectableVftbl*)Inspectable.AbiToProjectionVftablePtr);
+
+        return vftbl;
+    }
+}
