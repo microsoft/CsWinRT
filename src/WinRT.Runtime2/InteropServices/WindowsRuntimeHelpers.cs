9ee59fd1a8c7e3201177eb25e0327eae2ece96e9
Sergio Pedri <sergio0694@live.com>
2025-02-28 11:58:06 -0800

    Add 'IsReferenceToManagedObject' helper

--- src/WinRT.Runtime2/InteropServices/WindowsRuntimeHelpers.cs
+++ src/WinRT.Runtime2/InteropServices/WindowsRuntimeHelpers.cs
@@ -4,6 +4,8 @@
 using System;
 using System.Runtime.CompilerServices;
 
+#pragma warning disable CS8909
+
 namespace WindowsRuntime.InteropServices;
 
 /// <summary>
@@ -47,5 +49,24 @@ public static unsafe class WindowsRuntimeHelpers
         vftbl[6] = fpEntry6;
 
         return vftbl;
+    }
+
+    /// <summary>
+    /// Checks whether a pointer to a COM object is actually a reference to a CCW produced for a managed object that was marshalled to native code.
+    /// </summary>
+    /// <param name="unknown">The pointer to a COM object.</param>
+    /// <returns>Whether <paramref name="unknown"/> refers to a CCW for a managed object, rather than a native COM object.</returns>
+    /// <remarks>
+    /// This method does not validate the input <paramref name="unknown"/> pointer not being <see langword="null"/>.
+    /// </remarks>
+    public static bool IsReferenceToManagedObject(void* unknown)
+    {
+        IUnknownVftbl* unknownVftbl = (IUnknownVftbl*)(void**)unknown;
+        IUnknownVftbl* runtimeVftbl = (IUnknownVftbl*)IUnknown.AbiToProjectionVftablePtr;
+
+        return
+            unknownVftbl->QueryInterface == runtimeVftbl->QueryInterface &&
+            unknownVftbl->AddRef == runtimeVftbl->AddRef &&
+            unknownVftbl->Release == runtimeVftbl->Release;
     }
 }
