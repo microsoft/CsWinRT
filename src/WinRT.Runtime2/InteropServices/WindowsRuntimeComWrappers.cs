d0fd1f0733842f244019b663a7eab4201686c0e7
Sergio Pedri <sergio0694@live.com>
2025-02-27 19:25:59 -0800

    Add blank 'WindowsRuntimeComWrappers'

---
+++ src/WinRT.Runtime2/InteropServices/WindowsRuntimeComWrappers.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+namespace WindowsRuntime.InteropServices;
+
+/// <summary>
+/// The <see cref="ComWrappers"/> implementation for Windows Runtime interop.
+/// </summary>
+public sealed unsafe class WindowsRuntimeComWrappers : ComWrappers
+{
+    /// <inheritdoc/>
+    protected override ComInterfaceEntry* ComputeVtables(object obj, CreateComInterfaceFlags flags, out int count)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc/>
+    protected override object? CreateObject(nint externalComObject, CreateObjectFlags flags)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc/>
+    protected override void ReleaseObjects(IEnumerable objects)
+    {
+        throw new NotImplementedException();
+    }
+}
