8c857bab82634e2d9bbc4542728196a637afcc38
Sergio Pedri <sergio0694@live.com>
2025-02-28 11:27:16 -0800

    Move 'HStringReference' to separate file

--- src/WinRT.Runtime2/InteropServices/Marshalling/HStringMarshaller.cs
+++ src/WinRT.Runtime2/InteropServices/Marshalling/HStringMarshaller.cs
@@ -8,30 +8,6 @@ using System.Runtime.InteropServices;
 namespace WindowsRuntime.InteropServices.Marshalling;
 
 /// <summary>
-/// Represents a reference to a fast-pass <c>HSTRING</c> value (passed without copying).
-/// </summary>
-public ref struct HStringReference
-{
-    /// <summary>
-    /// The underlying header for the <c>HSTRING</c> reference.
-    /// </summary>
-    internal HSTRING_HEADER _header;
-
-    /// <summary>
-    /// The fast-pass <c>HSTRING</c> value.
-    /// </summary>
-    internal HSTRING _hstring;
-
-    /// <summary>
-    /// Gets the fast-pass <c>HSTRING</c> value for this reference.
-    /// </summary>
-    /// <remarks>
-    /// It is not valid to escape this value outside of the scope of the current <see cref="HStringReference"/> instance.
-    /// </remarks>
-    public readonly HSTRING HString => _hstring;
-}
-
-/// <summary>
 /// A marshaller for the Windows Runtime <c>HSTRING</c> type.
 /// </summary>
 public static unsafe class HStringMarshaller
---
+++ src/WinRT.Runtime2/InteropServices/Marshalling/HStringReference.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace WindowsRuntime.InteropServices.Marshalling;
+
+/// <summary>
+/// Represents a reference to a fast-pass <c>HSTRING</c> value (passed without copying).
+/// </summary>
+public ref struct HStringReference
+{
+    /// <summary>
+    /// The underlying header for the <c>HSTRING</c> reference.
+    /// </summary>
+    internal HSTRING_HEADER _header;
+
+    /// <summary>
+    /// The fast-pass <c>HSTRING</c> value.
+    /// </summary>
+    internal HSTRING _hstring;
+
+    /// <summary>
+    /// Gets the fast-pass <c>HSTRING</c> value for this reference.
+    /// </summary>
+    /// <remarks>
+    /// It is not valid to escape this value outside of the scope of the current <see cref="HStringReference"/> instance.
+    /// </remarks>
+    public readonly HSTRING HString => _hstring;
+}
