d5a1c550a602da187bbe58c55f067be8ce688da2
Sergio Pedri <sergio0694@live.com>
2025-02-27 17:58:21 -0800

    Tweaks to T4 files

--- src/WinRT.Runtime2/InteropServices/WellKnownErrorCodes.g.cs
+++ src/WinRT.Runtime2/InteropServices/WellKnownErrorCodes.g.cs
@@ -1,12 +1,14 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-namespace WindowsRuntime.InteropServices
+namespace WindowsRuntime.InteropServices;
+
+/// <summary><c>HRESULT</c>-s for common scenarios.</summary>
+internal static class WellKnownErrorCodes
 {
-    /// <summary><c>HRESULT</c>-s for common scenarios.</summary>
-    internal static class WellKnownErrorCodes
-    {
-        public const HRESULT S_OK = unchecked((int)0x0000);
-        public const HRESULT E_POINTER = unchecked((int)0x80004003);
-    }
+    /// <summary>Operation successful.</summary>
+    public const HRESULT S_OK = unchecked((int)0x00000000);
+
+    /// <summary>Pointer that is not valid.</summary>
+    public const HRESULT E_POINTER = unchecked((int)0x80004003);
 }
