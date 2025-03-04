e3943f0f63a26d5b8533014417614292eb5e29d1
Sergio Pedri <sergio0694@live.com>
2025-03-03 20:08:19 -0800

    Add draft weak reference vtables

---
+++ src/WinRT.Runtime2/InteropServices/ProjectionVtables/IWeakReference.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using static System.Runtime.InteropServices.ComWrappers;
+
+namespace WindowsRuntime.InteropServices;
+
+/// <summary>
+/// The <c>IWeakReference</c> implementation for managed types.
+/// </summary>
+/// <see href="https://learn.microsoft.com/windows/win32/api/weakreference/nn-weakreference-iweakreference"/>
+internal static unsafe class IWeakReference
+{
+    /// <summary>
+    /// The vtable for the <c>IWeakReference</c> implementation.
+    /// </summary>
+    public static nint AbiToProjectionVftablePtr { get; } = GetAbiToProjectionVftablePtr();
+
+    /// <summary>
+    /// Computes the <c>IWeakReference</c> implementation vtable.
+    /// </summary>
+    private static nint GetAbiToProjectionVftablePtr()
+    {
+        IWeakReferenceVftbl* vftbl = (IWeakReferenceVftbl*)WindowsRuntimeHelpers.AllocateTypeAssociatedUnknownVtable(typeof(IWeakReference), 4);
+
+        vftbl->Resolve = &Resolve;
+
+        return (nint)vftbl;
+    }
+
+    /// <see href="https://learn.microsoft.com/windows/win32/api/weakreference/nf-weakreference-iweakreference-resolve(refiid_iinspectable)"/>
+    [UnmanagedCallersOnly(CallConvs = [typeof(CallConvMemberFunction)])]
+    private static HRESULT Resolve(void* thisPtr, Guid* riid, void** objectReference)
+    {
+        *objectReference = null;
+
+        try
+        {
+            object instance = ComInterfaceDispatch.GetInstance<object>((ComInterfaceDispatch*)thisPtr);
+
+            // TODO
+            *objectReference = null;
+
+            return WellKnownErrorCodes.S_OK;
+        }
+        catch (Exception e)
+        {
+            return e.HResult;
+        }
+    }
+}
---
+++ src/WinRT.Runtime2/InteropServices/ProjectionVtables/IWeakReferenceSource.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using static System.Runtime.InteropServices.ComWrappers;
+
+namespace WindowsRuntime.InteropServices;
+
+/// <summary>
+/// The <c>IWeakReferenceSource</c> implementation for managed types.
+/// </summary>
+/// <see href="https://learn.microsoft.com/windows/win32/api/weakreference/nn-weakreference-iweakreferencesource"/>
+internal static unsafe class IWeakReferenceSource
+{
+    /// <summary>
+    /// The vtable for the <c>IWeakReferenceSource</c> implementation.
+    /// </summary>
+    public static nint AbiToProjectionVftablePtr { get; } = GetAbiToProjectionVftablePtr();
+
+    /// <summary>
+    /// Computes the <c>IWeakReferenceSource</c> implementation vtable.
+    /// </summary>
+    private static nint GetAbiToProjectionVftablePtr()
+    {
+        IWeakReferenceSourceVftbl* vftbl = (IWeakReferenceSourceVftbl*)WindowsRuntimeHelpers.AllocateTypeAssociatedUnknownVtable(typeof(IWeakReferenceSource), 4);
+
+        vftbl->GetWeakReference = &GetWeakReference;
+
+        return (nint)vftbl;
+    }
+
+    /// <see href="https://learn.microsoft.com/windows/win32/api/weakreference/nf-weakreference-iweakreferencesource-getweakreference"/>
+    [UnmanagedCallersOnly(CallConvs = [typeof(CallConvMemberFunction)])]
+    private static HRESULT GetWeakReference(void* thisPtr, void** weakReference)
+    {
+        *weakReference = null;
+
+        try
+        {
+            object instance = ComInterfaceDispatch.GetInstance<object>((ComInterfaceDispatch*)thisPtr);
+
+            // TODO
+            *weakReference = null;
+
+            return WellKnownErrorCodes.S_OK;
+        }
+        catch (Exception e)
+        {
+            return e.HResult;
+        }
+    }
+}
