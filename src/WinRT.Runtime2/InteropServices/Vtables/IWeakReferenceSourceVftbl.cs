6f5241e5b0b32c27274244a85d113cc906f30e58
Sergio Pedri <sergio0694@live.com>
2025-03-03 20:01:09 -0800

    Create IWeakReferenceSourceVftbl.cs

---
+++ src/WinRT.Runtime2/InteropServices/Vtables/IWeakReferenceSourceVftbl.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.Runtime.CompilerServices;
+
+#pragma warning disable CS0649
+
+namespace WindowsRuntime.InteropServices;
+
+/// <summary>
+/// Binding type for the <c>IWeakReferenceSource</c> interface vtable.
+/// </summary>
+/// <see href="https://learn.microsoft.com/windows/win32/api/weakreference/nn-weakreference-iweakreferencesource"/>
+internal unsafe struct IWeakReferenceSourceVftbl
+{
+    public delegate* unmanaged[MemberFunction]<void*, Guid*, void**, HRESULT> QueryInterface;
+    public delegate* unmanaged[MemberFunction]<void*, uint> AddRef;
+    public delegate* unmanaged[MemberFunction]<void*, uint> Release;
+    public delegate* unmanaged[MemberFunction]<void*, void**, HRESULT> GetWeakReference;
+
+    /// <summary>
+    /// Retrieves a weak reference from an <c>IWeakReferenceSource</c>.
+    /// </summary>
+    /// <param name="thisPtr">The target COM object.</param>
+    /// <param name="weakReference">The weak reference.</param>
+    /// <returns>If this method succeeds, then it returns <c>S_OK</c>. Otherwise, it returns an <c>HRESULT</c> error code.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static HRESULT GetWeakReferenceUnsafe(void* thisPtr, void** weakReference)
+    {
+        return ((IWeakReferenceSourceVftbl*)thisPtr)->GetWeakReference(thisPtr, weakReference);
+    }
+}
