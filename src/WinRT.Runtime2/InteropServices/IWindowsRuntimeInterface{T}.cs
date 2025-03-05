b0d706476abcb89cbc645a0375d860766cfca0e3
Sergio Pedri <sergio0694@live.com>
2025-03-04 19:32:53 -0800

    Add 'IWindowsRuntimeInterface<T>'

---
+++ src/WinRT.Runtime2/InteropServices/IWindowsRuntimeInterface{T}.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace WindowsRuntime.InteropServices;
+
+/// <summary>
+/// An interface providing access to the cached Windows Runtime object reference for a given interface.
+/// </summary>
+/// <typeparam name="T">The interface providing access to.</typeparam>
+/// <remarks>
+/// <para>
+/// This interface is only meant to be implemented by projected runtime classes.
+/// </para>
+/// <para>
+/// The <typeparamref name="T"/> type must refer to a projected Windows Runtime interface.
+/// </para>
+/// </remarks>
+public interface IWindowsRuntimeInterface<T>
+    where T : class
+{
+    /// <summary>
+    /// Gets the cached <see cref="WindowsRuntimeObjectReference"/> instance for the interface <typeparamref name="T"/>.
+    /// </summary>
+    /// <returns>The cached <see cref="WindowsRuntimeObjectReference"/> instance for the interface <typeparamref name="T"/>.</returns>
+    WindowsRuntimeObjectReference GetInterface();
+}
