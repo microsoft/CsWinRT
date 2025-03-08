fa7939b06320223fad2ddd9953116438ac7975d6
Sergio Pedri <sergio0694@live.com>
2025-03-07 16:35:22 -0800

    draft

--- src/WinRT.Runtime2/InteropServices/WindowsRuntimeComWrappers.cs
+++ src/WinRT.Runtime2/InteropServices/WindowsRuntimeComWrappers.cs
@@ -13,6 +13,17 @@ namespace WindowsRuntime.InteropServices;
 public sealed unsafe class WindowsRuntimeComWrappers : ComWrappers
 {
     /// <summary>
+    /// The statically-visible type that should be used by <see cref="CreateObject"/>, if available.
+    /// </summary>
+    /// <remarks>
+    /// This can be set by a thread right before calling <see cref="CreateObject"/>, to pass additional
+    /// information to the <see cref="ComWrappers"/> instance. It should be set to <see langword="null"/>
+    /// immediately afterwards, to ensure following calls won't accidentally see the wrong type.
+    /// </remarks>
+    [ThreadStatic]
+    internal static Type? CreateObjectTargetType;
+
+    /// <summary>
     /// Gets the shared default instance of <see cref="WindowsRuntimeComWrappers"/>.
     /// </summary>
     /// <remarks>
@@ -23,12 +34,13 @@ public sealed unsafe class WindowsRuntimeComWrappers : ComWrappers
     /// <inheritdoc/>
     protected override ComInterfaceEntry* ComputeVtables(object obj, CreateComInterfaceFlags flags, out int count)
     {
-        throw new NotImplementedException();
+
     }
 
     /// <inheritdoc/>
     protected override object? CreateObject(nint externalComObject, CreateObjectFlags flags)
     {
+
         throw new NotImplementedException();
     }
 
