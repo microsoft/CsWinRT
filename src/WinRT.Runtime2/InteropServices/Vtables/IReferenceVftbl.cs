519bb6dda3d3c42a31e3db784cbb9e2245101e6d
Sergio Pedri <sergio0694@live.com>
2025-03-10 22:06:57 -0700

    Add 'IReferenceVftbl' type

---
+++ src/WinRT.Runtime2/InteropServices/Vtables/IReferenceVftbl.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.Runtime.CompilerServices;
+
+#pragma warning disable CS0649
+
+namespace WindowsRuntime.InteropServices;
+
+/// <summary>
+/// Binding type for the <c>IReference`1</c> interface vtable.
+/// </summary>
+/// <see href="https://learn.microsoft.com/uwp/api/windows.foundation.ireference-1"/>
+internal unsafe struct IReferenceVftbl
+{
+    public delegate* unmanaged[MemberFunction]<void*, Guid*, void**, HRESULT> QueryInterface;
+    public delegate* unmanaged[MemberFunction]<void*, uint> AddRef;
+    public delegate* unmanaged[MemberFunction]<void*, uint> Release;
+    public delegate* unmanaged[MemberFunction]<void*, uint*, Guid**, HRESULT> GetIids;
+    public delegate* unmanaged[MemberFunction]<void*, HSTRING*, HRESULT> GetRuntimeClassName;
+    public delegate* unmanaged[MemberFunction]<void*, TrustLevel*, HRESULT> GetTrustLevel;
+    public delegate* unmanaged[MemberFunction]<void*, void*, HRESULT> Value;
+
+    /// <summary>
+    /// Gets the type that is represented as an <c>IPropertyValue</c>.
+    /// </summary>
+    /// <param name="thisPtr">The target COM object.</param>
+    /// <param name="value">The resulting value.</param>
+    /// <returns>The <c>HRESULT</c> for the operation.</returns>
+    /// <remarks>
+    /// This method is only meant to be used for <c>IReference`1</c> instantiations of delegate types.
+    /// </remarks>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static HRESULT ValueUnsafe(void* thisPtr, void** value)
+    {
+        return ((IReferenceVftbl*)thisPtr)->Value(thisPtr, value);
+    }
+}
