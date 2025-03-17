f51b158c16f43c5f444790f6dc0ec6dd6c616f1e
Sergio Pedri <sergio0694@live.com>
2025-03-16 22:56:48 -0700

    Add 'WindowsRuntimeComWrappersCallback'

---
+++ src/WinRT.Runtime2/InteropServices/WindowsRuntimeComWrappersCallback.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using WindowsRuntime.InteropServices.Marshalling;
+
+namespace WindowsRuntime.InteropServices;
+
+/// <summary>
+/// A callback helper for <see cref="IWindowsRuntimeComWrappersCallback"/>.
+/// </summary>
+internal abstract unsafe class WindowsRuntimeComWrappersCallback
+{
+    /// <summary>
+    /// Resolves a <see cref="WindowsRuntimeComWrappersCallback"/> instance for a given <see cref="IWindowsRuntimeComWrappersCallback"/> implementation.
+    /// </summary>
+    /// <typeparam name="TCallback">The callback type.</typeparam>
+    /// <returns>The <see cref="WindowsRuntimeComWrappersCallback"/> instance for <typeparamref name="TCallback"/>.</returns>
+    public static WindowsRuntimeComWrappersCallback GetInstance<TCallback>()
+        where TCallback : IWindowsRuntimeComWrappersCallback, allows ref struct
+    {
+        return WindowsRuntimeComWrappersCallbackHost<TCallback>.Instance;
+    }
+
+    /// <inheritdoc cref="IWindowsRuntimeComWrappersCallback.CreateObject"/>
+    public abstract object CreateObject(void* value);
+}
+
+/// <summary>
+/// A callback host for <see cref="IWindowsRuntimeComWrappersCallback"/>.
+/// </summary>
+/// <typeparam name="TCallback">The callback type.</typeparam>
+file sealed class WindowsRuntimeComWrappersCallbackHost<TCallback> : WindowsRuntimeComWrappersCallback
+    where TCallback : IWindowsRuntimeComWrappersCallback, allows ref struct
+{
+    /// <summary>
+    /// The singleton instance wrapping <typeparamref name="TCallback"/>.
+    /// </summary>
+    public static readonly WindowsRuntimeComWrappersCallbackHost<TCallback> Instance = new();
+
+    /// <inheritdoc/>
+    public override unsafe object CreateObject(void* value)
+    {
+        return TCallback.CreateObject(value);
+    }
+}
