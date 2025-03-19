389d1234765661ad773bf076265ac7c4c484ad56
Sergio Pedri <sergio0694@live.com>
2025-03-18 23:05:58 -0700

    Port and update 'Point' type

---
+++ src/WinRT.Runtime2/Windows.Foundation/Point.cs
@@ -0,0 +1,143 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+
+namespace Windows.Foundation;
+
+/// <summary>
+/// epresents x- and y-coordinate values that define a point in a two-dimensional plane.
+/// </summary>
+/// <see href="https://learn.microsoft.com/uwp/api/windows.foundation.point"/>
+public struct Point : IEquatable<Point>, IFormattable
+{
+    /// <summary>
+    /// The horizontal position of the point.
+    /// </summary>
+    public float X;
+
+    /// <summary>
+    /// The vertical position of the point.
+    /// </summary>
+    public float Y;
+
+    /// <summary>
+    /// Creates a new <see cref="Point"/> value with the specified parameters.
+    /// </summary>
+    /// <param name="x">The horizontal position of the point.</param>
+    /// <param name="y">The vertical position of the point.</param>
+    public Point(float x, float y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    /// <summary>
+    /// Deconstructs the current <see cref="Point"/> value into its components.
+    /// </summary>
+    /// <param name="x">The resulting horizontal position.</param>
+    /// <param name="y">The resulting vertical position.</param>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public readonly void Deconstruct(out float x, out float y)
+    {
+        x = X;
+        y = Y;
+    }
+
+    /// <inheritdoc/>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public readonly bool Equals(Point other)
+    {
+        // Defer to 'Vector2.Equals', which is very well optimized.
+        // This results in a branch-free codegen via SIMD intrinsics.
+        return Unsafe.BitCast<Point, Vector2>(this).Equals(Unsafe.BitCast<Point, Vector2>(other));
+    }
+
+    /// <inheritdoc/>
+    public override readonly bool Equals([NotNullWhen(true)] object? obj)
+    {
+        return obj is Point other && Equals(other);
+    }
+
+    /// <inheritdoc/>
+    public override readonly int GetHashCode()
+    {
+        return HashCode.Combine(X, Y);
+    }
+
+    /// <inheritdoc/>
+    public override readonly string ToString()
+    {
+        char separator = GetNumericListSeparator(null);
+
+        return $"{X}{separator}{Y}";
+    }
+
+    /// <inheritdoc/>
+    public readonly string ToString(string? format, IFormatProvider? formatProvider)
+    {
+        // Fast path if both arguments are 'null'
+        if (format is null && formatProvider is null)
+        {
+            return ToString();
+        }
+
+        // We need the separator as a 'string', so we can pass it as a literal
+        string separator = GetNumericListSeparator(formatProvider) is ',' ? "," : ";";
+
+        DefaultInterpolatedStringHandler handler = new(
+            literalLength: 1,
+            formattedCount: 2,
+            provider: formatProvider,
+            initialBuffer: stackalloc char[128]);
+
+        handler.AppendFormatted(X, format);
+        handler.AppendLiteral(separator);
+        handler.AppendFormatted(Y, format);
+
+        return handler.ToStringAndClear();
+    }
+
+    /// <summary>
+    /// Gets the numeric list separator for a given provider.
+    /// </summary>
+    /// <param name="provider">The input provider to use for formatting.</param>
+    /// <returns>The numeric list separator to use.</returns>
+    private static char GetNumericListSeparator(IFormatProvider? provider)
+    {
+        const char CommaSeparator = ',';
+        const char SemicolonSeparator = ';';
+
+        NumberFormatInfo numberFormat = NumberFormatInfo.GetInstance(provider);
+
+        // If the decimal separator is ',', use ';', otherwise use ','
+        return
+            (numberFormat.NumberDecimalSeparator.Length > 0) &&
+            (numberFormat.NumberDecimalSeparator[0] == CommaSeparator)
+            ? SemicolonSeparator
+            : CommaSeparator;
+    }
+
+    /// <summary>
+    /// Returns a value that indicates whether each pair of elements in two specified values is equal.
+    /// </summary>
+    /// <param name="left">The first value to compare.</param>
+    /// <param name="right">The second value to compare.</param>
+    /// <returns><see langword="true"/> if <paramref name="left"/> and <paramref name="right"/> are equal, otherwise <see langword="false"/>.</returns>
+    /// <remarks>Two <see cref="Point"/> values are equal if each component in <paramref name="left"/> is equal to the corresponding component in <paramref name="right"/>.</remarks>
+    public static bool operator ==(Point left, Point right) => left.Equals(right);
+
+    /// <summary>
+    /// Returns a value that indicates whether either pair of elements in two specified values is not equal.
+    /// </summary>
+    /// <param name="left">The first value to compare.</param>
+    /// <param name="right">The second value to compare.</param>
+    /// <returns><see langword="true"/> if <paramref name="left"/> and <paramref name="right"/> are not equal, otherwise <see langword="false"/>.</returns>
+    /// <remarks>Two <see cref="Point"/> values are equal if each component in <paramref name="left"/> is equal to the corresponding component in <paramref name="right"/>.</remarks>
+    public static bool operator !=(Point left, Point right) => !left.Equals(right);
+}
