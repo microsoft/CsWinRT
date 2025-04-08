521f591bcf63f1acf2883d63e31d4be5bbd8fa1b
Manodasan Wignarajah <mawign@microsoft.com>
2025-04-07 19:20:24 -0700

    Fix KeyValuePair pointer reuse issue from IInspectable getting disposed before returned KeyValuePair (#1967)

--- src/WinRT.Runtime/Projections/KeyValuePair.cs
+++ src/WinRT.Runtime/Projections/KeyValuePair.cs
@@ -152,7 +152,7 @@ namespace ABI.System.Collections.Generic
 #if NET
             ComWrappersSupport.RegisterTypedRcwFactory(
                 typeof(global::System.Collections.Generic.KeyValuePair<K, V>),
-                KeyValuePair<K, V>.CreateRcw);
+                ComWrappersSupport.CreateReferenceCachingFactory(KeyValuePair<K, V>.CreateRcw));
 #endif
             KeyValuePairMethods<K, V>._RcwHelperInitialized = true;
             return true;
