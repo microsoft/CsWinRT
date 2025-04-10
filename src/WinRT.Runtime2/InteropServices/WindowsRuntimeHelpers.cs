13a29114626575a541c24fc98ebe4c679f06dd26
Sergio Pedri <sergio0694@live.com>
2025-04-09 23:22:20 -0700

    Remove leftover 'WindowsRuntimeHelpers' helpers

--- src/WinRT.Runtime2/InteropServices/WindowsRuntimeHelpers.cs
+++ src/WinRT.Runtime2/InteropServices/WindowsRuntimeHelpers.cs
@@ -1,9 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-using System;
-using System.Runtime.CompilerServices;
-
 #pragma warning disable CS8909
 
 namespace WindowsRuntime.InteropServices;
@@ -13,82 +10,6 @@ namespace WindowsRuntime.InteropServices;
 /// </summary>
 public static unsafe class WindowsRuntimeHelpers
 {
-    /// <summary>
-    /// Allocates an initializes an <c>IUnknown</c> vtable that's associated with a given type.
-    /// </summary>
-    /// <param name="type">The type associated with the allocated memory.</param>
-    /// <param name="count">The number of total entries in the resulting vtable.</param>
-    /// <returns>The resulting <c>IUnknown</c> vtable.</returns>
-    /// <exception cref="ArgumentNullException">Thrown if <paramref name="type"/> is <see langword="null"/>.</exception>
-    /// <exception cref="ArgumentOutOfRangeException">Thrown if <paramref name="count"/> is less than <c>3</c> (the size of <c>IUnknown</c>).</exception>
-    public static void** AllocateTypeAssociatedUnknownVtable(Type type, int count)
-    {
-        ArgumentOutOfRangeException.ThrowIfLessThan(count, 3);
-
-        void** vftbl = (void**)RuntimeHelpers.AllocateTypeAssociatedMemory(type, sizeof(void*) * count);
-
-        Unsafe.Copy(vftbl, in *(IUnknownVftbl*)IUnknownImpl.AbiToProjectionVftablePtr);
-
-        return vftbl;
-    }
-
-    /// <summary>
-    /// Allocates an initializes an <c>IUnknown</c> vtable that's associated with a given type, with additional vtable entries.
-    /// </summary>
-    /// <param name="type">The type associated with the allocated memory.</param>
-    /// <param name="fpEntry3">The function pointer at index <c>3</c> in the vtable.</param>
-    /// <returns>The resulting <c>IUnknown</c> vtable.</returns>
-    /// <remarks>
-    /// It is the caller's responsibility to ensure the additional vtable entry is valid and has the correct signature for the resulting vtable.
-    /// </remarks>
-    /// <exception cref="ArgumentNullException">Thrown if <paramref name="type"/> is <see langword="null"/>.</exception>
-    public static void** AllocateTypeAssociatedUnknownVtableUnsafe(Type type, void* fpEntry3)
-    {
-        void** vftbl = AllocateTypeAssociatedInspectableVtable(type, 4);
-
-        vftbl[3] = fpEntry3;
-
-        return vftbl;
-    }
-
-    /// <summary>
-    /// Allocates an initializes an <c>IInspectable</c> vtable that's associated with a given type.
-    /// </summary>
-    /// <param name="type">The type associated with the allocated memory.</param>
-    /// <param name="count">The number of total entries in the resulting vtable.</param>
-    /// <returns>The resulting <c>IInspectable</c> vtable.</returns>
-    /// <exception cref="ArgumentNullException">Thrown if <paramref name="type"/> is <see langword="null"/>.</exception>
-    /// <exception cref="ArgumentOutOfRangeException">Thrown if <paramref name="count"/> is less than <c>6</c> (the size of <c>IInspectable</c>).</exception>
-    public static void** AllocateTypeAssociatedInspectableVtable(Type type, int count)
-    {
-        ArgumentOutOfRangeException.ThrowIfLessThan(count, 6);
-
-        void** vftbl = (void**)RuntimeHelpers.AllocateTypeAssociatedMemory(type, sizeof(void*) * count);
-
-        Unsafe.Copy(vftbl, in *(IInspectableVftbl*)IInspectableImpl.AbiToProjectionVftablePtr);
-
-        return vftbl;
-    }
-
-    /// <summary>
-    /// Allocates an initializes an <c>IInspectable</c> vtable that's associated with a given type, with additional vtable entries.
-    /// </summary>
-    /// <param name="type">The type associated with the allocated memory.</param>
-    /// <param name="fpEntry6">The function pointer at index <c>6</c> in the vtable.</param>
-    /// <returns>The resulting <c>IInspectable</c> vtable.</returns>
-    /// <remarks>
-    /// It is the caller's responsibility to ensure the additional vtable entry is valid and has the correct signature for the resulting vtable.
-    /// </remarks>
-    /// <exception cref="ArgumentNullException">Thrown if <paramref name="type"/> is <see langword="null"/>.</exception>
-    public static void** AllocateTypeAssociatedInspectableVtableUnsafe(Type type, void* fpEntry6)
-    {
-        void** vftbl = AllocateTypeAssociatedInspectableVtable(type, 7);
-
-        vftbl[6] = fpEntry6;
-
-        return vftbl;
-    }
-
     /// <summary>
     /// Checks whether a pointer to a COM object is actually a reference to a CCW produced for a managed object that was marshalled to native code.
     /// </summary>
