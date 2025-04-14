8f4ec3d14a46913ed08870ecab90842c417ae847
Sergio Pedri <sergio0694@live.com>
2025-04-13 23:02:07 -0700

    Add 'IDisposable' projection draft

---
+++ src/WinRT.Runtime2/ABI.System/IDisposable.cs
@@ -0,0 +1,89 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using WindowsRuntime.InteropServices;
+using WindowsRuntime.InteropServices.Marshalling;
+using static System.Runtime.InteropServices.ComWrappers;
+
+#pragma warning disable CS0649
+
+namespace ABI.System;
+
+/// <summary>
+/// Binding type for <see cref="global::System.IDisposable"/>.
+/// </summary>
+internal unsafe struct IDisposableVftbl
+{
+    public delegate* unmanaged[MemberFunction]<void*, Guid*, void**, HRESULT> QueryInterface;
+    public delegate* unmanaged[MemberFunction]<void*, uint> AddRef;
+    public delegate* unmanaged[MemberFunction]<void*, uint> Release;
+    public delegate* unmanaged[MemberFunction]<void*, uint*, Guid**, HRESULT> GetIids;
+    public delegate* unmanaged[MemberFunction]<void*, HSTRING*, HRESULT> GetRuntimeClassName;
+    public delegate* unmanaged[MemberFunction]<void*, TrustLevel*, HRESULT> GetTrustLevel;
+    public delegate* unmanaged[MemberFunction]<void*, HRESULT> Close;
+}
+
+/// <summary>
+/// The <see cref="global::System.IDisposable"/> implementation.
+/// </summary>
+[EditorBrowsable(EditorBrowsableState.Never)]
+public static unsafe class IDisposableImpl
+{
+    /// <summary>
+    /// The <see cref="IDisposableVftbl"/> value for the managed <see cref="global::System.IDisposable"/> implementation.
+    /// </summary>
+    [FixedAddressValueType]
+    private static readonly IDisposableVftbl Vftbl;
+
+    /// <summary>
+    /// Initializes <see cref="Vftbl"/>.
+    /// </summary>
+    static IDisposableImpl()
+    {
+        *(IInspectableVftbl*)Unsafe.AsPointer(ref Vftbl) = *(IInspectableVftbl*)IInspectableImpl.AbiToProjectionVftablePtr;
+
+        Vftbl.Close = &Close;
+    }
+
+    /// <summary>
+    /// Gets a pointer to the managed <see cref="global::System.IDisposable"/> implementation.
+    /// </summary>
+    public static nint AbiToProjectionVftablePtr
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get => (nint)Unsafe.AsPointer(ref Unsafe.AsRef(in Vftbl));
+    }
+
+    /// <see href="https://learn.microsoft.com/uwp/api/windows.foundation.iclosable.close"/>
+    [UnmanagedCallersOnly(CallConvs = [typeof(CallConvMemberFunction)])]
+    private static HRESULT Close(void* thisPtr)
+    {
+        try
+        {
+            ComInterfaceDispatch.GetInstance<global::System.IDisposable>((ComInterfaceDispatch*)thisPtr).Dispose();
+
+            return WellKnownErrorCodes.S_OK;
+        }
+        catch (global::System.Exception e)
+        {
+            return RestrictedErrorInfoExceptionMarshaller.ConvertToUnmanaged(e);
+        }
+    }
+}
+
+/// <summary>
+/// The <see cref="IDynamicInterfaceCastable"/> implementation for <see cref="global::System.IDisposable"/>.
+/// </summary>
+[DynamicInterfaceCastableImplementation]
+internal unsafe interface IDisposable : global::System.IDisposable
+{
+    /// <inheritdoc/>
+    void global::System.IDisposable.Dispose()
+    {
+        // TODO
+    }
+}
