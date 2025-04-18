72e340a22a4e1fb41b1d6219f571537fb273d871
Sergio Pedri <sergio0694@live.com>
2025-04-18 16:33:03 -0700

    Add 'Size' ABI type

---
+++ src/WinRT.Runtime2/ABI.Windows.Foundation/Size.cs
@@ -0,0 +1,182 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Windows.Foundation;
+using WindowsRuntime.InteropServices;
+using WindowsRuntime.InteropServices.Marshalling;
+using static System.Runtime.InteropServices.ComWrappers;
+
+#pragma warning disable CS0649
+
+[assembly: TypeMap<WindowsRuntimeTypeMapUniverse>(
+    value: "Windows.Foundation.IReference<Windows.Foundation.Size>",
+    target: typeof(Size),
+    trimTarget: typeof(Size))]
+
+namespace ABI.Windows.Foundation;
+
+/// <summary>
+/// Marshaller for <see cref="Size"/>.
+/// </summary>
+[EditorBrowsable(EditorBrowsableState.Never)]
+public static unsafe class SizeMarshaller
+{
+    /// <inheritdoc cref="WindowsRuntimeValueTypeMarshaller.BoxToUnmanaged"/>
+    public static WindowsRuntimeObjectReferenceValue BoxToUnmanaged(Size? value)
+    {
+        return WindowsRuntimeValueTypeMarshaller.BoxToUnmanaged(value, in WellKnownInterfaceIds.IID_IReferenceOfSize);
+    }
+
+    /// <inheritdoc cref="WindowsRuntimeValueTypeMarshaller.UnboxToManaged(void*)"/>
+    public static Size? UnboxToManaged(void* value)
+    {
+        return WindowsRuntimeValueTypeMarshaller.UnboxToManaged<Size>(value);
+    }
+}
+
+/// <summary>
+/// The set of <see cref="ComInterfaceEntry"/> values for <see cref="Size"/>.
+/// </summary>
+file struct SizeInterfaceEntries
+{
+    public ComInterfaceEntry IReferenceOfSize;
+    public ComInterfaceEntry IStringable;
+    public ComInterfaceEntry IWeakReferenceSource;
+    public ComInterfaceEntry IMarshal;
+    public ComInterfaceEntry IAgileObject;
+    public ComInterfaceEntry IInspectable;
+    public ComInterfaceEntry IUnknown;
+}
+
+/// <summary>
+/// The implementation of <see cref="SizeInterfaceEntries"/>.
+/// </summary>
+file static class SizeInterfaceEntriesImpl
+{
+    /// <summary>
+    /// The <see cref="SizeInterfaceEntries"/> value for <see cref="Size"/>.
+    /// </summary>
+    [FixedAddressValueType]
+    public static readonly SizeInterfaceEntries Entries;
+
+    /// <summary>
+    /// Initializes <see cref="Entries"/>.
+    /// </summary>
+    static SizeInterfaceEntriesImpl()
+    {
+        Entries.IReferenceOfSize.IID = WellKnownInterfaceIds.IID_IReferenceOfSize;
+        Entries.IReferenceOfSize.Vtable = SizeReferenceImpl.Vtable;
+        Entries.IStringable.IID = WellKnownInterfaceIds.IID_IStringable;
+        Entries.IStringable.Vtable = IStringableImpl.Vtable;
+        Entries.IWeakReferenceSource.IID = WellKnownInterfaceIds.IID_IWeakReferenceSource;
+        Entries.IWeakReferenceSource.Vtable = IWeakReferenceSourceImpl.Vtable;
+        Entries.IMarshal.IID = WellKnownInterfaceIds.IID_IMarshal;
+        Entries.IMarshal.Vtable = IMarshalImpl.Vtable;
+        Entries.IAgileObject.IID = WellKnownInterfaceIds.IID_IAgileObject;
+        Entries.IAgileObject.Vtable = IUnknownImpl.Vtable;
+        Entries.IInspectable.IID = WellKnownInterfaceIds.IID_IInspectable;
+        Entries.IInspectable.Vtable = IInspectableImpl.Vtable;
+        Entries.IUnknown.IID = WellKnownInterfaceIds.IID_IUnknown;
+        Entries.IUnknown.Vtable = IUnknownImpl.Vtable;
+    }
+}
+
+/// <summary>
+/// A custom <see cref="WindowsRuntimeComWrappersMarshallerAttribute"/> implementation for <see cref="Size"/>.
+/// </summary>
+internal sealed unsafe class SizeComWrappersMarshallerAttribute : WindowsRuntimeComWrappersMarshallerAttribute
+{
+    /// <inheritdoc/>
+    public override void* GetOrCreateComInterfaceForObject(object value)
+    {
+        return (void*)WindowsRuntimeComWrappers.Default.GetOrCreateComInterfaceForObject(value, CreateComInterfaceFlags.None);
+    }
+
+    /// <inheritdoc/>
+    public override ComInterfaceEntry* ComputeVtables(out int count)
+    {
+        count = sizeof(SizeInterfaceEntries) / sizeof(ComInterfaceEntry);
+
+        return (ComInterfaceEntry*)Unsafe.AsPointer(ref Unsafe.AsRef(in SizeInterfaceEntriesImpl.Entries));
+    }
+
+    /// <inheritdoc/>
+    public override object CreateObject(void* value)
+    {
+        return WindowsRuntimeValueTypeMarshaller.UnboxToManagedUnsafe<Size>(value, in WellKnownInterfaceIds.IID_IReferenceOfSize);
+    }
+}
+
+/// <summary>
+/// Binding type for the <c>IReference`1</c> implementation for <see cref="Size"/>.
+/// </summary>
+file unsafe struct SizeReferenceVftbl
+{
+    public delegate* unmanaged[MemberFunction]<void*, Guid*, void**, HRESULT> QueryInterface;
+    public delegate* unmanaged[MemberFunction]<void*, uint> AddRef;
+    public delegate* unmanaged[MemberFunction]<void*, uint> Release;
+    public delegate* unmanaged[MemberFunction]<void*, uint*, Guid**, HRESULT> GetIids;
+    public delegate* unmanaged[MemberFunction]<void*, HSTRING*, HRESULT> GetRuntimeClassName;
+    public delegate* unmanaged[MemberFunction]<void*, TrustLevel*, HRESULT> GetTrustLevel;
+    public delegate* unmanaged[MemberFunction]<void*, Size*, HRESULT> Value;
+}
+
+/// <summary>
+/// The <c>IReference`1</c> implementation for <see cref="Size"/>.
+/// </summary>
+file static unsafe class SizeReferenceImpl
+{
+    /// <summary>
+    /// The <see cref="SizeReferenceVftbl"/> value for the managed <c>IReference`1</c> implementation.
+    /// </summary>
+    [FixedAddressValueType]
+    private static readonly SizeReferenceVftbl Vftbl;
+
+    /// <summary>
+    /// Initializes <see cref="Vftbl"/>.
+    /// </summary>
+    static SizeReferenceImpl()
+    {
+        *(IInspectableVftbl*)Unsafe.AsPointer(ref Vftbl) = *(IInspectableVftbl*)IInspectableImpl.Vtable;
+
+        Vftbl.Value = &Value;
+    }
+
+    /// <summary>
+    /// Gets a Sizeer to the managed <c>IReference`1</c> implementation.
+    /// </summary>
+    public static nint Vtable
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get => (nint)Unsafe.AsPointer(ref Unsafe.AsRef(in Vftbl));
+    }
+
+    /// <see href="https://learn.microsoft.com/uwp/api/windows.foundation.ireference-1.value"/>
+    [UnmanagedCallersOnly(CallConvs = [typeof(CallConvMemberFunction)])]
+    private static HRESULT Value(void* thisPtr, Size* result)
+    {
+        if (result is null)
+        {
+            return WellKnownErrorCodes.E_POINTER;
+        }
+
+        try
+        {
+            Size unboxedValue = (Size)ComInterfaceDispatch.GetInstance<object>((ComInterfaceDispatch*)thisPtr);
+
+            Unsafe.WriteUnaligned(result, unboxedValue);
+
+            return WellKnownErrorCodes.S_OK;
+        }
+        catch (Exception e)
+        {
+            Unsafe.WriteUnaligned(result, default(Size));
+
+            return RestrictedErrorInfoExceptionMarshaller.ConvertToUnmanaged(e);
+        }
+    }
+}
