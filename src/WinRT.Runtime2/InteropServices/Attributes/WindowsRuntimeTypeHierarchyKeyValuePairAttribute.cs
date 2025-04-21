0556c5fa182eb9d47ea37a1ddee1e74ce521e03a
Sergio Pedri <sergio0694@live.com>
2025-04-21 13:33:51 -0700

    Add 'WindowsRuntimeTypeHierarchyKeyValuePairAttribute'

---
+++ src/WinRT.Runtime2/InteropServices/Attributes/WindowsRuntimeTypeHierarchyKeyValuePairAttribute.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+
+namespace WindowsRuntime.InteropServices;
+
+/// <summary>
+/// Indicates a key value pair in the global hierarchy lookup to use to create managed instances of derived Windows Runtime types.
+/// </summary>
+[AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true, Inherited = false)]
+public sealed class WindowsRuntimeTypeHierarchyKeyValuePairAttribute : Attribute
+{
+    /// <summary>
+    /// Creates a new <see cref="WindowsRuntimeTypeHierarchyKeyValuePairAttribute"/> instance with the specified parameters.
+    /// </summary>
+    /// <param name="runtimeClassName">The runtime class name of the input Windows Runtime type.</param>
+    /// <param name="baseRuntimeClassName">The resulting runtime class name of the base type for <paramref name="baseRuntimeClassName"/>.</param>
+    public WindowsRuntimeTypeHierarchyKeyValuePairAttribute(string runtimeClassName, string baseRuntimeClassName)
+    {
+        RuntimeClassName = runtimeClassName;
+        BaseRuntimeClassName = baseRuntimeClassName;
+    }
+
+    /// <summary>
+    /// Gets the runtime class name of the input Windows Runtime type.
+    /// </summary>
+    public string RuntimeClassName { get; }
+
+    /// <summary>
+    /// Gets the resulting runtime class name of the base type for <see cref="RuntimeClassName"/>.
+    /// </summary>
+    public string BaseRuntimeClassName { get; }
+}
