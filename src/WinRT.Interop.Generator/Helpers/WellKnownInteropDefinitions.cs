794e3fb603310cc45e02098758f09ff803e30153
Sergio Pedri <sergio0694@live.com>
2025-04-27 17:29:44 -0700

    Add more 'WellKnown' types

---
+++ src/WinRT.Interop.Generator/Helpers/WellKnownInteropDefinitions.cs
@@ -0,0 +1,72 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Diagnostics.CodeAnalysis;
+using AsmResolver.DotNet;
+using WindowsRuntime.InteropGenerator.Factories;
+
+namespace WindowsRuntime.InteropGenerator.References;
+
+/// <summary>
+/// Well known definitions to APIs from the interop assembly being produced.
+/// </summary>
+internal sealed class WellKnownInteropDefinitions
+{
+    /// <summary>
+    /// The <see cref="ModuleDefinition"/> for the interop assembly being produced.
+    /// </summary>
+    private readonly ModuleDefinition _interopModule;
+
+    /// <summary>
+    /// Creates a new <see cref="WellKnownInteropReferences"/> instance.
+    /// </summary>
+    /// <param name="interopModule">The <see cref="ModuleDefinition"/> for the interop assembly being produced.</param>
+    public WellKnownInteropDefinitions(ModuleDefinition interopModule)
+    {
+        _interopModule = interopModule;
+    }
+
+    /// <summary>
+    /// Gets the <see cref="TypeDefinition"/> for the <c>ccRvaFields</c> type.
+    /// </summary>
+    /// <remarks>
+    /// This type has exactly one nested type, for RVA fields of size 16 (ie. <see cref="System.Guid"/>).
+    /// </remarks>
+    [field: MaybeNull, AllowNull]
+    public TypeDefinition RvaFields => field ??= WellKnownTypeDefinitionFactory.RvaFields(_interopModule.DefaultImporter);
+
+    /// <summary>
+    /// Gets the <see cref="TypeDefinition"/> for the <c>IIDRvaDataSize=16</c> type.
+    /// </summary>
+    public TypeDefinition IIDRvaDataSize_16 => RvaFields.NestedTypes[0];
+
+    /// <summary>
+    /// Gets the <see cref="TypeDefinition"/> for the <c>IUnknownVftbl</c> type.
+    /// </summary>
+    [field: MaybeNull, AllowNull]
+    public TypeDefinition IUnknownVftbl => field ??= WellKnownTypeDefinitionFactory.IUnknownVftbl(_interopModule.CorLibTypeFactory, _interopModule.DefaultImporter);
+
+    /// <summary>
+    /// Gets the <see cref="TypeDefinition"/> for the <c>IInspectableVftbl</c> type.
+    /// </summary>
+    [field: MaybeNull, AllowNull]
+    public TypeDefinition IInspectableVftbl => field ??= WellKnownTypeDefinitionFactory.IInspectableVftbl(_interopModule.CorLibTypeFactory, _interopModule.DefaultImporter);
+
+    /// <summary>
+    /// Gets the <see cref="TypeDefinition"/> for the <c>DelegateVftbl</c> type.
+    /// </summary>
+    [field: MaybeNull, AllowNull]
+    public TypeDefinition DelegateVftbl => field ??= WellKnownTypeDefinitionFactory.DelegateVftbl(_interopModule.CorLibTypeFactory, _interopModule.DefaultImporter);
+
+    /// <summary>
+    /// Gets the <see cref="TypeDefinition"/> for the <c>DelegateReferenceVftbl</c> type.
+    /// </summary>
+    [field: MaybeNull, AllowNull]
+    public TypeDefinition DelegateReferenceVftbl => field ??= WellKnownTypeDefinitionFactory.DelegateReferenceVftbl(_interopModule.CorLibTypeFactory, _interopModule.DefaultImporter);
+
+    /// <summary>
+    /// Gets the <see cref="TypeDefinition"/> for the <c>DelegateInterfaceEntries</c> type.
+    /// </summary>
+    [field: MaybeNull, AllowNull]
+    public TypeDefinition DelegateInterfaceEntries => field ??= WellKnownTypeDefinitionFactory.DelegateInterfaceEntriesType(_interopModule.DefaultImporter);
+}
---
+++ src/WinRT.Interop.Generator/Helpers/WellKnownInteropReferences.cs
@@ -0,0 +1,103 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Diagnostics.CodeAnalysis;
+using AsmResolver.DotNet;
+using AsmResolver.DotNet.Signatures;
+
+namespace WindowsRuntime.InteropGenerator.References;
+
+/// <summary>
+/// Well known references to APIs from the Windows Runtime assembly.
+/// </summary>
+internal sealed class WellKnownInteropReferences
+{
+    /// <summary>
+    /// The <see cref="ModuleDefinition"/> for the Windows Runtime assembly.
+    /// </summary>
+    private readonly ModuleDefinition _windowsRuntimeModule;
+
+    /// <summary>
+    /// Creates a new <see cref="WellKnownInteropReferences"/> instance.
+    /// </summary>
+    /// <param name="windowsRuntimeModule">The <see cref="ModuleDefinition"/> for the Windows Runtime assembly.</param>
+    public WellKnownInteropReferences(ModuleDefinition windowsRuntimeModule)
+    {
+        _windowsRuntimeModule = windowsRuntimeModule;
+    }
+
+    /// <summary>
+    /// Gets the <see cref="TypeReference"/> for <c>WindowsRuntime.InteropServices.IUnknownImpl</c>.
+    /// </summary>
+    [field: MaybeNull, AllowNull]
+    public TypeReference IUnknownImpl => field ??= _windowsRuntimeModule.CreateTypeReference("WindowsRuntime.InteropServices", "IUnknownImpl");
+
+    /// <summary>
+    /// Gets the <see cref="TypeReference"/> for <c>WindowsRuntime.InteropServices.IInspectableImpl</c>.
+    /// </summary>
+    [field: MaybeNull, AllowNull]
+    public TypeReference IInspectableImpl => field ??= _windowsRuntimeModule.CreateTypeReference("WindowsRuntime.InteropServices", "IInspectableImpl");
+
+    /// <summary>
+    /// Gets the <see cref="TypeReference"/> for <c>WindowsRuntime.InteropServices.IStringableImpl</c>.
+    /// </summary>
+    [field: MaybeNull, AllowNull]
+    public TypeReference IStringableImpl => field ??= _windowsRuntimeModule.CreateTypeReference("WindowsRuntime.InteropServices", "IStringableImpl");
+
+    /// <summary>
+    /// Gets the <see cref="TypeReference"/> for <c>WindowsRuntime.InteropServices.IMarshalImpl</c>.
+    /// </summary>
+    [field: MaybeNull, AllowNull]
+    public TypeReference IMarshalImpl => field ??= _windowsRuntimeModule.CreateTypeReference("WindowsRuntime.InteropServices", "IMarshalImpl");
+
+    /// <summary>
+    /// Gets the <see cref="TypeReference"/> for <c>WindowsRuntime.InteropServices.IWeakReferenceSourceImpl</c>.
+    /// </summary>
+    [field: MaybeNull, AllowNull]
+    public TypeReference IWeakReferenceSourceImpl => field ??= _windowsRuntimeModule.CreateTypeReference("WindowsRuntime.InteropServices", "IWeakReferenceSourceImpl");
+
+    /// <summary>
+    /// Gets the <see cref="MemberReference"/> for <c>WindowsRuntime.InteropServices.IUnknownImpl.get_Vtable()</c>.
+    /// </summary>
+    [field: MaybeNull, AllowNull]
+    public MemberReference IUnknownImplget_Vtable => field ??= IUnknownImpl
+        .CreateMemberReference("get_Vtable", MethodSignature.CreateStatic(
+            returnType: _windowsRuntimeModule.CorLibTypeFactory.IntPtr,
+            parameterTypes: []));
+
+    /// <summary>
+    /// Gets the <see cref="MemberReference"/> for <c>WindowsRuntime.InteropServices.IInspectableImpl.get_Vtable()</c>.
+    /// </summary>
+    [field: MaybeNull, AllowNull]
+    public MemberReference IInspectableImplget_Vtable => field ??= IInspectableImpl
+        .CreateMemberReference("get_Vtable", MethodSignature.CreateStatic(
+            returnType: _windowsRuntimeModule.CorLibTypeFactory.IntPtr,
+            parameterTypes: []));
+
+    /// <summary>
+    /// Gets the <see cref="MemberReference"/> for <c>WindowsRuntime.InteropServices.IStringableImpl.get_Vtable()</c>.
+    /// </summary>
+    [field: MaybeNull, AllowNull]
+    public MemberReference IStringableImplget_Vtable => field ??= IStringableImpl
+        .CreateMemberReference("get_Vtable", MethodSignature.CreateStatic(
+            returnType: _windowsRuntimeModule.CorLibTypeFactory.IntPtr,
+            parameterTypes: []));
+
+    /// <summary>
+    /// Gets the <see cref="MemberReference"/> for <c>WindowsRuntime.InteropServices.IMarshalImpl.get_Vtable()</c>.
+    /// </summary>
+    [field: MaybeNull, AllowNull]
+    public MemberReference IMarshalImplget_Vtable => field ??= IMarshalImpl
+        .CreateMemberReference("get_Vtable", MethodSignature.CreateStatic(
+            returnType: _windowsRuntimeModule.CorLibTypeFactory.IntPtr,
+            parameterTypes: []));
+
+    /// <summary>
+    /// Gets the <see cref="MemberReference"/> for <c>WindowsRuntime.InteropServices.IWeakReferenceSourceImpl.get_Vtable()</c>.
+    /// </summary>
+    [field: MaybeNull, AllowNull]
+    public MemberReference IWeakReferenceSourceImplget_Vtable => field ??= IWeakReferenceSourceImpl
+        .CreateMemberReference("get_Vtable", MethodSignature.CreateStatic(
+            returnType: _windowsRuntimeModule.CorLibTypeFactory.IntPtr,
+            parameterTypes: []));
+}
