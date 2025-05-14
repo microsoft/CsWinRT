333d2d64d6934d87e07f54a68f3a7bec33456832
Sergio Pedri <sergio0694@live.com>
2025-05-13 20:21:37 -0700

    Add typed 'EventHandlerEventSource<,>' type

--- src/WinRT.Runtime2/Polyfills.cs
+++ src/WinRT.Runtime2/Polyfills.cs
@@ -3,40 +3,48 @@
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 
-namespace System.Runtime.InteropServices;
-
-[AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
-public sealed class TypeMapAttribute<TTypeMapGroup> : Attribute
+namespace System
 {
-    public TypeMapAttribute(string value, Type target) { }
+    public delegate void EventHandler<in TSender, in TEventArgs>(TSender sender, TEventArgs e)
+        where TSender : allows ref struct
+        where TEventArgs : allows ref struct;
 
-    //[RequiresUnreferencedCode("Interop types may be removed by trimming")]
-    public TypeMapAttribute(string value, Type target, Type trimTarget) { }
-}
-
-[AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
-public sealed class TypeMapAssemblyTargetAttribute<TTypeMapGroup> : Attribute
-{
-    public TypeMapAssemblyTargetAttribute(string assemblyName) { }
-}
-
-[AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
-public sealed class TypeMapAssociationAttribute<TTypeMapGroup> : Attribute
-{
-    public TypeMapAssociationAttribute(Type source, Type proxy) { }
-}
-
-public static class TypeMapping
-{
-    [RequiresUnreferencedCode("Interop types may be removed by trimming")]
-    public static IReadOnlyDictionary<string, Type> GetOrCreateExternalTypeMapping<TTypeMapGroup>()
+    namespace Runtime.InteropServices
     {
-        return null!;
-    }
+        [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
+        public sealed class TypeMapAttribute<TTypeMapGroup> : Attribute
+        {
+            public TypeMapAttribute(string value, Type target) { }
 
-    [RequiresUnreferencedCode("Interop types may be removed by trimming")]
-    public static IReadOnlyDictionary<Type, Type> GetOrCreateProxyTypeMapping<TTypeMapGroup>()
-    {
-        return null!;
+            //[RequiresUnreferencedCode("Interop types may be removed by trimming")]
+            public TypeMapAttribute(string value, Type target, Type trimTarget) { }
+        }
+
+        [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
+        public sealed class TypeMapAssemblyTargetAttribute<TTypeMapGroup> : Attribute
+        {
+            public TypeMapAssemblyTargetAttribute(string assemblyName) { }
+        }
+
+        [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
+        public sealed class TypeMapAssociationAttribute<TTypeMapGroup> : Attribute
+        {
+            public TypeMapAssociationAttribute(Type source, Type proxy) { }
+        }
+
+        public static class TypeMapping
+        {
+            [RequiresUnreferencedCode("Interop types may be removed by trimming")]
+            public static IReadOnlyDictionary<string, Type> GetOrCreateExternalTypeMapping<TTypeMapGroup>()
+            {
+                return null!;
+            }
+
+            [RequiresUnreferencedCode("Interop types may be removed by trimming")]
+            public static IReadOnlyDictionary<Type, Type> GetOrCreateProxyTypeMapping<TTypeMapGroup>()
+            {
+                return null!;
+            }
+        }
     }
 }
