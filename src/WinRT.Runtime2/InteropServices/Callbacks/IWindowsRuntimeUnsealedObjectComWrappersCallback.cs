9d027a64db2f6c5cd96a9ae38b75baf6c0b54b51
Sergio Pedri <sergio0694@live.com>
2025-05-16 16:38:09 -0700

    Add 'IWindowsRuntimeUnsealedObjectComWrappersCallback'

---
+++ src/WinRT.Runtime2/InteropServices/Callbacks/IWindowsRuntimeUnsealedObjectComWrappersCallback.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+
+namespace WindowsRuntime.InteropServices;
+
+/// <summary>
+/// An interface for callbacks for <see cref="ComWrappers.CreateObject"/>, for unsealed Windows Runtime objects.
+/// </summary>
+public unsafe interface IWindowsRuntimeUnsealedObjectComWrappersCallback
+{
+    /// <summary>
+    /// Creates a managed Windows Runtime object for a given native object of an unsealed type.
+    /// </summary>
+    /// <param name="value">The input native object to marshal.</param>
+    /// <param name="runtimeClassName">The runtime class name of the native object.</param>
+    /// <param name="wrapperObject">The resulting wrapper object, if it could be created.</param>
+    /// <param name="wrapperFlags">Flags used to describe the created wrapper object.</param>
+    /// <returns>Whether <paramref name="wrapperObject"/> could be created successfully.</returns>
+    /// <remarks>
+    /// <para>
+    /// Implementations of this method can use <paramref name="runtimeClassName"/> to determine whether the actual
+    /// runtime class name of <paramref name="value"/> matches the one they are implementing the marshalling logic
+    /// for. If so, they can optimize the marshalling operation by reusing the <paramref name="value"/> pointer.
+    /// </para>
+    /// <para>
+    /// The same semantics around <paramref name="value"/> apply as in <see cref="IWindowsRuntimeObjectComWrappersCallback.CreateObject"/>.
+    /// </para>
+    /// </remarks>
+    /// <seealso cref="IWindowsRuntimeObjectComWrappersCallback"/>
+    static abstract bool TryCreateObject(
+        void* value,
+        ReadOnlySpan<char> runtimeClassName,
+        [NotNullWhen(true)] out object? wrapperObject,
+        out CreatedWrapperFlags wrapperFlags);
+}
---
+++ src/WinRT.Runtime2/InteropServices/Callbacks/WindowsRuntimeUnsealedObjectComWrappersCallback.cs
@@ -0,0 +1,55 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+
+namespace WindowsRuntime.InteropServices;
+
+/// <summary>
+/// A callback helper for <see cref="IWindowsRuntimeUnsealedObjectComWrappersCallback"/>.
+/// </summary>
+internal abstract unsafe class WindowsRuntimeUnsealedObjectComWrappersCallback
+{
+    /// <summary>
+    /// Resolves a <see cref="WindowsRuntimeUnsealedObjectComWrappersCallback"/> instance for a given <see cref="IWindowsRuntimeUnsealedObjectComWrappersCallback"/> implementation.
+    /// </summary>
+    /// <typeparam name="TCallback">The callback type.</typeparam>
+    /// <returns>The <see cref="WindowsRuntimeUnsealedObjectComWrappersCallback"/> instance for <typeparamref name="TCallback"/>.</returns>
+    public static WindowsRuntimeUnsealedObjectComWrappersCallback GetInstance<TCallback>()
+        where TCallback : IWindowsRuntimeUnsealedObjectComWrappersCallback, allows ref struct
+    {
+        return WindowsRuntimeUnsealedObjectComWrappersCallbackHost<TCallback>.Instance;
+    }
+
+    /// <inheritdoc cref="IWindowsRuntimeUnsealedObjectComWrappersCallback.TryCreateObject"/>
+    public abstract bool CreateObject(
+        void* value,
+        ReadOnlySpan<char> runtimeClassName,
+        [NotNullWhen(true)] out object? wrapperObject,
+        out CreatedWrapperFlags wrapperFlags);
+}
+
+/// <summary>
+/// A callback host for <see cref="IWindowsRuntimeUnsealedObjectComWrappersCallback"/>.
+/// </summary>
+/// <typeparam name="TCallback">The callback type.</typeparam>
+file sealed class WindowsRuntimeUnsealedObjectComWrappersCallbackHost<TCallback> : WindowsRuntimeUnsealedObjectComWrappersCallback
+    where TCallback : IWindowsRuntimeUnsealedObjectComWrappersCallback, allows ref struct
+{
+    /// <summary>
+    /// The singleton instance wrapping <typeparamref name="TCallback"/>.
+    /// </summary>
+    public static readonly WindowsRuntimeUnsealedObjectComWrappersCallbackHost<TCallback> Instance = new();
+
+    /// <inheritdoc/>
+    public override unsafe bool CreateObject(
+        void* value,
+        ReadOnlySpan<char> runtimeClassName,
+        [NotNullWhen(true)] out object? wrapperObject,
+        out CreatedWrapperFlags wrapperFlags)
+    {
+        return TCallback.TryCreateObject(value, runtimeClassName, out wrapperObject, out wrapperFlags);
+    }
+}
