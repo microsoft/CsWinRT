ac4f607224a72cf53f05e124721f7e9b7ff39924
Sergio Pedri <sergio0694@live.com>
2025-05-15 22:47:09 -0700

    Update XML docs in 'InteropReferences'

--- src/WinRT.Interop.Generator/References/InteropReferences.cs
+++ src/WinRT.Interop.Generator/References/InteropReferences.cs
@@ -40,43 +40,43 @@ internal sealed class InteropReferences
     public CorLibTypeFactory CorLibTypeFactory => _interopModule.CorLibTypeFactory;
 
     /// <summary>
-    /// Gets the <see cref="TypeReference"/> for <c>System.Attribute</c>.
+    /// Gets the <see cref="TypeReference"/> for <see cref="System.Attribute"/>.
     /// </summary>
     [field: MaybeNull, AllowNull]
     public TypeReference Attribute => field ??= _interopModule.CorLibTypeFactory.CorLibScope.CreateTypeReference("System", "Attribute");
 
     /// <summary>
-    /// Gets the <see cref="TypeReference"/> for <c>System.AttributeTargets</c>.
+    /// Gets the <see cref="TypeReference"/> for <see cref="System.AttributeTargets"/>.
     /// </summary>
     [field: MaybeNull, AllowNull]
     public TypeReference AttributeTargets => field ??= _interopModule.CorLibTypeFactory.CorLibScope.CreateTypeReference("System", "AttributeTargets");
 
     /// <summary>
-    /// Gets the <see cref="TypeReference"/> for <c>System.AttributeUsageAttribute</c>.
+    /// Gets the <see cref="TypeReference"/> for <see cref="System.AttributeUsageAttribute"/>.
     /// </summary>
     [field: MaybeNull, AllowNull]
     public TypeReference AttributeUsageAttribute => field ??= _interopModule.CorLibTypeFactory.CorLibScope.CreateTypeReference("System", "AttributeUsageAttribute");
 
     /// <summary>
-    /// Gets the <see cref="TypeReference"/> for <c>System.Type</c>.
+    /// Gets the <see cref="TypeReference"/> for <see cref="System.Type"/>.
     /// </summary>
     [field: MaybeNull, AllowNull]
     public TypeReference Type => field ??= _interopModule.CorLibTypeFactory.CorLibScope.CreateTypeReference("System", "Type");
 
     /// <summary>
-    /// Gets the <see cref="TypeReference"/> for <c>System.ValueType</c>.
+    /// Gets the <see cref="TypeReference"/> for <see cref="System.ValueType"/>.
     /// </summary>
     [field: MaybeNull, AllowNull]
     public TypeReference ValueType => field ??= _interopModule.CorLibTypeFactory.CorLibScope.CreateTypeReference("System", "ValueType");
 
     /// <summary>
-    /// Gets the <see cref="TypeReference"/> for <c>System.ReadOnlySpan&lt;T&gt;</c>.
+    /// Gets the <see cref="TypeReference"/> for <see cref="System.ReadOnlySpan{T}"/>.
     /// </summary>
     [field: MaybeNull, AllowNull]
     public TypeReference ReadOnlySpan => field ??= _interopModule.CorLibTypeFactory.CorLibScope.CreateTypeReference("System", "ReadOnlySpan`1");
 
     /// <summary>
-    /// Gets the <see cref="ITypeDefOrRef"/> for <c>System.ReadOnlySpan&lt;T&gt;</c> of <see cref="char"/>.
+    /// Gets the <see cref="ITypeDefOrRef"/> for <see cref="System.ReadOnlySpan{T}"/> of <see cref="char"/>.
     /// </summary>
     [field: MaybeNull, AllowNull]
     public ITypeDefOrRef ReadOnlySpanChar => field ??= ReadOnlySpan
@@ -84,103 +84,103 @@ internal sealed class InteropReferences
         .ToTypeDefOrRef();
 
     /// <summary>
-    /// Gets the <see cref="TypeReference"/> for <c>System.Exception</c>.
+    /// Gets the <see cref="TypeReference"/> for <see cref="System.Exception"/>.
     /// </summary>
     [field: MaybeNull, AllowNull]
     public TypeReference Exception => field ??= _interopModule.CorLibTypeFactory.CorLibScope.CreateTypeReference("System", "Exception");
 
     /// <summary>
-    /// Gets the <see cref="TypeReference"/> for <c>System.Guid</c>.
+    /// Gets the <see cref="TypeReference"/> for <see cref="System.Guid"/>.
     /// </summary>
     [field: MaybeNull, AllowNull]
     public TypeReference Guid => field ??= _interopModule.CorLibTypeFactory.CorLibScope.CreateTypeReference("System", "Guid");
 
     /// <summary>
-    /// Gets the <see cref="TypeReference"/> for <c>System.EventHandler</c>.
+    /// Gets the <see cref="TypeReference"/> for <see cref="System.EventHandler"/>.
     /// </summary>
     [field: MaybeNull, AllowNull]
     public TypeReference EventHandler => field ??= _interopModule.CorLibTypeFactory.CorLibScope.CreateTypeReference("System", "EventHandler");
 
     /// <summary>
-    /// Gets the <see cref="TypeReference"/> for <c>System.EventHandler&lt;TEventArgs&gt;</c>.
+    /// Gets the <see cref="TypeReference"/> for <see cref="System.EventHandler{TEventArgs}"/>.
     /// </summary>
     [field: MaybeNull, AllowNull]
     public TypeReference EventHandler1 => field ??= _interopModule.CorLibTypeFactory.CorLibScope.CreateTypeReference("System", "EventHandler`1");
 
     /// <summary>
-    /// Gets the <see cref="TypeReference"/> for <c>System.EventHandler&lt;TSender, TEventArgs&gt;</c>.
+    /// Gets the <see cref="TypeReference"/> for <see cref="System.EventHandler{TSender, TEventArgs}"/>.
     /// </summary>
     [field: MaybeNull, AllowNull]
     public TypeReference EventHandler2 => field ??= _interopModule.CorLibTypeFactory.CorLibScope.CreateTypeReference("System", "EventHandler`2");
 
     /// <summary>
-    /// Gets the <see cref="TypeReference"/> for <c>System.Collections.Generic.IEnumerable&lt;T&gt;</c>.
+    /// Gets the <see cref="TypeReference"/> for <see cref="System.Collections.Generic.IEnumerable{T}"/>.
     /// </summary>
     [field: MaybeNull, AllowNull]
     public TypeReference IEnumerable1 => field ??= _interopModule.CorLibTypeFactory.CorLibScope.CreateTypeReference("System.Collections.Generic", "IEnumerable`1");
 
     /// <summary>
-    /// Gets the <see cref="TypeReference"/> for <c>System.Collections.Generic.KeyValuePair&lt;TKey, TValue&gt;</c>.
+    /// Gets the <see cref="TypeReference"/> for <see cref="System.Collections.Generic.KeyValuePair{TKey, TValue}"/>.
     /// </summary>
     [field: MaybeNull, AllowNull]
     public TypeReference KeyValuePair => field ??= _interopModule.CorLibTypeFactory.CorLibScope.CreateTypeReference("System.Collections.Generic", "KeyValuePair`2");
 
     /// <summary>
-    /// Gets the <see cref="TypeReference"/> for <c>System.MemoryExtensions</c>.
+    /// Gets the <see cref="TypeReference"/> for <see cref="System.MemoryExtensions"/>.
     /// </summary>
     [field: MaybeNull, AllowNull]
     public TypeReference MemoryExtensions => field ??= _interopModule.CorLibTypeFactory.CorLibScope.CreateTypeReference("System", "MemoryExtensions");
 
     /// <summary>
-    /// Gets the <see cref="TypeReference"/> for <c>System.Runtime.InteropServices.MemoryMarshal</c>.
+    /// Gets the <see cref="TypeReference"/> for <see cref="System.Runtime.InteropServices.MemoryMarshal"/>.
     /// </summary>
     [field: MaybeNull, AllowNull]
     public TypeReference MemoryMarshal => field ??= _interopModule.CorLibTypeFactory.CorLibScope.CreateTypeReference("System.Runtime.InteropServices", "MemoryMarshal");
 
     /// <summary>
-    /// Gets the <see cref="TypeReference"/> for <c>System.Runtime.InteropServices.ComWrappers.ComInterfaceDispatch</c>.
+    /// Gets the <see cref="TypeReference"/> for <see cref="System.Runtime.InteropServices.ComWrappers.ComInterfaceDispatch"/>.
     /// </summary>
     [field: MaybeNull, AllowNull]
     public TypeReference ComInterfaceDispatch => field ??= _interopModule.CorLibTypeFactory.CorLibScope.CreateTypeReference("System.Runtime.InteropServices.ComWrappers", "ComWrappers/ComInterfaceDispatch");
 
     /// <summary>
-    /// Gets the <see cref="TypeReference"/> for <c>System.Runtime.InteropServices.ComWrappers.ComInterfaceEntry</c>.
+    /// Gets the <see cref="TypeReference"/> for <see cref="System.Runtime.InteropServices.ComWrappers.ComInterfaceEntry"/>.
     /// </summary>
     [field: MaybeNull, AllowNull]
     public TypeReference ComInterfaceEntry => field ??= _interopModule.CorLibTypeFactory.CorLibScope.CreateTypeReference("System.Runtime.InteropServices.ComWrappers", "ComWrappers/ComInterfaceEntry");
 
     /// <summary>
-    /// Gets the <see cref="TypeReference"/> for <c>System.Runtime.InteropServices.InAttribute</c>.
+    /// Gets the <see cref="TypeReference"/> for <see cref="System.Runtime.InteropServices.InAttribute"/>.
     /// </summary>
     [field: MaybeNull, AllowNull]
     public TypeReference InAttribute => field ??= _interopModule.CorLibTypeFactory.CorLibScope.CreateTypeReference("System.Runtime.InteropServices", "InAttribute");
 
     /// <summary>
-    /// Gets the <see cref="TypeReference"/> for <c>System.Runtime.CompilerServices.IsReadOnlyAttribute</c>.
+    /// Gets the <see cref="TypeReference"/> for <see cref="System.Runtime.CompilerServices.IsReadOnlyAttribute"/>.
     /// </summary>
     [field: MaybeNull, AllowNull]
     public TypeReference IsReadOnlyAttribute => field ??= _interopModule.CorLibTypeFactory.CorLibScope.CreateTypeReference("System.Runtime.CompilerServices", "IsReadOnlyAttribute");
 
     /// <summary>
-    /// Gets the <see cref="TypeReference"/> for <c>System.Runtime.CompilerServices.FixedAddressValueTypeAttribute</c>.
+    /// Gets the <see cref="TypeReference"/> for <see cref="System.Runtime.CompilerServices.FixedAddressValueTypeAttribute"/>.
     /// </summary>
     [field: MaybeNull, AllowNull]
     public TypeReference FixedAddressValueTypeAttribute => field ??= _interopModule.CorLibTypeFactory.CorLibScope.CreateTypeReference("System.Runtime.CompilerServices", "FixedAddressValueTypeAttribute");
 
     /// <summary>
-    /// Gets the <see cref="TypeReference"/> for <c>System.Runtime.CompilerServices.ScopedRefAttribute</c>.
+    /// Gets the <see cref="TypeReference"/> for <see cref="System.Runtime.CompilerServices.ScopedRefAttribute"/>.
     /// </summary>
     [field: MaybeNull, AllowNull]
     public TypeReference ScopedRefAttribute => field ??= _interopModule.CorLibTypeFactory.CorLibScope.CreateTypeReference("System.Runtime.CompilerServices", "ScopedRefAttribute");
 
     /// <summary>
-    /// Gets the <see cref="TypeReference"/> for <c>System.Runtime.CompilerServices.CallConvMemberFunction</c>.
+    /// Gets the <see cref="TypeReference"/> for <see cref="System.Runtime.CompilerServices.CallConvMemberFunction"/>.
     /// </summary>
     [field: MaybeNull, AllowNull]
     public TypeReference CallConvMemberFunction => field ??= _interopModule.CorLibTypeFactory.CorLibScope.CreateTypeReference("System.Runtime.CompilerServices", "CallConvMemberFunction");
 
     /// <summary>
-    /// Gets the <see cref="TypeReference"/> for <c>System.Runtime.InteropServices.UnmanagedCallersOnlyAttribute</c>.
+    /// Gets the <see cref="TypeReference"/> for <see cref="System.Runtime.InteropServices.UnmanagedCallersOnlyAttribute"/>.
     /// </summary>
     [field: MaybeNull, AllowNull]
     public TypeReference UnmanagedCallersOnlyAttribute => field ??= _interopModule.CorLibTypeFactory.CorLibScope.CreateTypeReference("System.Runtime.InteropServices", "UnmanagedCallersOnlyAttribute");
@@ -294,14 +294,14 @@ internal sealed class InteropReferences
     public TypeReference EventHandler2EventSource => field ??= _windowsRuntimeModule.CreateTypeReference("WindowsRuntime.InteropServices", "EventHandlerEventSource`2");
 
     /// <summary>
-    /// Gets the <see cref="MemberReference"/> for <c>System.Attribute.ctor()</c>.
+    /// Gets the <see cref="MemberReference"/> for <see cref="System.Attribute.Attribute()"/>.
     /// </summary>
     [field: MaybeNull, AllowNull]
     public MemberReference Attribute_ctor => field ??= Attribute
         .CreateMemberReference(".ctor", MethodSignature.CreateInstance(_windowsRuntimeModule.CorLibTypeFactory.Void));
 
     /// <summary>
-    /// Gets the <see cref="TypeReference"/> for <c>System.AttributeUsageAttribute</c>'s constructor using <see cref="AttributeTargets"/>.
+    /// Gets the <see cref="TypeReference"/> for <see cref="System.AttributeUsageAttribute.AttributeUsageAttribute(System.AttributeTargets)"/>.
     /// </summary>
     [field: MaybeNull, AllowNull]
     public MemberReference AttributeUsageAttribute_ctor_AttributeTargets => field ??= AttributeUsageAttribute
@@ -310,7 +310,7 @@ internal sealed class InteropReferences
             parameterTypes: [AttributeTargets.ToTypeSignature(isValueType: true)]));
 
     /// <summary>
-    /// Gets the <see cref="MemberReference"/> for <c>System.ReadOnlySpane&lt;T&gt;.this[int]</c> (of <see cref="char"/>).
+    /// Gets the <see cref="MemberReference"/> for <see cref="System.ReadOnlySpan{T}"/>'s indexer (of <see cref="char"/>).
     /// </summary>
     [field: MaybeNull, AllowNull]
     public MemberReference ReadOnlySpanCharget_Item => field ??= ReadOnlySpanChar
@@ -322,14 +322,14 @@ internal sealed class InteropReferences
             parameterTypes: [_interopModule.CorLibTypeFactory.Int32]));
 
     /// <summary>
-    /// Gets the <see cref="MemberReference"/> for <c>System.ReadOnlySpane&lt;T&gt;.Length</c> (of <see cref="char"/>).
+    /// Gets the <see cref="MemberReference"/> for <see cref="System.ReadOnlySpan{T}.Length"/> (of <see cref="char"/>).
     /// </summary>
     [field: MaybeNull, AllowNull]
     public MemberReference ReadOnlySpanCharget_Length => field ??= ReadOnlySpanChar
         .CreateMemberReference("get_Length", MethodSignature.CreateInstance(_interopModule.CorLibTypeFactory.Int32));
 
     /// <summary>
-    /// Gets the <see cref="MethodSpecification"/> for <c>System.MemoryExtensions.SequenceEqual&lt;T&gt;</c> (for <see cref="ReadOnlySpanChar"/>).
+    /// Gets the <see cref="MethodSpecification"/> for <see cref="System.MemoryExtensions.SequenceEqual{T}(System.Span{T}, System.ReadOnlySpan{T})"/> (for <see cref="ReadOnlySpanChar"/>).
     /// </summary>
     [field: MaybeNull, AllowNull]
     public MethodSpecification MemoryExtensionsSequenceEqualChar => field ??= MemoryExtensions
@@ -342,7 +342,7 @@ internal sealed class InteropReferences
         .MakeGenericInstanceMethod(_interopModule.CorLibTypeFactory.Char);
 
     /// <summary>
-    /// Gets the <see cref="MethodSpecification"/> for <c>System.Runtime.InteropServices.MemoryMarshal.CreateSpan</c>.
+    /// Gets the <see cref="MethodSpecification"/> for <see cref="System.Runtime.InteropServices.MemoryMarshal.CreateSpan"/>.
     /// </summary>
     [field: MaybeNull, AllowNull]
     public MemberReference MemoryMarshalCreateSpan => field ??= MemoryMarshal
@@ -354,35 +354,35 @@ internal sealed class InteropReferences
                 _interopModule.CorLibTypeFactory.Int32]));
 
     /// <summary>
-    /// Gets the <see cref="TypeReference"/> for <c>System.Runtime.CompilerServices.FixedAddressValueTypeAttribute</c>'s constructor.
+    /// Gets the <see cref="TypeReference"/> for <see cref="System.Runtime.CompilerServices.FixedAddressValueTypeAttribute.FixedAddressValueTypeAttribute()"/>.
     /// </summary>
     [field: MaybeNull, AllowNull]
     public MemberReference FixedAddressValueTypeAttribute_ctor => field ??= FixedAddressValueTypeAttribute
         .CreateMemberReference(".ctor", MethodSignature.CreateInstance(returnType: _interopModule.CorLibTypeFactory.Void));
 
     /// <summary>
-    /// Gets the <see cref="TypeReference"/> for <c>System.Runtime.CompilerServices.IsReadOnlyAttribute</c>'s constructor.
+    /// Gets the <see cref="TypeReference"/> for <see cref="System.Runtime.CompilerServices.IsReadOnlyAttribute.IsReadOnlyAttribute()"/>.
     /// </summary>
     [field: MaybeNull, AllowNull]
     public MemberReference IsReadOnlyAttribute_ctor => field ??= IsReadOnlyAttribute
         .CreateMemberReference(".ctor", MethodSignature.CreateInstance(returnType: _interopModule.CorLibTypeFactory.Void));
 
     /// <summary>
-    /// Gets the <see cref="TypeReference"/> for <c>System.Runtime.CompilerServices.ScopedRefAttribute</c>'s constructor.
+    /// Gets the <see cref="TypeReference"/> for <see cref="System.Runtime.CompilerServices.ScopedRefAttribute.ScopedRefAttribute()"/>.
     /// </summary>
     [field: MaybeNull, AllowNull]
     public MemberReference ScopedRefAttribute_ctor => field ??= ScopedRefAttribute
         .CreateMemberReference(".ctor", MethodSignature.CreateInstance(returnType: _interopModule.CorLibTypeFactory.Void));
 
     /// <summary>
-    /// Gets the <see cref="TypeReference"/> for <c>System.Runtime.InteropServices.UnmanagedCallersOnlyAttribute</c>'s constructor.
+    /// Gets the <see cref="TypeReference"/> for <see cref="System.Runtime.InteropServices.UnmanagedCallersOnlyAttribute.UnmanagedCallersOnlyAttribute()"/>.
     /// </summary>
     [field: MaybeNull, AllowNull]
     public MemberReference UnmanagedCallersOnlyAttribute_ctor => field ??= UnmanagedCallersOnlyAttribute
         .CreateMemberReference(".ctor", MethodSignature.CreateInstance(returnType: _interopModule.CorLibTypeFactory.Void));
 
     /// <summary>
-    /// Gets the <see cref="MemberReference"/> for <c>System.Runtime.InteropServices.ComWrappers.ComInterfaceDispatch.GetInstance&lt;T&gt;(ComWrappers.ComInterfaceDispatch*)</c>.
+    /// Gets the <see cref="MemberReference"/> for <see cref="System.Runtime.InteropServices.ComWrappers.ComInterfaceDispatch.GetInstance"/>.
     /// </summary>
     [field: MaybeNull, AllowNull]
     public MemberReference ComInterfaceDispatchGetInstance => field ??= ComInterfaceDispatch
@@ -392,13 +392,13 @@ internal sealed class InteropReferences
             parameterTypes: [_interopModule.CreateTypeReference("System.Runtime.InteropServices", "ComWrappers/ComInterfaceDispatch").MakePointerType()]));
 
     /// <summary>
-    /// Gets the <see cref="MemberReference"/> for <c>System.Runtime.InteropServices.ComWrappers.ComInterfaceEntry.IID</c>.
+    /// Gets the <see cref="MemberReference"/> for <see cref="System.Runtime.InteropServices.ComWrappers.ComInterfaceEntry.IID"/>.
     /// </summary>
     [field: MaybeNull, AllowNull]
     public MemberReference ComInterfaceEntryIID => field ??= ComInterfaceEntry.CreateMemberReference("IID", new FieldSignature(Guid.ToTypeSignature(isValueType: true)));
 
     /// <summary>
-    /// Gets the <see cref="MemberReference"/> for <c>System.Runtime.InteropServices.ComWrappers.ComInterfaceEntry.IID</c>.
+    /// Gets the <see cref="MemberReference"/> for <see cref="System.Runtime.InteropServices.ComWrappers.ComInterfaceEntry.Vtable"/>.
     /// </summary>
     [field: MaybeNull, AllowNull]
     public MemberReference ComInterfaceEntryVtable => field ??= ComInterfaceEntry.CreateMemberReference("Vtable", new FieldSignature(_interopModule.CorLibTypeFactory.IntPtr));
