bdffcf5de9754af583380b9bd93714a12378fb5c
Sergio Pedri <sergio0694@live.com>
2025-05-15 18:54:08 -0700

    Remove unnecessary timeout setting

--- src/WinRT.Interop.Generator/Program.cs
+++ src/WinRT.Interop.Generator/Program.cs
@@ -1,13 +1,8 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-using System;
 using ConsoleAppFramework;
 using WindowsRuntime.InteropGenerator.Generation;
-
-// The default timeout is just 5 seconds, but it's reasonable to take more than that.
-// Users can cancel running this tool from Visual Studio, so a higher timeout is fine.
-ConsoleApp.Timeout = TimeSpan.FromMinutes(1);
 
 // Run the interop generator with all parsed arguments
 ConsoleApp.Run(args, InteropGenerator.Run);
