d5e25098f75a2c86ea3b5f5c6c9cf2396c7449c7
Sergio Pedri <sergio0694@live.com>
2025-05-18 11:40:02 -0700

    Add 'WindowsRuntimeEnumerable<T>'

---
+++ src/WinRT.Runtime2/Collections/WindowsRuntimeEnumerable{T}.cs
@@ -0,0 +1,72 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using WindowsRuntime.InteropServices;
+
+#pragma warning disable CA1816
+
+namespace WindowsRuntime;
+
+/// <summary>
+/// The base class for all projected Windows Runtime <see cref="IEnumerable{T}"/> types.
+/// </summary>
+/// <typeparam name="T">The type of objects to enumerate.</typeparam>
+/// <remarks>
+/// This type should only be used as a base type by generated generic instantiations.
+/// </remarks>
+/// <see href="https://learn.microsoft.com/uwp/api/windows.foundation.collections.iiterable-1"/>
+[Obsolete("This type is an implementation detail, and it's only meant to be consumed by 'cswinrtgen'")]
+[EditorBrowsable(EditorBrowsableState.Never)]
+public abstract class WindowsRuntimeEnumerable<T> : WindowsRuntimeObject, IEnumerable<T>, IWindowsRuntimeInterface<IEnumerable<T>>
+{
+    /// <summary>
+    /// Creates a <see cref="WindowsRuntimeEnumerable{T}"/> instance with the specified parameters.
+    /// </summary>
+    /// <param name="nativeObjectReference">The inner Windows Runtime object reference to wrap in the current instance.</param>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="nativeObjectReference"/> is <see langword="null"/>.</exception>
+    public WindowsRuntimeEnumerable(WindowsRuntimeObjectReference nativeObjectReference)
+        : base(nativeObjectReference)
+    {
+    }
+
+    /// <inheritdoc/>
+    protected internal override bool HasUnwrappableNativeObjectReference => true;
+
+    /// <summary>
+    /// Returns an iterator for the items in the collection.
+    /// </summary>
+    /// <returns>The iterator.</returns>
+    /// <remarks>
+    /// This method should directly implement the <c>Windows.Foundation.Collections.IIterable&lt;T&gt;.First</c> method.
+    /// </remarks>
+    /// <see href="https://learn.microsoft.com/uwp/api/windows.foundation.collections.iiterable-1.first"/>
+    protected abstract IEnumerator<T> FirstNative();
+
+    /// <inheritdoc/>
+    public IEnumerator<T> GetEnumerator()
+    {
+        return FirstNative();
+    }
+
+    /// <inheritdoc/>
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    /// <inheritdoc/>
+    WindowsRuntimeObjectReferenceValue IWindowsRuntimeInterface<IEnumerable<T>>.GetInterface()
+    {
+        return NativeObjectReference.AsValue();
+    }
+
+    /// <inheritdoc/>
+    protected sealed override bool IsOverridableInterface(in Guid iid)
+    {
+        return false;
+    }
+}
