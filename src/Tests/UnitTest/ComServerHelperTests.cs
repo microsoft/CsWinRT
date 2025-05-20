eca2d2b9d842b1a7e6c1ad974076b1c89e0fcb5f
Seva Iliushin <sevkorobot@gmail.com>
2025-05-20 15:59:46 -0700

    add oop com tests

---
+++ src/Tests/UnitTest/ComServerHelperTests.cs
@@ -0,0 +1,37 @@
+﻿#if NET8_0_OR_GREATER
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+using ComServerHelpers;
+using ComServerHelpers.CsWinRT;
+using Windows.Foundation;
+using Xunit;
+
+namespace UnitTest
+{
+    public class ComServerHelperTests
+    {
+
+        [Fact]
+        public void TestCOMRegistrationAndActivation()
+        {
+            ComServer server = new ComServer();
+            OOPAsyncAction obj = new OOPAsyncAction();
+            server.RegisterClass<OOPAsyncAction, IAsyncAction>(() => { return obj; });
+            server.Start();
+
+            var currentExecutingDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var launchExePath = $"{currentExecutingDir}\\OOPExe.exe";
+            var proc = Process.Start(launchExePath);
+
+            Thread.Sleep(5000);
+            obj.Close();
+            Assert.True(obj.delegateCalled);
+            server.Dispose();
+        }
+    }
+}
+#endif
