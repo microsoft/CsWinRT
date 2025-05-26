921f7199e28870cce661b51089448895dacf904e
Sergio Pedri <sergio0694@live.com>
2025-05-26 15:04:18 -0700

    Update WindowsRuntimeEnumerator{T}.cs

--- src/WinRT.Runtime2/Collections/WindowsRuntimeEnumerator{T}.cs
+++ src/WinRT.Runtime2/Collections/WindowsRuntimeEnumerator{T}.cs
@@ -5,9 +5,10 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
 using WindowsRuntime.InteropServices;
 
-#pragma warning disable CA1816
+#pragma warning disable CA1816, IDE0046
 
 namespace WindowsRuntime;
 
@@ -23,9 +24,20 @@ namespace WindowsRuntime;
 [EditorBrowsable(EditorBrowsableState.Never)]
 public abstract unsafe class WindowsRuntimeEnumerator<T> : WindowsRuntimeObject, IEnumerator<T>, IWindowsRuntimeInterface<IEnumerator<T>>
 {
-    private bool _hadCurrent = true;
+    /// <summary>
+    /// Indicates whether the underlying enumerator has been initialized.
+    /// </summary>
     private bool _isInitialized = false;
-    private T _current = default!;
+
+    /// <summary>
+    /// Indicates whether the previous call to <c>HasCurrent</c> was successful.
+    /// </summary>
+    private bool _hadCurrent = true;
+
+    /// <summary>
+    /// The cached <see cref="Current"/> value, if available.
+    /// </summary>
+    private T? _current;
 
     /// <summary>
     /// Creates a <see cref="WindowsRuntimeEnumerator{T}"/> instance with the specified parameters.
@@ -51,15 +63,73 @@ public abstract unsafe class WindowsRuntimeEnumerator<T> : WindowsRuntimeObject,
     protected abstract T CurrentNative();
 
     /// <inheritdoc/>
-    public T Current => CurrentNative();
+    public T Current
+    {
+        get
+        {
+            // The enumerator has not been advanced to the first element yet
+            if (!_isInitialized)
+            {
+                throw new InvalidOperationException("InvalidOperation_EnumNotStarted");
+            }
+
+            // The enumerator has reached the end of the collection
+            if (!_hadCurrent)
+            {
+                throw new InvalidOperationException("InvalidOperation_EnumEnded");
+            }
+
+            return _current!;
+        }
+    }
 
     /// <inheritdoc/>
     object IEnumerator.Current => Current!;
 
     /// <inheritdoc/>
+    [MemberNotNullWhen(true, nameof(_current))]
     public bool MoveNext()
     {
-        return false;
+        // If we've passed the end of the iteration, 'IEnumerable<T>' should return 'false',
+        // while 'IIterable<T>' will fail the interface call. So we need to check for that.
+        if (!_hadCurrent)
+        {
+            return false;
+        }
+
+        // 'IIterator<T>' starts at index '0', rather than '-1'. If this is the first call, we
+        // need to just check 'HasCurrent' rather than actually moving to the next element.
+        try
+        {
+            if (!_isInitialized)
+            {
+                _hadCurrent = IIteratorMethods.HasCurrent(NativeObjectReference);
+                _isInitialized = true;
+            }
+            else
+            {
+                _hadCurrent = IIteratorMethods.MoveNext(NativeObjectReference);
+            }
+
+            // We want to save away the current value for two reasons:
+            //  1. Accessing 'Current' is cheap on other iterators, so having it be a property which is a
+            //     simple field access preserves the expected performance characteristics (as opposed to
+            //     triggering a COM call every time the property is accessed).
+            //  2. This allows us to preserve the same semantics as generic collection iteration when iterating
+            //     beyond the end of the collection, namely that 'Current' continues to return the last value
+            //     of the collection.
+            if (_hadCurrent)
+            {
+                _current = CurrentNative();
+            }
+
+            return _hadCurrent;
+        }
+        catch (Exception e) when (e.HResult == WellKnownErrorCodes.E_BOUNDS)
+        {
+            // Translate 'E_BOUNDS' into an 'InvalidOperationException' for an updated enumeration
+            throw new InvalidOperationException("InvalidOperation_EnumFailedVersion", e);
+        }
     }
 
     /// <inheritdoc/>
