865061ee850af86b584d48ef9dd6df24250722ed
Sergio Pedri <sergio0694@live.com>
2025-05-26 19:20:57 -0700

    Add 'ReadOnlyDictionaryKeyCollection<,>'

--- src/WinRT.Runtime2/InteropServices/Collections/IEnumeratorAdapter{T}.cs
+++ src/WinRT.Runtime2/InteropServices/Collections/IEnumeratorAdapter{T}.cs
@@ -13,7 +13,7 @@ namespace WindowsRuntime.InteropServices;
 /// </summary>
 /// <typeparam name="T">The type of objects to enumerate.</typeparam>
 /// <remarks>
-/// This type should only be used as a base type by generated generic instantiations.
+/// This type should only be used by generated code.
 /// </remarks>
 /// <see href="https://learn.microsoft.com/uwp/api/windows.foundation.collections.iiterator-1"/>
 [Obsolete("This type is an implementation detail, and it's only meant to be consumed by 'cswinrtgen'")]
