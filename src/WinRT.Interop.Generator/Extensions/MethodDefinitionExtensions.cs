fc7411f913714e9d8854f754aa9d580f5e9f7252
Sergio Pedri <sergio0694@live.com>
2025-06-08 16:55:19 -0700

    Add 'MethodDefinitionExtensions'

---
+++ src/WinRT.Interop.Generator/Extensions/MethodDefinitionExtensions.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using AsmResolver.DotNet;
+using AsmResolver.DotNet.Code.Cil;
+
+namespace WindowsRuntime.InteropGenerator;
+
+/// <summary>
+/// Extensions for the <see cref="MethodDefinition"/> type.
+/// </summary>
+internal static class MethodDefinitionExtensions
+{
+    extension(MethodDefinition method)
+    {
+        /// <inheritdoc cref="CilMethodBody.Instructions"/>
+        public CilInstructionCollection CilInstructions => (method.CilMethodBody ??= new CilMethodBody()).Instructions;
+
+        /// <inheritdoc cref="CilMethodBody.LocalVariables"/>
+        public CilLocalVariableCollection CilLocalVariables => (method.CilMethodBody ??= new CilMethodBody()).LocalVariables;
+
+        /// <inheritdoc cref="CilMethodBody.ExceptionHandlers"/>
+        public IList<CilExceptionHandler> CilExceptionHandlers => (method.CilMethodBody ??= new CilMethodBody()).ExceptionHandlers;
+    }
+}
