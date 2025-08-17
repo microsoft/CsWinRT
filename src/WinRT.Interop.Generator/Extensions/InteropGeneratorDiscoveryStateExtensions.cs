2b79e0d9905d018a1a101038cf21644f78cb05fe
Sergio Pedri <sergio0694@live.com>
2025-08-17 03:25:08 -0700

    Refactor generic interface tracking to extension method
    Moved the TrackGenericInterfaceType logic from InteropGenerator.Discover.cs to a new InteropGeneratorDiscoveryStateExtensions.cs extension class. This improves code organization and reusability by centralizing generic interface tracking logic.

---
+++ src/WinRT.Interop.Generator/Extensions/InteropGeneratorDiscoveryStateExtensions.cs
@@ -0,0 +1,57 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using AsmResolver.DotNet.Signatures;
+using WindowsRuntime.InteropGenerator.Generation;
+using WindowsRuntime.InteropGenerator.References;
+
+namespace WindowsRuntime.InteropGenerator;
+
+/// <summary>
+/// Extensions for <see cref="InteropGeneratorDiscoveryState"/>.
+/// </summary>
+internal static class InteropGeneratorDiscoveryStateExtensions
+{
+    /// <summary>
+    /// Tracks a generic interface type of any custom-mapped type.
+    /// </summary>
+    /// <param name="discoveryState">The current <see cref="InteropGeneratorDiscoveryState"/> instance.</param>
+    /// <param name="typeSignature">The generic interface type.</param>
+    /// <param name="interopReferences">The <see cref="InteropReferences"/> instance to use.</param>
+    public static void TrackGenericInterfaceType(
+        this InteropGeneratorDiscoveryState discoveryState,
+        GenericInstanceTypeSignature typeSignature,
+        InteropReferences interopReferences)
+    {
+        if (SignatureComparer.IgnoreVersion.Equals(typeSignature.GenericType, interopReferences.IEnumerator1))
+        {
+            discoveryState.TrackIEnumerator1Type(typeSignature);
+        }
+        else if (SignatureComparer.IgnoreVersion.Equals(typeSignature.GenericType, interopReferences.IEnumerable1))
+        {
+            discoveryState.TrackIEnumerable1Type(typeSignature);
+
+            // We need special handling for 'IEnumerator<T>' types whenever we discover any constructed 'IEnumerable<T>'
+            // type. This ensures that we're never missing any 'IEnumerator<T>' instantiation, which we might depend on
+            // from other generated code, or projections. This special handling is needed because unlike with the other
+            // interfaces, 'IEnumerator<T>' will not show up as a base interface for other collection interface types.
+            discoveryState.TrackIEnumerator1Type(interopReferences.IEnumerator1.MakeGenericReferenceType(typeSignature.TypeArguments[0]));
+        }
+        else if (SignatureComparer.IgnoreVersion.Equals(typeSignature.GenericType, interopReferences.IList1))
+        {
+            discoveryState.TrackIList1Type(typeSignature);
+        }
+        else if (SignatureComparer.IgnoreVersion.Equals(typeSignature.GenericType, interopReferences.IReadOnlyList1))
+        {
+            discoveryState.TrackIReadOnlyList1Type(typeSignature);
+        }
+        else if (SignatureComparer.IgnoreVersion.Equals(typeSignature.GenericType, interopReferences.IDictionary2))
+        {
+            discoveryState.TrackIDictionary2Type(typeSignature);
+        }
+        else if (SignatureComparer.IgnoreVersion.Equals(typeSignature.GenericType, interopReferences.IReadOnlyDictionary2))
+        {
+            discoveryState.TrackIReadOnlyDictionary2Type(typeSignature);
+        }
+    }
+}
