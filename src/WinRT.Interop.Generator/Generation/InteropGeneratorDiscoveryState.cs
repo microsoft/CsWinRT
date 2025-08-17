f4a5dcc819a650418159b56d5975c12b7beed512
Sergio Pedri <sergio0694@live.com>
2025-08-17 05:11:02 -0700

    Track user-defined vtable types in discovery state
    Added support for tracking user-defined vtable types in InteropGeneratorDiscoveryState. The class now deduplicates vtable type sets and exposes collections for user-defined types and their associated vtable types.

--- src/WinRT.Interop.Generator/Generation/InteropGeneratorDiscoveryState.cs
+++ src/WinRT.Interop.Generator/Generation/InteropGeneratorDiscoveryState.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using AsmResolver.DotNet;
 using AsmResolver.DotNet.Signatures;
 using WindowsRuntime.InteropGenerator.Errors;
+using WindowsRuntime.InteropGenerator.Models;
 
 namespace WindowsRuntime.InteropGenerator.Generation;
 
@@ -48,7 +49,10 @@ internal sealed class InteropGeneratorDiscoveryState
     private readonly ConcurrentDictionary<SzArrayTypeSignature, byte> _szArrayTypes = new(SignatureComparer.IgnoreVersion);
 
     /// <summary>Backing field for <see cref="UserDefinedTypes"/>.</summary>
-    private readonly ConcurrentDictionary<TypeSignature, byte> _userDefinedTypes = new(SignatureComparer.IgnoreVersion);
+    private readonly ConcurrentDictionary<TypeSignature, TypeSignatureEquatableSet> _userDefinedTypes = new(SignatureComparer.IgnoreVersion);
+
+    /// <summary>Backing field for <see cref="UserDefinedVtableTypes"/>.</summary>
+    private readonly ConcurrentDictionary<TypeSignatureEquatableSet, TypeSignatureEquatableSet> _userDefinedVtableTypes = [];
 
     /// <summary>
     /// Indicates whether the current state is readonly.
@@ -124,6 +128,16 @@ internal sealed class InteropGeneratorDiscoveryState
     /// Gets all user-defined types.
     /// </summary>
     public IReadOnlyCollection<TypeSignature> UserDefinedTypes => (IReadOnlyCollection<TypeSignature>)_userDefinedTypes.Keys;
+
+    /// <summary>
+    /// Gets all user-defined types and their vtable types.
+    /// </summary>
+    public IReadOnlyDictionary<TypeSignature, TypeSignatureEquatableSet> UserDefinedAndVtableTypes => _userDefinedTypes;
+
+    /// <summary>
+    /// Gets all user-defined vtable types (for each user-defined type).
+    /// </summary>
+    public IReadOnlyCollection<TypeSignatureEquatableSet> UserDefinedVtableTypes => (IReadOnlyCollection<TypeSignatureEquatableSet>)_userDefinedVtableTypes.Keys;
 
     /// <summary>
     /// Gets whether any of the loaded modules reference the WinRT runtime .dll version 2.
@@ -257,11 +271,18 @@ internal sealed class InteropGeneratorDiscoveryState
     /// Tracks a user-defined type.
     /// </summary>
     /// <param name="userDefinedType">The user-defined type.</param>
-    public void TrackUserDefinedType(TypeSignature userDefinedType)
+    /// <param name="vtableTypes">The vtable types for <paramref name="userDefinedType"/>.</param>
+    public void TrackUserDefinedType(TypeSignature userDefinedType, TypeSignatureEquatableSet vtableTypes)
     {
         ThrowIfReadOnly();
 
-        _ = _userDefinedTypes.TryAdd(userDefinedType, 0);
+        // We want to de-duplicate all vtables across all user-defined types we need to generate CCW code for.
+        // This is because a significant portion of these vtables will be identical (it will be the case for
+        // all user-defined types that implement the same set of projected Windows Runtime interfaces). So here
+        // we first add this set to our map, and reuse the cached instance for this set (if it exists) later.
+        TypeSignatureEquatableSet cachedVtableTypes = _userDefinedVtableTypes.GetOrAdd(vtableTypes, vtableTypes);
+
+        _ = _userDefinedTypes.TryAdd(userDefinedType, cachedVtableTypes);
     }
 
     /// <summary>
