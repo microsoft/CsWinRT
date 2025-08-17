160607d061df2f013b14bf34e468f3e1d2687fb5
Sergio Pedri <sergio0694@live.com>
2025-08-17 03:25:08 -0700

    Refactor generic interface tracking to extension method
    Moved the TrackGenericInterfaceType logic from InteropGenerator.Discover.cs to a new InteropGeneratorDiscoveryStateExtensions.cs extension class. This improves code organization and reusability by centralizing generic interface tracking logic.

--- src/WinRT.Interop.Generator/Generation/InteropGenerator.Discover.cs
+++ src/WinRT.Interop.Generator/Generation/InteropGenerator.Discover.cs
@@ -265,49 +265,12 @@ internal partial class InteropGenerator
                 // Track all projected Windows Runtime generic interfaces
                 if (typeDefinition.IsInterface)
                 {
-                    static void TrackGenericInterfaceType(
-                        InteropGeneratorDiscoveryState discoveryState,
-                        GenericInstanceTypeSignature typeSignature,
-                        InteropReferences interopReferences)
-                    {
-                        if (SignatureComparer.IgnoreVersion.Equals(typeSignature.GenericType, interopReferences.IEnumerator1))
-                        {
-                            discoveryState.TrackIEnumerator1Type(typeSignature);
-                        }
-                        else if (SignatureComparer.IgnoreVersion.Equals(typeSignature.GenericType, interopReferences.IEnumerable1))
-                        {
-                            discoveryState.TrackIEnumerable1Type(typeSignature);
-
-                            // We need special handling for 'IEnumerator<T>' types whenever we discover any constructed 'IEnumerable<T>'
-                            // type. This ensures that we're never missing any 'IEnumerator<T>' instantiation, which we might depend on
-                            // from other generated code, or projections. This special handling is needed because unlike with the other
-                            // interfaces, 'IEnumerator<T>' will not show up as a base interface for other collection interface types.
-                            discoveryState.TrackIEnumerator1Type(interopReferences.IEnumerator1.MakeGenericReferenceType(typeSignature.TypeArguments[0]));
-                        }
-                        else if (SignatureComparer.IgnoreVersion.Equals(typeSignature.GenericType, interopReferences.IList1))
-                        {
-                            discoveryState.TrackIList1Type(typeSignature);
-                        }
-                        else if (SignatureComparer.IgnoreVersion.Equals(typeSignature.GenericType, interopReferences.IReadOnlyList1))
-                        {
-                            discoveryState.TrackIReadOnlyList1Type(typeSignature);
-                        }
-                        else if (SignatureComparer.IgnoreVersion.Equals(typeSignature.GenericType, interopReferences.IDictionary2))
-                        {
-                            discoveryState.TrackIDictionary2Type(typeSignature);
-                        }
-                        else if (SignatureComparer.IgnoreVersion.Equals(typeSignature.GenericType, interopReferences.IReadOnlyDictionary2))
-                        {
-                            discoveryState.TrackIReadOnlyDictionary2Type(typeSignature);
-                        }
-                    }
-
-                    TrackGenericInterfaceType(discoveryState, typeSignature, interopReferences);
+                    discoveryState.TrackGenericInterfaceType(typeSignature, interopReferences);
 
                     // We also want to crawl base interfaces
                     foreach (GenericInstanceTypeSignature interfaceSignature in typeDefinition.EnumerateGenericInstanceInterfaceSignatures(typeSignature))
                     {
-                        TrackGenericInterfaceType(discoveryState, interfaceSignature, interopReferences);
+                        discoveryState.TrackGenericInterfaceType(interfaceSignature, interopReferences);
                     }
 
                     continue;
