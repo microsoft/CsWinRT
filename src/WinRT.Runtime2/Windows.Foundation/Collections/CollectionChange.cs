19df761e08a20cc28b878782e3c3858f56256058
Sergio Pedri <sergio0694@live.com>
2025-08-22 04:43:32 -0700

    Add CollectionChange enum and marshalling support
    Introduces the CollectionChange enum in Windows.Foundation.Collections and its ABI marshaller implementation. Updates WellKnownInterfaceIds to include IID_IReferenceOfCollectionChange and its template entry for COM interop. This enables proper handling and marshalling of CollectionChange notifications in Windows Runtime collections.

---
+++ src/WinRT.Runtime2/Windows.Foundation/Collections/CollectionChange.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using ABI.Windows.Foundation.Collections;
+using WindowsRuntime;
+
+namespace Windows.Foundation.Collections;
+
+/// <summary>
+/// Describes the action that causes a change to a collection.
+/// </summary>
+/// <remarks>
+/// There is only one notification per type of change to a collection. For example, if an item is inserted, then only a notification
+/// for an insertion is sent to a listener that is subscribed to receive change notifications. Use the <c>Index</c> property of the
+/// event arguments interface in use (e.g. <see cref="IMapChangedEventArgs{K}"/>) to determine the location o the change.
+/// </remarks>
+/// <see href="https://learn.microsoft.com/uwp/api/windows.foundation.collections.collectionchange"/>
+[WindowsRuntimeMetadata("Windows.Foundation.FoundationContract")]
+[WindowsRuntimeClassName("Windows.Foundation.IReference<Windows.Foundation.Collections.CollectionChange>")]
+[CollectionChangeComWrappersMarshaller]
+public enum CollectionChange
+{
+    /// <summary>
+    /// The collection is changed.
+    /// </summary>
+    Reset = 0,
+
+    /// <summary>
+    /// An item is added to the collection.
+    /// </summary>
+    ItemInserted = 1,
+
+    /// <summary>
+    /// An item is removed from the collection.
+    /// </summary>
+    ItemRemoved = 2,
+
+    /// <summary>
+    /// An item is changed in the collection.
+    /// </summary>
+    ItemChanged = 3
+}
