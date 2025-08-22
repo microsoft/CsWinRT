3f260fbfa7d5c79630c84243032bcdfe1160fc7b
Sergio Pedri <sergio0694@live.com>
2025-08-22 05:02:35 -0700

    Add WinRT collection change event interfaces and handlers
    Introduces interfaces and delegates for observable map and vector collections in Windows.Foundation.Collections, including IObservableMap, IObservableVector, IMapChangedEventArgs, IVectorChangedEventArgs, and their respective event handler delegates. These additions enable notification and handling of dynamic changes in map and vector collections.

---
+++ src/WinRT.Runtime2/Windows.Foundation/Collections/IObservableMap{K, V}.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Collections;
+using System.Collections.Generic;
+using WindowsRuntime;
+
+namespace Windows.Foundation.Collections;
+
+/// <summary>
+/// Notifies listeners of dynamic changes to a map, such as when items are added or removed.
+/// </summary>
+/// <typeparam name="K">The type of keys in the observable map.</typeparam>
+/// <typeparam name="V">The type of values in the observable map.</typeparam>
+[WindowsRuntimeMetadata("Windows.Foundation.FoundationContract")]
+public interface IObservableMap<K, V> : IDictionary<K, V>, ICollection<KeyValuePair<K, V>>, IEnumerable<KeyValuePair<K, V>>, IEnumerable
+{
+    /// <summary>
+    /// Occurs when the map changes.
+    /// </summary>
+    /// <remarks>
+    /// The event handler receives an <see cref="IMapChangedEventArgs{K}"/> object that contains data that describes the event.
+    /// </remarks>
+    event MapChangedEventHandler<K, V> MapChanged;
+}
