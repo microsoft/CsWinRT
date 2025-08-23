3716cf8cc2a5209eaf80cb88a91d08bc90e8e2dd
Sergio Pedri <sergio0694@live.com>
2025-08-23 03:49:02 -0700

    Refactor event source type creation logic
    Extracted common logic for event source type creation into a new DerivedEventSource method to reduce code duplication. Updated existing methods to use the new helper for both EventHandler1 and EventHandler2 event sources.

--- src/WinRT.Interop.Generator/Builders/InteropTypeDefinitionBuilder.EventSource.cs
+++ src/WinRT.Interop.Generator/Builders/InteropTypeDefinitionBuilder.EventSource.cs
@@ -33,14 +33,72 @@ internal partial class InteropTypeDefinitionBuilder
             ModuleDefinition module,
             out TypeDefinition eventSourceType)
         {
-            TypeSignature baseEventSourceType = interopReferences.EventHandler1EventSource.MakeGenericReferenceType(delegateType.TypeArguments[0]);
+            DerivedEventSource(
+                delegateType: delegateType,
+                baseEventSourceType: interopReferences.EventHandler1EventSource,
+                baseEventSource_ctor: interopReferences.EventHandler1EventSource_ctor,
+                baseEventSourceConvertToUnmanaged: interopReferences.EventHandler1EventSourceConvertToUnmanaged(delegateType),
+                marshallerType: marshallerType,
+                interopReferences: interopReferences,
+                module: module,
+                eventSourceType: out eventSourceType);
+        }
+
+        /// <summary>
+        /// Creates a new type definition for the event source type for some <see cref="System.EventHandler{TSender, TEventArgs}"/> type.
+        /// </summary>
+        /// <param name="delegateType">The <see cref="TypeSignature"/> for the <see cref="System.EventHandler{TSender, TEventArgs}"/> type.</param>
+        /// <param name="marshallerType">The <see cref="TypeDefinition"/> instance returned by <see cref="Delegate.Marshaller"/>.</param>
+        /// <param name="interopReferences">The <see cref="InteropReferences"/> instance to use.</param>
+        /// <param name="module">The module that will contain the type being created.</param>
+        /// <param name="eventSourceType">The resulting event source type.</param>
+        public static void EventHandler2(
+            GenericInstanceTypeSignature delegateType,
+            TypeDefinition marshallerType,
+            InteropReferences interopReferences,
+            ModuleDefinition module,
+            out TypeDefinition eventSourceType)
+        {
+            DerivedEventSource(
+                delegateType: delegateType,
+                baseEventSourceType: interopReferences.EventHandler2EventSource,
+                baseEventSource_ctor: interopReferences.EventHandler2EventSource_ctor,
+                baseEventSourceConvertToUnmanaged: interopReferences.EventHandler2EventSourceConvertToUnmanaged(delegateType),
+                marshallerType: marshallerType,
+                interopReferences: interopReferences,
+                module: module,
+                eventSourceType: out eventSourceType);
+        }
+
+        /// <summary>
+        /// Creates a new type definition for the event source type for some delegate type.
+        /// </summary>
+        /// <param name="delegateType">The <see cref="TypeSignature"/> for the delegate type.</param>
+        /// <param name="baseEventSourceType">The <see cref="TypeReference"/> for the base event source type.</param>
+        /// <param name="baseEventSource_ctor">The <see cref="MemberReference"/> for the constructor of the base event source type.</param>
+        /// <param name="baseEventSourceConvertToUnmanaged">The <see cref="MemberReference"/> for the marshalling method of the base event source type.</param>
+        /// <param name="marshallerType">The <see cref="TypeDefinition"/> instance returned by <see cref="Delegate.Marshaller"/>.</param>
+        /// <param name="interopReferences">The <see cref="InteropReferences"/> instance to use.</param>
+        /// <param name="module">The module that will contain the type being created.</param>
+        /// <param name="eventSourceType">The resulting event source type.</param>
+        private static void DerivedEventSource(
+            GenericInstanceTypeSignature delegateType,
+            TypeReference baseEventSourceType,
+            MemberReference baseEventSource_ctor,
+            MemberReference baseEventSourceConvertToUnmanaged,
+            TypeDefinition marshallerType,
+            InteropReferences interopReferences,
+            ModuleDefinition module,
+            out TypeDefinition eventSourceType)
+        {
+            TypeSignature baseEventSourceSignature = baseEventSourceType.MakeGenericReferenceType([.. delegateType.TypeArguments]);
 
             // We're declaring an 'internal sealed class' type
             eventSourceType = new(
                 ns: "ABI.WindowsRuntime.InteropServices"u8,
-                name: InteropUtf8NameFactory.TypeName(baseEventSourceType),
+                name: InteropUtf8NameFactory.TypeName(baseEventSourceSignature),
                 attributes: TypeAttributes.AutoLayout | TypeAttributes.Sealed | TypeAttributes.BeforeFieldInit,
-                baseType: baseEventSourceType.Import(module).ToTypeDefOrRef());
+                baseType: baseEventSourceSignature.Import(module).ToTypeDefOrRef());
 
             module.TopLevelTypes.Add(eventSourceType);
 
@@ -63,7 +121,7 @@ internal partial class InteropTypeDefinitionBuilder
             _ = ctor.CilMethodBody!.Instructions.Insert(0, Ldarg_0);
             _ = ctor.CilMethodBody!.Instructions.Insert(1, Ldarg_1);
             _ = ctor.CilMethodBody!.Instructions.Insert(2, Ldarg_2);
-            _ = ctor.CilMethodBody!.Instructions.Insert(3, Call, interopReferences.EventHandler1EventSource_ctor.Import(module));
+            _ = ctor.CilMethodBody!.Instructions.Insert(3, Call, baseEventSource_ctor.Import(module));
 
             // Define the 'ConvertToUnmanaged' method as follows:
             //
@@ -85,71 +143,7 @@ internal partial class InteropTypeDefinitionBuilder
 
             // Add and implement the 'ConvertToUnmanaged' method
             eventSourceType.AddMethodImplementation(
-                declaration: interopReferences.EventHandler1EventSourceConvertToUnmanaged(delegateType).Import(module),
-                method: convertToUnmanagedMethod);
-        }
-
-        /// <summary>
-        /// Creates a new type definition for the event source type for some <see cref="System.EventHandler{TSender, TEventArgs}"/> type.
-        /// </summary>
-        /// <param name="delegateType">The <see cref="TypeSignature"/> for the <see cref="System.EventHandler{TSender, TEventArgs}"/> type.</param>
-        /// <param name="marshallerType">The <see cref="TypeDefinition"/> instance returned by <see cref="Delegate.Marshaller"/>.</param>
-        /// <param name="interopReferences">The <see cref="InteropReferences"/> instance to use.</param>
-        /// <param name="module">The module that will contain the type being created.</param>
-        /// <param name="eventSourceType">The resulting event source type.</param>
-        public static void EventHandler2(
-            GenericInstanceTypeSignature delegateType,
-            TypeDefinition marshallerType,
-            InteropReferences interopReferences,
-            ModuleDefinition module,
-            out TypeDefinition eventSourceType)
-        {
-            TypeSignature baseEventSourceType = interopReferences.EventHandler2EventSource.MakeGenericReferenceType([.. delegateType.TypeArguments]);
-
-            // We're declaring an 'internal sealed class' type
-            eventSourceType = new(
-                ns: "ABI.WindowsRuntime.InteropServices"u8,
-                name: InteropUtf8NameFactory.TypeName(baseEventSourceType),
-                attributes: TypeAttributes.AutoLayout | TypeAttributes.Sealed | TypeAttributes.BeforeFieldInit,
-                baseType: baseEventSourceType.Import(module).ToTypeDefOrRef());
-
-            module.TopLevelTypes.Add(eventSourceType);
-
-            // Define the constructor (same as above)
-            MethodDefinition ctor = MethodDefinition.CreateConstructor(
-                module: module,
-                parameterTypes: [
-                    interopReferences.WindowsRuntimeObjectReference.Import(module).ToReferenceTypeSignature(),
-                    module.CorLibTypeFactory.Int32]);
-
-            eventSourceType.Methods.Add(ctor);
-
-            _ = ctor.CilMethodBody!.Instructions.Insert(0, Ldarg_0);
-            _ = ctor.CilMethodBody!.Instructions.Insert(1, Ldarg_1);
-            _ = ctor.CilMethodBody!.Instructions.Insert(2, Ldarg_2);
-            _ = ctor.CilMethodBody!.Instructions.Insert(3, Call, interopReferences.EventHandler2EventSource_ctor.Import(module));
-
-            // Define the 'ConvertToUnmanaged' method as follows:
-            //
-            // public override WindowsRuntimeObjectReferenceValue ConvertToUnmanaged(<DELEGATE_TYPE> value)
-            MethodDefinition convertToUnmanagedMethod = new(
-                name: "ConvertToUnmanaged"u8,
-                attributes: MethodAttributes.Public | MethodAttributes.HideBySig | MethodAttributes.Virtual,
-                signature: MethodSignature.CreateInstance(
-                    returnType: interopReferences.WindowsRuntimeObjectReferenceValue.Import(module).ToValueTypeSignature(),
-                    parameterTypes: [delegateType.Import(module)]))
-            {
-                CilInstructions =
-                {
-                    { Ldarg_1 },
-                    { Call, marshallerType.GetMethod("ConvertToUnmanaged"u8) },
-                    { Ret }
-                }
-            };
-
-            // Add and implement the 'ConvertToUnmanaged' method
-            eventSourceType.AddMethodImplementation(
-                declaration: interopReferences.EventHandler2EventSourceConvertToUnmanaged(delegateType).Import(module),
+                declaration: baseEventSourceConvertToUnmanaged.Import(module),
                 method: convertToUnmanagedMethod);
         }
     }
