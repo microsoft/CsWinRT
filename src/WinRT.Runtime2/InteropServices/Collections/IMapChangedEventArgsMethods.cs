5365761f466eddc234a80a2dbbd0bc83a63a9c2b
Sergio Pedri <sergio0694@live.com>
2025-08-23 04:28:50 -0700

    Add IMapChangedEventArgs interop methods and vtable
    Introduces IMapChangedEventArgsMethods for accessing CollectionChange from WindowsRuntimeObjectReference, and defines IMapChangedEventArgsVftbl struct for vtable bindings. These types support interop scenarios and are intended for use by generated code only.

---
+++ src/WinRT.Runtime2/InteropServices/Collections/IMapChangedEventArgsMethods.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Windows.Foundation.Collections;
+
+namespace WindowsRuntime.InteropServices;
+
+/// <summary>
+/// Methods for <c>Windows.Foundation.Collections.IMapChangedEventArgs&lt;K&gt;</c> types.
+/// </summary>
+/// <remarks>
+/// This type should only be used by generated code.
+/// </remarks>
+[Obsolete("This type is an implementation detail, and it's only meant to be consumed by 'cswinrtgen'")]
+[EditorBrowsable(EditorBrowsableState.Never)]
+public static unsafe class IMapChangedEventArgsMethods
+{
+    /// <summary>
+    /// Gets the type of change that occurred in the map.
+    /// </summary>
+    /// <param name="thisReference">The <see cref="WindowsRuntimeObjectReference"/> instance to use to invoke the native method.</param>
+    /// <returns>The type of change in the map.</returns>
+    /// <see href="https://learn.microsoft.com/uwp/api/windows.foundation.collections.imapchangedeventargs-1.collectionchange"/>
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public static CollectionChange CollectionChange(WindowsRuntimeObjectReference thisReference)
+    {
+        using WindowsRuntimeObjectReferenceValue thisValue = thisReference.AsValue();
+
+        void* thisPtr = thisValue.GetThisPtrUnsafe();
+        CollectionChange result;
+
+        RestrictedErrorInfo.ThrowExceptionForHR(((IMapChangedEventArgsVftbl*)*(void***)thisPtr)->get_CollectionChange(thisPtr, &result));
+
+        return result;
+    }
+}
