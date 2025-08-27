6eb6f43fa8b631a4bd97a9152effbf0cc88f9a4b
Manodasan Wignarajah <mawign@microsoft.com>
2025-08-27 02:04:14 -0700

    Update testwinrt

--- src/get_testwinrt.cmd
+++ src/get_testwinrt.cmd
@@ -14,7 +14,7 @@ git checkout -f master
 if ErrorLevel 1 popd & exit /b !ErrorLevel!
 git fetch -f
 if ErrorLevel 1 popd & exit /b !ErrorLevel!
-git reset -q --hard 00fdd7b98daa0156f4214d0a8eb3afffc89efe48
+git reset -q --hard 55224a29d80cdc08c79e1f13db0c8dd376c701b2
 if ErrorLevel 1 popd & exit /b !ErrorLevel!
 echo Restoring Nuget
 %this_dir%.nuget\nuget.exe restore
