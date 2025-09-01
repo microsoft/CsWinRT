38c38fcd161d7bac3f8d4684d432dd5423ed24d9
Sergio Pedri <sergio0694@live.com>
2025-09-01 10:25:23 -0700

    Add IVectorChangedEventArgs interop support
    Introduces IVectorChangedEventArgsMethods and IVectorChangedEventArgsVftbl to provide interop methods and vtable bindings for Windows.Foundation.Collections.IVectorChangedEventArgs. These types are intended for use by generated code to access collection change and index information from native WinRT objects.

---
+++ src/WinRT.Runtime2/InteropServices/Collections/IVectorChangedEventArgsMethods.cs
@@ -0,0 +1,58 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Windows.Foundation.Collections;
+
+namespace WindowsRuntime.InteropServices;
+
+/// <summary>
+/// Methods for <c>Windows.Foundation.Collections.IVectorChangedEventArgs</c> types.
+/// </summary>
+/// <remarks>
+/// This type should only be used by generated code.
+/// </remarks>
+[Obsolete("This type is an implementation detail, and it's only meant to be consumed by 'cswinrtgen'")]
+[EditorBrowsable(EditorBrowsableState.Never)]
+public static unsafe class IVectorChangedEventArgsMethods
+{
+    /// <summary>
+    /// Gets the type of change that occurred in the vector.
+    /// </summary>
+    /// <param name="thisReference">The <see cref="WindowsRuntimeObjectReference"/> instance to use to invoke the native method.</param>
+    /// <returns>The type of change in the vector.</returns>
+    /// <see href="https://learn.microsoft.com/uwp/api/windows.foundation.collections.ivectorchangedeventargs.collectionchange"/>
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public static CollectionChange CollectionChange(WindowsRuntimeObjectReference thisReference)
+    {
+        using WindowsRuntimeObjectReferenceValue thisValue = thisReference.AsValue();
+
+        void* thisPtr = thisValue.GetThisPtrUnsafe();
+        CollectionChange result;
+
+        RestrictedErrorInfo.ThrowExceptionForHR(((IVectorChangedEventArgsVftbl*)*(void***)thisPtr)->get_CollectionChange(thisPtr, &result));
+
+        return result;
+    }
+
+    /// <summary>
+    /// Gets the index where the change occurred in the vector.
+    /// </summary>
+    /// <param name="thisReference">The <see cref="WindowsRuntimeObjectReference"/> instance to use to invoke the native method.</param>
+    /// <returns>The index where the change occurred in the vector.</returns>
+    /// <see href="https://learn.microsoft.com/uwp/api/windows.foundation.collections.ivectorchangedeventargs.index"/>
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public static uint Index(WindowsRuntimeObjectReference thisReference)
+    {
+        using WindowsRuntimeObjectReferenceValue thisValue = thisReference.AsValue();
+
+        void* thisPtr = thisValue.GetThisPtrUnsafe();
+        uint result;
+
+        RestrictedErrorInfo.ThrowExceptionForHR(((IVectorChangedEventArgsVftbl*)*(void***)thisPtr)->get_Index(thisPtr, &result));
+
+        return result;
+    }
+}
---
+++ src/WinRT.Runtime2/InteropServices/Vtables/IVectorChangedEventArgsVftbl.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.Runtime.InteropServices;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+namespace WindowsRuntime.InteropServices;
+
+/// <summary>
+/// Binding type for the <c>IVectorChangedEventArgs&lt;K&gt;</c> interface vtable.
+/// </summary>
+/// <see href="https://learn.microsoft.com/uwp/api/windows.foundation.collections.ivectorchangedeventargs"/>
+[StructLayout(LayoutKind.Sequential)]
+internal unsafe struct IVectorChangedEventArgsVftbl
+{
+    public delegate* unmanaged[MemberFunction]<void*, Guid*, void**, HRESULT> QueryInterface;
+    public delegate* unmanaged[MemberFunction]<void*, uint> AddRef;
+    public delegate* unmanaged[MemberFunction]<void*, uint> Release;
+    public delegate* unmanaged[MemberFunction]<void*, uint*, Guid**, HRESULT> GetIids;
+    public delegate* unmanaged[MemberFunction]<void*, HSTRING*, HRESULT> GetRuntimeClassName;
+    public delegate* unmanaged[MemberFunction]<void*, TrustLevel*, HRESULT> GetTrustLevel;
+    public delegate* unmanaged[MemberFunction]<void*, CollectionChange*, HRESULT> get_CollectionChange;
+    public delegate* unmanaged[MemberFunction]<void*, uint*, HRESULT> get_Index;
+}
