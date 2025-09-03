f8326998677a6d024f2758612d31606e188843e9
Sergio Pedri <sergio0694@live.com>
2025-09-03 02:35:54 -0700

    Remove unused System.Diagnostics imports
    Eliminated unnecessary using directives for System.Diagnostics in EventHandler.cs and Object.cs to clean up code and reduce dependencies.

--- src/WinRT.Runtime2/ABI/System/EventHandler.cs
+++ src/WinRT.Runtime2/ABI/System/EventHandler.cs
@@ -3,7 +3,6 @@
 
 using System;
 using System.ComponentModel;
-using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using Windows.Foundation;
--- src/WinRT.Runtime2/ABI/System/Object.cs
+++ src/WinRT.Runtime2/ABI/System/Object.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT License.
 
 using System.ComponentModel;
-using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using WindowsRuntime;
