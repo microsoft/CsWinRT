1e488f36058e26ed11b54cf9c10a91df105e16dc
Sergio Pedri <sergio0694@live.com>
2025-09-08 14:59:59 -0700

    Remove unused System.Diagnostics.CodeAnalysis imports
    Eliminated unnecessary using directives for System.Diagnostics.CodeAnalysis from multiple files to clean up code and reduce dependencies.

--- src/WinRT.Runtime2/ABI/System/Collections.Specialized/INotifyCollectionChanged.cs
+++ src/WinRT.Runtime2/ABI/System/Collections.Specialized/INotifyCollectionChanged.cs
@@ -4,7 +4,6 @@
 using System;
 using System.Collections.Specialized;
 using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Threading;
--- src/WinRT.Runtime2/ABI/System/ComponentModel/INotifyDataErrorInfo.cs
+++ src/WinRT.Runtime2/ABI/System/ComponentModel/INotifyDataErrorInfo.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Threading;
