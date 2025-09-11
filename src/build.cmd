aabae58b6601dfc431cb04327501bf2fcd9a3627
Manodasan Wignarajah <mawign@microsoft.com>
2025-09-11 11:40:43 -0700

    Update to RC1

--- src/build.cmd
+++ src/build.cmd
@@ -1,7 +1,7 @@
 @echo off
 if /i "%cswinrt_echo%" == "on" @echo on
 
-set CsWinRTBuildNetSDKVersion=10.0.100-preview.7.25380.108
+set CsWinRTBuildNetSDKVersion=10.0.100-rc.1.25451.107
 
 set this_dir=%~dp0
 
