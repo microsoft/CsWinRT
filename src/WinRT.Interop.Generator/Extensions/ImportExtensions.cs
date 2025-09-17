9a791c9223e04e8829db7b2e5cd44353f8f64202
Sergio Pedri <sergio0694@live.com>
2025-09-17 15:58:02 -0700

    Add Import extension for GenericInstanceTypeSignature
    Introduces an Import extension method for GenericInstanceTypeSignature, allowing it to be imported into a module using the default reference importer. Also updates parameter naming for clarity in the existing Import method.

--- src/WinRT.Interop.Generator/Extensions/ImportExtensions.cs
+++ src/WinRT.Interop.Generator/Extensions/ImportExtensions.cs
@@ -36,12 +36,23 @@ internal static class ImportExtensions
     /// <summary>
     /// Imports a type signature into a module using the default reference importer.
     /// </summary>
-    /// <param name="methodSignature">The <see cref="TypeSignature"/> instance to import.</param>
+    /// <param name="typeSignature">The <see cref="TypeSignature"/> instance to import.</param>
     /// <param name="module">The module to import into.</param>
     /// <returns>The imported <see cref="TypeSignature"/>.</returns>
-    public static TypeSignature Import(this TypeSignature methodSignature, ModuleDefinition module)
+    public static TypeSignature Import(this TypeSignature typeSignature, ModuleDefinition module)
     {
-        return methodSignature.ImportWith(module.DefaultImporter);
+        return typeSignature.ImportWith(module.DefaultImporter);
+    }
+
+    /// <summary>
+    /// Imports a type signature into a module using the default reference importer.
+    /// </summary>
+    /// <param name="typeSignature">The <see cref="TypeSignature"/> instance to import.</param>
+    /// <param name="module">The module to import into.</param>
+    /// <returns>The imported <see cref="TypeSignature"/>.</returns>
+    public static GenericInstanceTypeSignature Import(this GenericInstanceTypeSignature typeSignature, ModuleDefinition module)
+    {
+        return (GenericInstanceTypeSignature)typeSignature.ImportWith(module.DefaultImporter);
     }
 
     /// <summary>
