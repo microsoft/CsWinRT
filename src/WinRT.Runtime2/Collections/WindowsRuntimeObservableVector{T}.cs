f27d9c8f1035581e92c475e9ee216a5811fb56c1
Sergio Pedri <sergio0694@live.com>
2025-09-19 10:57:30 -0700

    Refactor WindowsRuntimeObservableVector to implement IList<T>
    Expanded WindowsRuntimeObservableVector to implement IList<T>, IReadOnlyList<T>, and IEnumerable<T>. Added support for standard list operations and refactored object reference management for IVector and IIterable interfaces. This improves .NET compatibility and enables direct usage of Windows Runtime vectors as .NET lists.

--- src/WinRT.Runtime2/Collections/WindowsRuntimeObservableVector{T}.cs
+++ src/WinRT.Runtime2/Collections/WindowsRuntimeObservableVector{T}.cs
@@ -2,7 +2,11 @@
 // Licensed under the MIT License.
 
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading;
 using Windows.Foundation.Collections;
 using WindowsRuntime.InteropServices;
 
@@ -31,9 +35,13 @@ public abstract class WindowsRuntimeObservableVector<
     TIIterableMethods,
     TIVector,
     TIVectorMethods,
-    TIObservableVectorMethods> : WindowsRuntimeList<T, TIIterable, TIIterableMethods, TIVectorMethods>,
+    TIObservableVectorMethods> : WindowsRuntimeObject,
     IObservableVector<T>,
-    IWindowsRuntimeInterface<IObservableVector<T>>
+    IList<T>,
+    IReadOnlyList<T>,
+    IWindowsRuntimeInterface<IObservableVector<T>>,
+    IWindowsRuntimeInterface<IList<T>>,
+    IWindowsRuntimeInterface<IEnumerable<T>>
     where TIIterable : IWindowsRuntimeInterface
     where TIIterableMethods : IIterableMethodsImpl<T>
     where TIVector : IWindowsRuntimeInterface
@@ -41,31 +49,161 @@ public abstract class WindowsRuntimeObservableVector<
     where TIObservableVectorMethods : IObservableVectorMethodsImpl<T>
 {
     /// <summary>
-    /// The cached object reference for <see cref="IObservableVector{T}"/> for the current object.
-    /// </summary>
-    private readonly WindowsRuntimeObjectReference _observableVectorObjectReference;
-
-    /// <summary>
-    /// Creates a <see cref="WindowsRuntimeList{T, TIIterable, TIEnumerableMethods, TIListMethods}"/> instance with the specified parameters.
+    /// Creates a <see cref="WindowsRuntimeObservableVector{T, TIIterable, TIIterableMethods, TIVector, TIVectorMethods, TIObservableVectorMethods}"/> instance with the specified parameters.
     /// </summary>
     /// <param name="nativeObjectReference">The inner Windows Runtime object reference to wrap in the current instance.</param>
     /// <exception cref="ArgumentNullException">Thrown if <paramref name="nativeObjectReference"/> is <see langword="null"/>.</exception>
     protected WindowsRuntimeObservableVector(WindowsRuntimeObjectReference nativeObjectReference)
-        : base(nativeObjectReference.As(in TIVector.IID))
+        : base(nativeObjectReference)
     {
-        _observableVectorObjectReference = nativeObjectReference;
+    }
+
+    /// <summary>
+    /// Gets the lazy-loaded, cached object reference for <c>Windows.Foundation.Collections.IVector&lt;T&gt;</c> for the current object.
+    /// </summary>
+    private WindowsRuntimeObjectReference IVectorObjectReference
+    {
+        get
+        {
+            [MethodImpl(MethodImplOptions.NoInlining)]
+            WindowsRuntimeObjectReference InitializeInspectableObjectReference()
+            {
+                _ = Interlocked.CompareExchange(
+                    location1: ref field,
+                    value: NativeObjectReference.As(in TIVector.IID),
+                    comparand: null);
+
+                return field;
+            }
+
+            return field ?? InitializeInspectableObjectReference();
+        }
+    }
+
+    /// <inheritdoc cref="WindowsRuntimeReadOnlyList{T, TIIterable, TIEnumerableMethods, TIReadOnlyListMethods}.IIterableObjectReference"/>
+    private WindowsRuntimeObjectReference IIterableObjectReference
+    {
+        get
+        {
+            [MethodImpl(MethodImplOptions.NoInlining)]
+            WindowsRuntimeObjectReference InitializeInspectableObjectReference()
+            {
+                _ = Interlocked.CompareExchange(
+                    location1: ref field,
+                    value: NativeObjectReference.As(in TIIterable.IID),
+                    comparand: null);
+
+                return field;
+            }
+
+            return field ?? InitializeInspectableObjectReference();
+        }
     }
 
     /// <inheritdoc/>
     public event VectorChangedEventHandler<T>? VectorChanged
     {
-        add => TIObservableVectorMethods.VectorChanged(this, _observableVectorObjectReference).Subscribe(value);
-        remove => TIObservableVectorMethods.VectorChanged(this, _observableVectorObjectReference).Unsubscribe(value);
+        add => TIObservableVectorMethods.VectorChanged(this, NativeObjectReference).Subscribe(value);
+        remove => TIObservableVectorMethods.VectorChanged(this, NativeObjectReference).Unsubscribe(value);
+    }
+
+    /// <inheritdoc/>
+    protected internal sealed override bool HasUnwrappableNativeObjectReference => true;
+
+    /// <inheritdoc/>
+    public int Count => IListMethods.Count(IVectorObjectReference);
+
+    /// <inheritdoc/>
+    public bool IsReadOnly => false;
+
+    /// <inheritdoc/>
+    public T this[int index]
+    {
+        get => IListMethods<T>.Item<TIVectorMethods>(IVectorObjectReference, index);
+        set => IListMethods<T>.Item<TIVectorMethods>(IVectorObjectReference, index, value);
+    }
+
+    /// <inheritdoc/>
+    public int IndexOf(T item)
+    {
+        return IListMethods<T>.IndexOf<TIVectorMethods>(IVectorObjectReference, item);
+    }
+
+    /// <inheritdoc/>
+    public void Insert(int index, T item)
+    {
+        IListMethods<T>.Insert<TIVectorMethods>(IVectorObjectReference, index, item);
+    }
+
+    /// <inheritdoc/>
+    public void RemoveAt(int index)
+    {
+        IListMethods.RemoveAt(IVectorObjectReference, index);
+    }
+
+    /// <inheritdoc/>
+    public void Add(T item)
+    {
+        IListMethods<T>.Add<TIVectorMethods>(IVectorObjectReference, item);
+    }
+
+    /// <inheritdoc/>
+    public void Clear()
+    {
+        IListMethods.Clear(IVectorObjectReference);
+    }
+
+    /// <inheritdoc/>
+    public bool Contains(T item)
+    {
+        return IListMethods<T>.Contains<TIVectorMethods>(IVectorObjectReference, item);
+    }
+
+    /// <inheritdoc/>
+    public void CopyTo(T[] array, int arrayIndex)
+    {
+        IListMethods<T>.CopyTo<TIVectorMethods>(IVectorObjectReference, array, arrayIndex);
+    }
+
+    /// <inheritdoc/>
+    public bool Remove(T item)
+    {
+        return IListMethods<T>.Remove<TIVectorMethods>(IVectorObjectReference, item);
+    }
+
+    /// <inheritdoc/>
+    public IEnumerator<T> GetEnumerator()
+    {
+        return TIIterableMethods.First(IIterableObjectReference);
+    }
+
+    /// <inheritdoc/>
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
     }
 
     /// <inheritdoc/>
     WindowsRuntimeObjectReferenceValue IWindowsRuntimeInterface<IObservableVector<T>>.GetInterface()
     {
-        return _observableVectorObjectReference.AsValue();
+        return NativeObjectReference.AsValue();
+    }
+
+    /// <inheritdoc/>
+    WindowsRuntimeObjectReferenceValue IWindowsRuntimeInterface<IList<T>>.GetInterface()
+    {
+        return IVectorObjectReference.AsValue();
+    }
+
+    /// <inheritdoc/>
+    WindowsRuntimeObjectReferenceValue IWindowsRuntimeInterface<IEnumerable<T>>.GetInterface()
+    {
+        return IIterableObjectReference.AsValue();
+    }
+
+    /// <inheritdoc/>
+    protected sealed override bool IsOverridableInterface(in Guid iid)
+    {
+        return false;
     }
 }
