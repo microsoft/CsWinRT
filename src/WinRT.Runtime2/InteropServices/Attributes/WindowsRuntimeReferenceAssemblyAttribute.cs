57d2ce76017902ba5dcbd9064a3daf715e759d74
Sergio Pedri <sergio0694@live.com>
2025-09-23 14:22:47 -0700

    Add WindowsRuntimeReferenceAssemblyAttribute
    Introduces a new attribute to identify assemblies as Windows Runtime reference assemblies. This attribute is intended for use by the CsWinRT generator and is analogous to ReferenceAssemblyAttribute, but specific to Windows Runtime projections.

---
+++ src/WinRT.Runtime2/InteropServices/Attributes/WindowsRuntimeReferenceAssemblyAttribute.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+
+namespace WindowsRuntime;
+
+/// <summary>
+/// Identifies an assembly as a Windows Runtime reference assembly, which contains metadata but no executable code.
+/// This is analogous to <see cref="System.Runtime.CompilerServices.ReferenceAssemblyAttribute"/>, but specifically
+/// for reference assemblies for generated Windows Runtime projections for a given Windows Runtime metadata file.
+/// </summary>
+/// <remarks>
+/// This attribute is emitted by the CsWinRT generator, and it is not meant to be used directly.
+/// </remarks>
+/// <seealso cref="System.Runtime.CompilerServices.ReferenceAssemblyAttribute"/>
+[AttributeUsage(AttributeTargets.Assembly, AllowMultiple = false, Inherited = false)]
+public sealed class WindowsRuntimeReferenceAssemblyAttribute : Attribute
+{
+    /// <summary>
+    /// Creates a new <see cref="WindowsRuntimeReferenceAssemblyAttribute"/> instance.
+    /// </summary>
+    public WindowsRuntimeReferenceAssemblyAttribute()
+    {
+    }
+}
