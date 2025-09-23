3d4701ab2f7945952b744cb5814db5347d487b4a
Sergio Pedri <sergio0694@live.com>
2025-09-22 20:12:36 -0700

    Add IObservableMapMethodsImpl interface
    Introduces IObservableMapMethodsImpl<TKey, TValue> for implementations of Windows.Foundation.Collections.IObservableMap types. This interface is intended for use by generated code and exposes a static abstract MapChanged method for event source retrieval.

---
+++ src/WinRT.Runtime2/InteropServices/Collections/IObservableMapMethodsImpl{TKey, TValue}.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.ComponentModel;
+
+namespace WindowsRuntime.InteropServices;
+
+/// <summary>
+/// An interface for implementations of <see cref="Windows.Foundation.Collections.IObservableMap{K, V}"/> types.
+/// </summary>
+/// <typeparam name="TKey">The type of keys in the observable map.</typeparam>
+/// <typeparam name="TValue">The type of values in the observable map.</typeparam>
+/// <remarks>
+/// This type should only be used by generated code.
+/// </remarks>
+[Obsolete("This type is an implementation detail, and it's only meant to be consumed by 'cswinrtgen'")]
+[EditorBrowsable(EditorBrowsableState.Never)]
+public interface IObservableMapMethodsImpl<TKey, TValue>
+{
+    /// <summary>
+    /// Returns the <see cref="EventSource{T}"/> instance associated with <see cref="Windows.Foundation.Collections.IObservableMap{K, V}.MapChanged"/>.
+    /// </summary>
+    /// <param name="thisObject">The <see cref="WindowsRuntimeObject"/> instance to use.</param>
+    /// <param name="thisReference">The <see cref="WindowsRuntimeObjectReference"/> instance to use to invoke the native method.</param>
+    /// <returns>The <see cref="EventSource{T}"/> instance associated with <see cref="Windows.Foundation.Collections.IObservableMap{K, V}.MapChanged"/>.</returns>
+    /// <see href="https://learn.microsoft.com/uwp/api/windows.foundation.collections.iobservablemap-2.mapchanged"/>
+    static abstract MapChangedEventHandlerEventSource<TKey, TValue> MapChanged(WindowsRuntimeObject thisObject, WindowsRuntimeObjectReference thisReference);
+}
