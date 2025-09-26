31c94ea9329ef16ed59199aeb00807676887c679
Manodasan Wignarajah <mawign@microsoft.com>
2025-09-26 01:02:03 -0700

    Add todo

--- src/cswinrt/code_writers.h
+++ src/cswinrt/code_writers.h
@@ -378,6 +378,7 @@ namespace cswinrt
 
     void write_interop_dll_type_name(writer& w, type_semantics const& /*semantics*/)
     {
+        // TODO
         w.write("ABI.WindowsRuntime.InteropServices.<#CsWinRT>EventHandlerEventSource`1<<#corlib>System-ComponentModel-DataErrorsChangedEventArgs>");
 
         /*for_typedef(w, semantics, [&](auto type)
