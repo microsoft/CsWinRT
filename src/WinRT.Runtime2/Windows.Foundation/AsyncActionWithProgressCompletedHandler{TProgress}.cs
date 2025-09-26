66070af91b5e860302457f90e646ed5a48af62d1
Sergio Pedri <sergio0694@live.com>
2025-09-26 13:46:55 -0700

    Add async action with progress interfaces and handlers
    Introduces IAsyncActionWithProgress<TProgress> interface and related delegate types AsyncActionProgressHandler<TProgress> and AsyncActionWithProgressCompletedHandler<TProgress> to support asynchronous actions with progress reporting in Windows.Foundation.

---
+++ src/WinRT.Runtime2/Windows.Foundation/AsyncActionWithProgressCompletedHandler{TProgress}.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Windows.Foundation.Metadata;
+using WindowsRuntime;
+
+namespace Windows.Foundation;
+
+/// <summary>
+/// Represents a method that handles the completed event of an asynchronous action that provides progress updates.
+/// </summary>
+/// <typeparam name="TProgress">The type of progress information.</typeparam>
+/// <param name="asyncInfo">The asynchronous action.</param>
+/// <param name="asyncStatus">One of the enumeration values.</param>
+[WindowsRuntimeMetadata("Windows.Foundation.FoundationContract")]
+[ContractVersion(typeof(FoundationContract), 65536u)]
+public delegate void AsyncActionWithProgressCompletedHandler<TProgress>(IAsyncActionWithProgress<TProgress> asyncInfo, AsyncStatus asyncStatus);
