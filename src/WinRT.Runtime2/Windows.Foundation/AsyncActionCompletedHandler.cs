7f4cde279eb200a9baa9a3e3a4eca8815603ecfd
Sergio Pedri <sergio0694@live.com>
2025-09-26 13:39:59 -0700

    Add AsyncActionCompletedHandler delegate
    Introduces AsyncActionCompletedHandler in Windows.Foundation to handle completion events for asynchronous operations. Includes metadata and contract version attributes.

---
+++ src/WinRT.Runtime2/Windows.Foundation/AsyncActionCompletedHandler.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Windows.Foundation.Metadata;
+using WindowsRuntime;
+
+namespace Windows.Foundation;
+
+/// <summary>
+/// Represents a method that handles the completed event of an asynchronous operation.
+/// </summary>
+/// <param name="asyncInfo">The asynchronous operation.</param>
+/// <param name="asyncStatus">One of the enumeration values.</param>
+[WindowsRuntimeMetadata("Windows.Foundation.FoundationContract")]
+[ContractVersion(typeof(FoundationContract), 65536u)]
+public delegate void AsyncActionCompletedHandler(IAsyncAction asyncInfo, AsyncStatus asyncStatus);
