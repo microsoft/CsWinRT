ac30f37cc2936efde014347917314c7923a26112
Sergio Pedri <sergio0694@live.com>
2025-09-26 21:51:40 -0700

    Add IAsyncOperationWithProgressMethodsImpl interface
    Introduces the IAsyncOperationWithProgressMethodsImpl<TResult, TProgress> interface for internal use by generated code. This interface provides static abstract methods for managing progress and completion handlers, as well as retrieving operation results for IAsyncOperationWithProgress types.

---
+++ src/WinRT.Runtime2/InteropServices/AsyncInfo/IAsyncOperationWithProgressMethodsImpl{TResult, TProgress}.cs
@@ -0,0 +1,61 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.ComponentModel;
+using Windows.Foundation;
+
+namespace WindowsRuntime.InteropServices;
+
+/// <summary>
+/// An interface for implementations of <see cref="IAsyncOperationWithProgress{TResult, TProgress}"/> types.
+/// </summary>
+/// <typeparam name="TResult">The result type.</typeparam>
+/// <typeparam name="TProgress">The type of progress information.</typeparam>
+/// <remarks>
+/// This type should only be used by generated code.
+/// </remarks>
+[Obsolete("This type is an implementation detail, and it's only meant to be consumed by 'cswinrtgen'")]
+[EditorBrowsable(EditorBrowsableState.Never)]
+public interface IAsyncOperationWithProgressMethodsImpl<TResult, TProgress>
+{
+    /// <summary>
+    /// Gets the callback method that receives progress notification.
+    /// </summary>
+    /// <param name="thisReference">The <see cref="WindowsRuntimeObjectReference"/> instance to use to invoke the native method.</param>
+    /// <returns>The callback.</returns>
+    /// <see href="https://learn.microsoft.com/uwp/api/windows.foundation.iasyncoperationwithprogress-2.progress"/>
+    static abstract AsyncOperationProgressHandler<TResult, TProgress>? Progress(WindowsRuntimeObjectReference thisReference);
+
+    /// <summary>
+    /// Sets the callback method that receives progress notification.
+    /// </summary>
+    /// <param name="thisReference">The <see cref="WindowsRuntimeObjectReference"/> instance to use to invoke the native method.</param>
+    /// <param name="handler">The callback.</param>
+    /// <see href="https://learn.microsoft.com/uwp/api/windows.foundation.iasyncoperationwithprogress-2.progress"/>
+    static abstract void Progress(WindowsRuntimeObjectReference thisReference, AsyncOperationProgressHandler<TResult, TProgress>? handler);
+
+    /// <summary>
+    /// Gets the delegate that is called when the action completes.
+    /// </summary>
+    /// <param name="thisReference">The <see cref="WindowsRuntimeObjectReference"/> instance to use to invoke the native method.</param>
+    /// <returns>The delegate.</returns>
+    /// <see href="https://learn.microsoft.com/uwp/api/windows.foundation.iasyncoperationwithprogress-2.completed"/>
+    static abstract AsyncOperationWithProgressCompletedHandler<TResult, TProgress>? Completed(WindowsRuntimeObjectReference thisReference);
+
+    /// <summary>
+    /// Sets the delegate that is called when the action completes.
+    /// </summary>
+    /// <param name="thisReference">The <see cref="WindowsRuntimeObjectReference"/> instance to use to invoke the native method.</param>
+    /// <param name="handler">The delegate.</param>
+    /// <see href="https://learn.microsoft.com/uwp/api/windows.foundation.iasyncoperationwithprogress-2.completed"/>
+    static abstract void Completed(WindowsRuntimeObjectReference thisReference, AsyncOperationWithProgressCompletedHandler<TResult, TProgress>? handler);
+
+    /// <summary>
+    /// Returns the results of the operation.
+    /// </summary>
+    /// <param name="thisReference">The <see cref="WindowsRuntimeObjectReference"/> instance to use to invoke the native method.</param>
+    /// <returns>The results of the operation.</returns>
+    /// <see href="https://learn.microsoft.com/uwp/api/windows.foundation.iasyncoperation-1.getresults"/>
+    static abstract TResult GetResults(WindowsRuntimeObjectReference thisReference);
+}
