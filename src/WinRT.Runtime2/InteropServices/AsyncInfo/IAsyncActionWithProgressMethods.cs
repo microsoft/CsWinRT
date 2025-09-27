808e8977ed3bbf85b05c8b4864572c19cab42f92
Sergio Pedri <sergio0694@live.com>
2025-09-26 21:29:22 -0700

    Add IAsyncActionWithProgressMethods utility class
    Introduces a static class providing helper methods for Windows.Foundation.IAsyncActionWithProgress<TProgress> types, intended for use by generated code. Includes a GetResults method to invoke the native implementation and handle errors.

---
+++ src/WinRT.Runtime2/InteropServices/AsyncInfo/IAsyncActionWithProgressMethods.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.ComponentModel;
+
+namespace WindowsRuntime.InteropServices;
+
+/// <summary>
+/// Methods for <see cref="Windows.Foundation.IAsyncActionWithProgress{TProgress}"/> types.
+/// </summary>
+/// <remarks>
+/// This type should only be used by generated code.
+/// </remarks>
+[Obsolete("This type is an implementation detail, and it's only meant to be consumed by 'cswinrtgen'")]
+[EditorBrowsable(EditorBrowsableState.Never)]
+public static unsafe class IAsyncActionWithProgressMethods
+{
+    /// <inheritdoc cref="Windows.Foundation.IAsyncActionWithProgress{TProgress}.GetResults"/>
+    /// <param name="thisReference">The <see cref="WindowsRuntimeObjectReference"/> instance to use to invoke the native method.</param>
+    public static void GetResults(WindowsRuntimeObjectReference thisReference)
+    {
+        using WindowsRuntimeObjectReferenceValue thisValue = thisReference.AsValue();
+
+        void* thisPtr = thisValue.GetThisPtrUnsafe();
+
+        RestrictedErrorInfo.ThrowExceptionForHR(((IAsyncActionWithProgressVftbl*)*(void***)thisPtr)->GetResults(thisPtr));
+    }
+}
