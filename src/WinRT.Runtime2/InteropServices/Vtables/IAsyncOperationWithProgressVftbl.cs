8e5d74ffb3c7575f0d68fc3b22adbf794686bfd8
Sergio Pedri <sergio0694@live.com>
2025-09-26 21:49:25 -0700

    Add IAsyncOperationWithProgress vtable struct
    Introduces IAsyncOperationWithProgressVftbl to define the vtable layout for the IAsyncOperationWithProgress<TResult, TProgress> interface, enabling interop scenarios in WinRT.Runtime2.

---
+++ src/WinRT.Runtime2/InteropServices/Vtables/IAsyncOperationWithProgressVftbl.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.Runtime.InteropServices;
+using Windows.Foundation;
+
+namespace WindowsRuntime.InteropServices;
+
+/// <summary>
+/// Binding type for the <see cref="IAsyncOperationWithProgress{TResult, TProgress}"/> interface vtable.
+/// </summary>
+/// <see href="https://learn.microsoft.com/uwp/api/windows.foundation.iasyncoperationwithprogress-2"/>
+[StructLayout(LayoutKind.Sequential)]
+internal unsafe struct IAsyncOperationWithProgressVftbl
+{
+    public delegate* unmanaged[MemberFunction]<void*, Guid*, void**, HRESULT> QueryInterface;
+    public delegate* unmanaged[MemberFunction]<void*, uint> AddRef;
+    public delegate* unmanaged[MemberFunction]<void*, uint> Release;
+    public delegate* unmanaged[MemberFunction]<void*, uint*, Guid**, HRESULT> GetIids;
+    public delegate* unmanaged[MemberFunction]<void*, HSTRING*, HRESULT> GetRuntimeClassName;
+    public delegate* unmanaged[MemberFunction]<void*, TrustLevel*, HRESULT> GetTrustLevel;
+    public delegate* unmanaged[MemberFunction]<void*, void**, HRESULT> get_Progress;
+    public delegate* unmanaged[MemberFunction]<void*, void*, HRESULT> set_Progress;
+    public delegate* unmanaged[MemberFunction]<void*, void**, HRESULT> get_Completed;
+    public delegate* unmanaged[MemberFunction]<void*, void*, HRESULT> set_Completed;
+
+    // Using 'void*' here for the 'retval' parameter, as we don't have a generic context
+    public delegate* unmanaged[MemberFunction]<void*, void*, HRESULT> GetResults;
+}
