79904800beb431f73db99e3a2d31ff95cc3bb369
Will Thant <kythant@microsoft.com>
2025-10-02 17:51:59 -0700

    SSet this back to internal

--- src/WinRT.Runtime2/InteropServices/Vtables/IUnknownVftbl.cs
+++ src/WinRT.Runtime2/InteropServices/Vtables/IUnknownVftbl.cs
@@ -12,7 +12,7 @@ namespace WindowsRuntime.InteropServices;
 /// </summary>
 /// <see href="https://learn.microsoft.com/windows/win32/api/unknwn/nn-unknwn-iunknown"/>
 [StructLayout(LayoutKind.Sequential)]
-public unsafe struct IUnknownVftbl
+internal unsafe struct IUnknownVftbl
 {
     public delegate* unmanaged[MemberFunction]<void*, Guid*, void**, HRESULT> QueryInterface;
     public delegate* unmanaged[MemberFunction]<void*, uint> AddRef;
