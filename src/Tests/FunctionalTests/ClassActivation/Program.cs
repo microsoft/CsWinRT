bc7eecc72d9ede9b9b7338169a3343cafe2694c4
Manodasan Wignarajah <mawign@microsoft.com>
2025-10-05 19:19:03 -0700

    Add basic test

--- src/Tests/FunctionalTests/ClassActivation/Program.cs
+++ src/Tests/FunctionalTests/ClassActivation/Program.cs
@@ -1,6 +1,43 @@
 ﻿using System;
+using TestComponent;
 using TestComponentCSharp;
 
+CustomDisposableTest customDisposableTest = new();
+customDisposableTest.Dispose();
+
+Composable composable = new();
+_ = composable.Value;
+composable.Value = 5;
+_ = composable.Value;
+
+_ = Composable.ExpectComposable(composable);
+_ = Composable.ExpectRequiredOne(composable);
+
+Composable composable2 = new(42);
+_ = composable2.Value;
+
+_ = Composable.ExpectComposable(composable2);
+_ = Composable.ExpectRequiredOne(composable2);
+
+_ = ComImports.NumObjects;
+
+
+/*TestComposable testComposable = new();
+
+_ = testComposable.Value;
+
+_ = Composable.ExpectComposable(testComposable);
+_ = Composable.ExpectRequiredOne(testComposable);
+
+_ = ComImports.MakeObject();
+
+*/
+
+sealed class TestComposable : Composable
+{
+}
+
+/*
 // new RCW / Factory activation
 var instance = new Class();
 
@@ -16,3 +53,4 @@ var instance2 = new Class(32);
 return instance.EnumProperty == expectedEnum && 
        instance.UriProperty == expectedUri &&
        instance2.IntProperty == 32 ? 100 : 101;
+*/
