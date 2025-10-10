464036d23d8303fe3b59bb510c5c3f6c5b3f0bc7
Manodasan Wignarajah <mawign@microsoft.com>
2025-10-10 07:51:52 -0700

    Baseline size test

--- src/Tests/FunctionalTests/ClassActivation/Program.cs
+++ src/Tests/FunctionalTests/ClassActivation/Program.cs
@@ -1,6 +1,39 @@
 ﻿using System;
+using TestComponent;
 using TestComponentCSharp;
 
+CustomDisposableTest customDisposableTest = new();
+customDisposableTest.Dispose();
+
+Composable composable = new();
+_ = composable.Value;
+composable.Value = 5;
+_ = composable.Value;
+
+_ = Composable.ExpectComposable(composable);
+_ = Composable.ExpectRequiredOne(composable);
+
+Composable composable2 = new(42);
+_ = composable2.Value;
+
+_ = Composable.ExpectComposable(composable2);
+_ = Composable.ExpectRequiredOne(composable2);
+
+_ = ComImports.NumObjects;
+_ = ComImports.MakeObject();
+
+TestComposable testComposable = new();
+
+_ = testComposable.Value;
+
+_ = Composable.ExpectComposable(testComposable);
+_ = Composable.ExpectRequiredOne(testComposable);
+
+sealed class TestComposable : Composable
+{
+}
+
+/*
 // new RCW / Factory activation
 var instance = new Class();
 
@@ -16,3 +49,4 @@ var instance2 = new Class(32);
 return instance.EnumProperty == expectedEnum && 
        instance.UriProperty == expectedUri &&
        instance2.IntProperty == 32 ? 100 : 101;
+*/
