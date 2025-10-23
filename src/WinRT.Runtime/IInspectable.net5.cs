b0a537d8d2dc42abb74a4b225bdd9da44eb6610f
Will Thant <kythant@microsoft.com>
2025-10-23 10:59:04 -0700

    Revert src/WinRT.Runtime/IInspectable.net5.cs

--- src/WinRT.Runtime/IInspectable.net5.cs
+++ src/WinRT.Runtime/IInspectable.net5.cs
@@ -4,30 +4,32 @@
 using System;
 using System.Collections.Concurrent;
 
-namespace WinRT;
-
-#if EMBED
-internal
-#else
-public
-#endif
-partial class IInspectable : IWinRTObject
+namespace WinRT
 {
-    IObjectReference IWinRTObject.NativeObject => _obj;
-    bool IWinRTObject.HasUnwrappableNativeObject => true;
-   
-    private volatile ConcurrentDictionary<RuntimeTypeHandle, IObjectReference> _queryInterfaceCache;
-    private ConcurrentDictionary<RuntimeTypeHandle, IObjectReference> MakeQueryInterfaceCache()
+#if EMBED
+    internal
+#else
+    public
+#endif
+    partial class IInspectable : IWinRTObject
     {
-        System.Threading.Interlocked.CompareExchange(ref _queryInterfaceCache, new ConcurrentDictionary<RuntimeTypeHandle, IObjectReference>(), null); 
-        return _queryInterfaceCache;
+        IObjectReference IWinRTObject.NativeObject => _obj;
+        bool IWinRTObject.HasUnwrappableNativeObject => true;
+
+        private volatile ConcurrentDictionary<RuntimeTypeHandle, IObjectReference> _queryInterfaceCache;
+        private ConcurrentDictionary<RuntimeTypeHandle, IObjectReference> MakeQueryInterfaceCache()
+        {
+            System.Threading.Interlocked.CompareExchange(ref _queryInterfaceCache, new ConcurrentDictionary<RuntimeTypeHandle, IObjectReference>(), null);
+            return _queryInterfaceCache;
+        }
+        ConcurrentDictionary<RuntimeTypeHandle, IObjectReference> IWinRTObject.QueryInterfaceCache => _queryInterfaceCache ?? MakeQueryInterfaceCache();
+        private volatile ConcurrentDictionary<RuntimeTypeHandle, object> _additionalTypeData;
+        private ConcurrentDictionary<RuntimeTypeHandle, object> MakeAdditionalTypeData()
+        {
+            System.Threading.Interlocked.CompareExchange(ref _additionalTypeData, new ConcurrentDictionary<RuntimeTypeHandle, object>(), null);
+            return _additionalTypeData;
+        }
+        ConcurrentDictionary<RuntimeTypeHandle, object> IWinRTObject.AdditionalTypeData => _additionalTypeData ?? MakeAdditionalTypeData();
     }
-    ConcurrentDictionary<RuntimeTypeHandle, IObjectReference> IWinRTObject.QueryInterfaceCache => _queryInterfaceCache ?? MakeQueryInterfaceCache();
-    private volatile ConcurrentDictionary<RuntimeTypeHandle, object> _additionalTypeData;
-    private ConcurrentDictionary<RuntimeTypeHandle, object> MakeAdditionalTypeData()
-    {
-        System.Threading.Interlocked.CompareExchange(ref _additionalTypeData, new ConcurrentDictionary<RuntimeTypeHandle, object>(), null); 
-        return _additionalTypeData;
-    }
-    ConcurrentDictionary<RuntimeTypeHandle, object> IWinRTObject.AdditionalTypeData => _additionalTypeData ?? MakeAdditionalTypeData();
+
 }
