b3ce6ef922d0147b1baca19c956a8b83fe34087d
Sergio Pedri <sergio0694@live.com>
2025-10-24 16:22:59 -0700

    Add ComWrappersCallbackType for IAsyncActionWithProgress
    Introduces the ComWrappersCallbackType method to InteropTypeDefinitionBuilder.IAsyncActionWithProgress1, enabling creation of callback types for IAsyncActionWithProgress<TProgress> interfaces. Updates InteropGenerator.Emit to utilize this new method for generating the appropriate callback type.

--- src/WinRT.Interop.Generator/Generation/InteropGenerator.Emit.cs
+++ src/WinRT.Interop.Generator/Generation/InteropGenerator.Emit.cs
@@ -1538,6 +1538,14 @@ internal partial class InteropGenerator
                     interopReferences: interopReferences,
                     module: module,
                     out TypeDefinition nativeObjectType);
+
+                InteropTypeDefinitionBuilder.IAsyncActionWithProgress1.ComWrappersCallbackType(
+                    actionType: typeSignature,
+                    nativeObjectType: nativeObjectType,
+                    get_IidMethod: get_IidMethod,
+                    interopReferences: interopReferences,
+                    module: module,
+                    out TypeDefinition operationComWrappersCallbackType);
             }
             catch (Exception e) when (!e.IsWellKnown)
             {
