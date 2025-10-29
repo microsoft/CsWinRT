98bbdd99b56bcdeb61675be797bfda89cd8dcd08
Sergio Pedri <sergio0694@live.com>
2025-10-28 18:13:37 -0700

    Add WellKnownExceptionDataKeys for exception data keys
    Introduces the WellKnownExceptionDataKeys static class to define constants for special keys used in Exception.Data for marshalled exceptions. This helps standardize the storage and retrieval of restricted error object references and related flags.

---
+++ src/WinRT.Runtime2/InteropServices/Exceptions/WellKnownExceptionDataKeys.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+
+namespace WindowsRuntime.InteropServices;
+
+/// <summary>
+/// Keys for special key-value pairs stored in <see cref="Exception.Data"/> for marshalled exceptions.
+/// </summary>
+internal static class WellKnownExceptionDataKeys
+{
+    /// <summary>
+    /// The <c>"RestrictedErrorObjectReference"</c> key for the stored <see cref="WindowsRuntimeObjectReference"/> instance.
+    /// </summary>
+    public const string RestrictedErrorObjectReference = "__RestrictedErrorObjectReference";
+
+    /// <summary>
+    /// The <c>"HasRestrictedLanguageErrorObject"</c> key indicating whether there's a stored <see cref="WindowsRuntimeObjectReference"/> instance.
+    /// </summary>
+    public const string HasRestrictedLanguageErrorObject = "__HasRestrictedLanguageErrorObject";
+}
