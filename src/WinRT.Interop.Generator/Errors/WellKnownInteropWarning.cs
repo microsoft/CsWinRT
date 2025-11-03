1b69bd7e3b9cab9aafd64341738301e354dfa55f
Sergio Pedri <sergio0694@live.com>
2025-11-03 13:08:14 -0800

    Add WellKnownInteropWarning class for interop warnings
    Introduces the WellKnownInteropWarning class to encapsulate interop generator warnings. Provides functionality to log warnings or throw exceptions based on the treatWarningsAsErrors flag.

---
+++ src/WinRT.Interop.Generator/Errors/WellKnownInteropWarning.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+namespace WindowsRuntime.InteropGenerator.Errors;
+
+/// <summary>
+/// A well known warning for the interop generator.
+/// </summary>
+internal sealed class WellKnownInteropWarning
+{
+    /// <summary>
+    /// The id of the warning.
+    /// </summary>
+    private readonly string _id;
+
+    /// <summary>
+    /// The warning message.
+    /// </summary>
+    private readonly string _message;
+
+    /// <summary>
+    /// Creates a new <see cref="WellKnownInteropException"/> instance with the specified parameters.
+    /// </summary>
+    /// <param name="id">The id of the warning.</param>
+    /// <param name="message">The warning message.</param>
+    public WellKnownInteropWarning(string id, string message)
+    {
+        _id = id;
+        _message = message;
+    }
+
+    /// <summary>
+    /// Logs the warning or throws, depending on <paramref name="treatWarningsAsErrors"/>.
+    /// </summary>
+    /// <param name="treatWarningsAsErrors">Whether to treat warnings as errors.</param>
+    /// <exception cref="WellKnownInteropException">Thrown if <paramref name="treatWarningsAsErrors"/> is <see langword="true"/>.</exception>
+    [StackTraceHidden]
+    public void LogOrThrow([DoesNotReturnIf(true)] bool treatWarningsAsErrors)
+    {
+        if (treatWarningsAsErrors)
+        {
+            throw new WellKnownInteropException(_id, _message, innerException: null);
+
+        }
+
+        Console.WriteLine($"warning {_id}: {_message}");
+    }
+}
