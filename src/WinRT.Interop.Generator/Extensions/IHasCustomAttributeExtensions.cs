cd268490627c5695becf7c272014b5a77867241a
Sergio Pedri <sergio0694@live.com>
2025-11-02 17:51:03 -0800

    Add TryGetCustomAttribute extension method
    Introduces TryGetCustomAttribute to retrieve a custom attribute of a specified type from a metadata member, returning the attribute if found. Refactors HasCustomAttribute to use the new method for improved code reuse and clarity.

--- src/WinRT.Interop.Generator/Extensions/IHasCustomAttributeExtensions.cs
+++ src/WinRT.Interop.Generator/Extensions/IHasCustomAttributeExtensions.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using System.Diagnostics.CodeAnalysis;
 using AsmResolver.DotNet;
 using AsmResolver.DotNet.Signatures;
 
@@ -12,6 +13,39 @@ namespace WindowsRuntime.InteropGenerator;
 internal static class IHasCustomAttributeExtensions
 {
     /// <summary>
+    /// Tries to get an attribute that matches a particular type from a given metadata member.
+    /// </summary>
+    /// <param name="member">The metadata member.</param>
+    /// <param name="attributeType">The attribute type to look for.</param>
+    /// <param name="attribute">The resulting attribute, if found.</param>
+    /// <returns>Whether <paramref name="attribute"/> was successfully retrieved.</returns>
+    public static bool TryGetCustomAttribute(this IHasCustomAttribute member, TypeReference attributeType, [NotNullWhen(true)] out CustomAttribute? attribute)
+    {
+        for (int i = 0; i < member.CustomAttributes.Count; i++)
+        {
+            CustomAttribute currentAttribute = member.CustomAttributes[i];
+
+            // Skip invalid cases and error scenarios (shouldn't happen under normal conditions)
+            if (currentAttribute.Type is null)
+            {
+                continue;
+            }
+
+            // Check that the attribute type is a match
+            if (SignatureComparer.IgnoreVersion.Equals(currentAttribute.Type, attributeType))
+            {
+                attribute = currentAttribute;
+
+                return true;
+            }
+        }
+
+        attribute = null;
+
+        return false;
+    }
+
+    /// <summary>
     /// Determines whether a metadata member is assigned an attribute that match a particular type.
     /// </summary>
     /// <param name="member">The metadata member.</param>
@@ -19,23 +53,6 @@ internal static class IHasCustomAttributeExtensions
     /// <returns>Whether <paramref name="member"/> has an attribute with the specified type.</returns>
     public static bool HasCustomAttribute(this IHasCustomAttribute member, TypeReference attributeType)
     {
-        for (int i = 0; i < member.CustomAttributes.Count; i++)
-        {
-            ITypeDefOrRef? declaringType = member.CustomAttributes[i].Type;
-
-            // Skip invalid cases and error scenarios (shouldn't happen under normal conditions)
-            if (declaringType is null)
-            {
-                continue;
-            }
-
-            // Check that the attribute type is a match
-            if (SignatureComparer.IgnoreVersion.Equals(declaringType, attributeType))
-            {
-                return true;
-            }
-        }
-
-        return false;
+        return TryGetCustomAttribute(member, attributeType, out _);
     }
 }
