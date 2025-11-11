cbf39b002bf41b226eafa4f04c9e5486c7903b21
Sergio Pedri <sergio0694@live.com>
2025-11-11 10:40:44 -0800

    Add overloads for TryGetCustomAttribute and update signatures
    Introduced new overloads for TryGetCustomAttribute in IHasCustomAttributeExtensions to support searching by namespace and name (string and Utf8String). Updated method signatures to use ITypeDescriptor instead of TypeReference for broader compatibility. Adjusted HasCustomAttribute and HasOrInheritsAttribute to match the new parameter types.

--- src/WinRT.Interop.Generator/Extensions/IHasCustomAttributeExtensions.cs
+++ src/WinRT.Interop.Generator/Extensions/IHasCustomAttributeExtensions.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 
 using System.Diagnostics.CodeAnalysis;
+using AsmResolver;
 using AsmResolver.DotNet;
 using AsmResolver.DotNet.Signatures;
 
@@ -19,7 +20,7 @@ internal static class IHasCustomAttributeExtensions
     /// <param name="attributeType">The attribute type to look for.</param>
     /// <param name="attribute">The resulting attribute, if found.</param>
     /// <returns>Whether <paramref name="attribute"/> was successfully retrieved.</returns>
-    public static bool TryGetCustomAttribute(this IHasCustomAttribute member, TypeReference attributeType, [NotNullWhen(true)] out CustomAttribute? attribute)
+    public static bool TryGetCustomAttribute(this IHasCustomAttribute member, ITypeDescriptor attributeType, [NotNullWhen(true)] out CustomAttribute? attribute)
     {
         for (int i = 0; i < member.CustomAttributes.Count; i++)
         {
@@ -46,13 +47,58 @@ internal static class IHasCustomAttributeExtensions
     }
 
     /// <summary>
+    /// Tries to get an attribute that matches a particular type from a given metadata member.
+    /// </summary>
+    /// <param name="member">The metadata member.</param>
+    /// <param name="ns">The namespace of the attribute type.</param>
+    /// <param name="name">The name of the attribute type.</param>
+    /// <param name="attribute">The resulting attribute, if found.</param>
+    /// <returns>Whether <paramref name="attribute"/> was successfully retrieved.</returns>
+    public static bool TryGetCustomAttribute(this IHasCustomAttribute member, string? ns, string? name, [NotNullWhen(true)] out CustomAttribute? attribute)
+    {
+        foreach (CustomAttribute currentAttribute in member.FindCustomAttributes(ns, name))
+        {
+            attribute = currentAttribute;
+
+            return true;
+        }
+
+        attribute = null;
+
+        return false;
+    }
+
+    /// <summary>
+    /// Tries to get an attribute that matches a particular type from a given metadata member.
+    /// </summary>
+    /// <param name="member">The metadata member.</param>
+    /// <param name="ns">The namespace of the attribute type.</param>
+    /// <param name="name">The name of the attribute type.</param>
+    /// <param name="attribute">The resulting attribute, if found.</param>
+    /// <returns>Whether <paramref name="attribute"/> was successfully retrieved.</returns>
+    public static bool TryGetCustomAttribute(this IHasCustomAttribute member, Utf8String? ns, Utf8String? name, [NotNullWhen(true)] out CustomAttribute? attribute)
+    {
+        foreach (CustomAttribute currentAttribute in member.FindCustomAttributes(ns, name))
+        {
+            attribute = currentAttribute;
+
+            return true;
+        }
+
+        attribute = null;
+
+        return false;
+    }
+
+    /// <summary>
     /// Determines whether a metadata member is assigned an attribute that match a particular type.
     /// </summary>
     /// <param name="member">The metadata member.</param>
     /// <param name="attributeType">The attribute type to look for.</param>
     /// <returns>Whether <paramref name="member"/> has an attribute with the specified type.</returns>
-    public static bool HasCustomAttribute(this IHasCustomAttribute member, TypeReference attributeType)
+    public static bool HasCustomAttribute(this IHasCustomAttribute member, ITypeDescriptor attributeType)
     {
         return TryGetCustomAttribute(member, attributeType, out _);
     }
+
 }
