b74be01d59e2469889941bee0925ff4a1b0952f3
Sergio Pedri <sergio0694@live.com>
2025-11-12 16:24:25 -0800

    Add WindowsRuntimeMetadataClassNameAttribute
    Introduces WindowsRuntimeMetadataClassNameAttribute for specifying runtime class names in generated projections. This attribute is marked obsolete and is intended for internal use, mirroring WindowsRuntimeClassNameAttribute but with restricted usage.

---
+++ src/WinRT.Runtime2/InteropServices/Attributes/WindowsRuntimeMetadataClassNameAttribute.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.ComponentModel;
+
+namespace WindowsRuntime.InteropServices;
+
+/// <summary>
+/// Indicates the runtime class name to use for types exposed to the Windows Runtime.
+/// </summary>
+/// <remarks>
+/// This is conceptually the same as <see cref="WindowsRuntimeClassNameAttribute"/>, but only used by generated projections,
+/// so that the public attribute can have its targets restricted to only the scenarios that would be valid for user types.
+/// </remarks>
+/// <see cref="WindowsRuntimeClassNameAttribute"/>
+[AttributeUsage(
+    AttributeTargets.Class |
+    AttributeTargets.Struct |
+    AttributeTargets.Enum |
+    AttributeTargets.Delegate,
+    AllowMultiple = false,
+    Inherited = false)]
+[Obsolete(WindowsRuntimeConstants.PrivateImplementationDetailObsoleteMessage,
+    DiagnosticId = WindowsRuntimeConstants.PrivateImplementationDetailObsoleteDiagnosticId,
+    UrlFormat = WindowsRuntimeConstants.CsWinRTDiagnosticsUrlFormat)]
+[EditorBrowsable(EditorBrowsableState.Never)]
+public sealed class WindowsRuntimeMetadataClassNameAttribute : Attribute
+{
+    /// <summary>
+    /// Creates a new <see cref="WindowsRuntimeMetadataClassNameAttribute"/> instance with the specified parameters.
+    /// </summary>
+    /// <param name="runtimeClassName">The runtime class name to use.</param>
+    public WindowsRuntimeMetadataClassNameAttribute(string runtimeClassName)
+    {
+        RuntimeClassName = runtimeClassName;
+    }
+
+    /// <summary>
+    /// Gets the runtime class name for the current instance.
+    /// </summary>
+    public string RuntimeClassName { get; }
+}
