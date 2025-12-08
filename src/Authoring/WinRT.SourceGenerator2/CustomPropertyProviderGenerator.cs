d658c80e3b6daf245a562d8874542ff8fc8d2851
Sergio Pedri <sergio0694@live.com>
2025-12-08 15:26:29 -0800

    Comment out bindable custom property generation logic
    The code responsible for generating bindable custom properties in CustomPropertyProviderGenerator has been commented out, likely to temporarily disable this feature or for refactoring purposes.

--- src/Authoring/WinRT.SourceGenerator2/CustomPropertyProviderGenerator.cs
+++ src/Authoring/WinRT.SourceGenerator2/CustomPropertyProviderGenerator.cs
@@ -1,10 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-using System;
-using System.Collections.Immutable;
-using System.Linq;
-using System.Threading;
 using Microsoft.CodeAnalysis;
 
 namespace WindowsRuntime.SourceGenerator;
@@ -18,16 +14,16 @@ public sealed partial class CustomPropertyProviderGenerator : IIncrementalGenera
     /// <inheritdoc/>
     public void Initialize(IncrementalGeneratorInitializationContext context)
     {
-        var bindableCustomPropertyAttributes = context.ForAttributeWithMetadataNameAndOptions(
-            fullyQualifiedMetadataName: "WinRT.GeneratedBindableCustomPropertyAttribute",
-            predicate: Execute.IsTargetNodeValid,
-            transform: static (n, _) => n)
-        .Combine(properties)
-        .Select(static ((GeneratorAttributeSyntaxContext generatorSyntaxContext, CsWinRTAotOptimizerProperties properties) value, CancellationToken _) =>
-                value.properties.IsCsWinRTAotOptimizerEnabled ? GetBindableCustomProperties(value.generatorSyntaxContext) : default)
-        .Where(static bindableCustomProperties => bindableCustomProperties != default)
-        .Collect()
-        .Combine(properties);
-        context.RegisterImplementationSourceOutput(bindableCustomPropertyAttributes, GenerateBindableCustomProperties);
+        //var bindableCustomPropertyAttributes = context.ForAttributeWithMetadataNameAndOptions(
+        //    fullyQualifiedMetadataName: "WinRT.GeneratedBindableCustomPropertyAttribute",
+        //    predicate: Execute.IsTargetNodeValid,
+        //    transform: static (n, _) => n)
+        //.Combine(properties)
+        //.Select(static ((GeneratorAttributeSyntaxContext generatorSyntaxContext, CsWinRTAotOptimizerProperties properties) value, CancellationToken _) =>
+        //        value.properties.IsCsWinRTAotOptimizerEnabled ? GetBindableCustomProperties(value.generatorSyntaxContext) : default)
+        //.Where(static bindableCustomProperties => bindableCustomProperties != default)
+        //.Collect()
+        //.Combine(properties);
+        //context.RegisterImplementationSourceOutput(bindableCustomPropertyAttributes, GenerateBindableCustomProperties);
     }
 }
