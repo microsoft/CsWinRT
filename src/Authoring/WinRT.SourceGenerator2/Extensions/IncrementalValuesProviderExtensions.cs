869d57fd23aadd278572ce41b055acc8995e7133
Sergio Pedri <sergio0694@live.com>
2025-12-08 16:01:54 -0800

    Add SkipNullValues extension for IncrementalValuesProvider
    Introduces an extension method to filter out null values from IncrementalValuesProvider instances, improving safety and convenience when working with nullable types in source generators.

---
+++ src/Authoring/WinRT.SourceGenerator2/Extensions/IncrementalValuesProviderExtensions.cs
@@ -0,0 +1,24 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Microsoft.CodeAnalysis;
+
+namespace WindowsRuntime.SourceGenerator;
+
+/// <summary>
+/// Extensions for <see cref="IncrementalValuesProvider{TValues}"/>.
+/// </summary>
+internal static class IncrementalValuesProviderExtensions
+{
+    /// <summary>
+    /// Skips all <see langword="null"/> values from a given provider.
+    /// </summary>
+    /// <typeparam name="T">The type of values being produced.</typeparam>
+    /// <param name="provider">The input <see cref="IncrementalValuesProvider{TValues}"/> instance.</param>
+    /// <returns>The resulting <see cref="IncrementalValuesProvider{TValues}"/> instance.</returns>
+    public static IncrementalValuesProvider<T> SkipNullValues<T>(IncrementalValuesProvider<T?> provider)
+        where T : class
+    {
+        return provider.Where(static value => value is not null)!;
+    }
+}
