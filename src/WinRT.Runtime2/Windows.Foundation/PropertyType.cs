d59c39b056fba1f16f781759e8c3216763487d02
Sergio Pedri <sergio0694@live.com>
2025-12-11 15:13:07 -0800

    Remove Obsolete attribute from PropertyType enum
    The Obsolete attribute and its related parameters were removed from the PropertyType enum in Windows.Foundation. This change likely reflects an update in deprecation policy or usage guidance.

--- src/WinRT.Runtime2/Windows.Foundation/PropertyType.cs
+++ src/WinRT.Runtime2/Windows.Foundation/PropertyType.cs
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-using System;
 using System.ComponentModel;
 using System.Runtime.Versioning;
 using Windows.Foundation.Metadata;
@@ -19,9 +18,6 @@ namespace Windows.Foundation;
 [WindowsRuntimeMetadata("Windows.Foundation.FoundationContract")]
 [SupportedOSPlatform("Windows10.0.10240.0")]
 [ContractVersion(typeof(FoundationContract), 65536u)]
-[Obsolete(WindowsRuntimeConstants.PrivateImplementationDetailObsoleteMessage,
-    DiagnosticId = WindowsRuntimeConstants.PrivateImplementationDetailObsoleteDiagnosticId,
-    UrlFormat = WindowsRuntimeConstants.CsWinRTDiagnosticsUrlFormat)]
 [EditorBrowsable(EditorBrowsableState.Never)]
 public enum PropertyType : uint
 {
