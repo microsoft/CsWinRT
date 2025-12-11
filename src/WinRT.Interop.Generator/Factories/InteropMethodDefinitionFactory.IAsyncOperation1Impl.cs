37b648c15db87f1489a9a374f33d36126fa29e3a
Sergio Pedri <sergio0694@live.com>
2025-12-11 14:38:12 -0800

    Add IAsyncOperation<TResult> GetResults method factory
    Introduces a static helper to generate the GetResults method implementation for Windows.Foundation.IAsyncOperation<TResult> and related interfaces. The method handles pointer validation, exception marshaling, and result value tracking for interop scenarios.

---
+++ src/WinRT.Interop.Generator/Factories/InteropMethodDefinitionFactory.IAsyncOperation1Impl.cs
@@ -0,0 +1,125 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using AsmResolver.DotNet;
+using AsmResolver.DotNet.Code.Cil;
+using AsmResolver.DotNet.Signatures;
+using AsmResolver.PE.DotNet.Cil;
+using AsmResolver.PE.DotNet.Metadata.Tables;
+using WindowsRuntime.InteropGenerator.Generation;
+using WindowsRuntime.InteropGenerator.References;
+using static AsmResolver.PE.DotNet.Cil.CilOpCodes;
+
+namespace WindowsRuntime.InteropGenerator.Factories;
+
+/// <inheritdoc cref="InteropMethodDefinitionFactory"/>
+internal partial class InteropMethodDefinitionFactory
+{
+    /// <summary>
+    /// Helpers for impl types for <c>Windows.Foundation.IAsyncOperation&lt;TResult&gt;</c> interfaces.
+    /// </summary>
+    public static class IAsyncOperation1Impl
+    {
+        /// <summary>
+        /// Creates a <see cref="MethodDefinition"/> for the <c>GetResults</c> export method.
+        /// </summary>
+        /// <param name="operationType">The <see cref="TypeSignature"/> for the async operation type.</param>
+        /// <param name="getResultsMethod">The interface method to invoke on <paramref name="operationType"/>.</param>
+        /// <param name="interopReferences">The <see cref="InteropReferences"/> instance to use.</param>
+        /// <param name="emitState">The emit state for this invocation.</param>
+        /// <param name="module">The interop module being built.</param>
+        /// /// <remarks>
+        /// This method can also be used to define the <c>GetResults</c> method for <c>Windows.Foundation.IAsyncOperationWithProgress&lt;TResult,TProgress&gt;</c> interfaces.
+        /// </remarks>
+        public static MethodDefinition GetResults(
+            GenericInstanceTypeSignature operationType,
+            MemberReference getResultsMethod,
+            InteropReferences interopReferences,
+            InteropGeneratorEmitState emitState,
+            ModuleDefinition module)
+        {
+            TypeSignature resultType = operationType.TypeArguments[0];
+
+            // Define the 'GetResults' method as follows:
+            //
+            // [UnmanagedCallersOnly(CallConvs = [typeof(CallConvMemberFunction)])]
+            // private static int GetResults(void* thisPtr, <ABI_ELEMENT_TYPE>* result)
+            MethodDefinition getResultsImplMethod = new(
+                name: "GetResults"u8,
+                attributes: MethodAttributes.Private | MethodAttributes.HideBySig | MethodAttributes.Static,
+                signature: MethodSignature.CreateStatic(
+                    returnType: module.CorLibTypeFactory.Int32,
+                    parameterTypes: [
+                        module.CorLibTypeFactory.Void.MakePointerType(),
+                        resultType.GetAbiType(interopReferences).Import(module).MakePointerType()]))
+            {
+                CustomAttributes = { InteropCustomAttributeFactory.UnmanagedCallersOnly(interopReferences, module) }
+            };
+
+            // Labels for jumps
+            CilInstruction nop_beforeTry = new(Nop);
+            CilInstruction ldarg_1_tryStart = new(Ldarg_1);
+            CilInstruction ldloc_0_returnHResult = new(Ldloc_0);
+            CilInstruction call_catchStartMarshalException = new(Call, interopReferences.RestrictedErrorInfoExceptionMarshallerConvertToUnmanaged.Import(module));
+            CilInstruction nop_convertToUnmanaged = new(Nop);
+
+            // Create a method body for the 'GetResults' method
+            getResultsImplMethod.CilMethodBody = new CilMethodBody()
+            {
+                // Declare 1 variable:
+                //   [0]: 'int' (the 'HRESULT' to return)
+                LocalVariables = { new CilLocalVariable(module.CorLibTypeFactory.Int32) },
+                Instructions =
+                {
+                    // Return 'E_POINTER' if the argument is 'null'
+                    { Ldarg_1 },
+                    { Ldc_I4_0 },
+                    { Conv_U },
+                    { Bne_Un_S, nop_beforeTry.CreateLabel() },
+                    { Ldc_I4, unchecked((int)0x80004003) },
+                    { Ret },
+                    { nop_beforeTry },
+
+                    // '.try' code
+                    { ldarg_1_tryStart },
+                    { Ldarg_0 },
+                    { Call, interopReferences.ComInterfaceDispatchGetInstance.MakeGenericInstanceMethod(operationType).Import(module) },
+                    { Callvirt, getResultsMethod.Import(module) },
+                    { nop_convertToUnmanaged },
+                    { Ldc_I4_0 },
+                    { Stloc_0 },
+                    { Leave_S, ldloc_0_returnHResult.CreateLabel() },
+
+                    // '.catch' code
+                    { call_catchStartMarshalException },
+                    { Stloc_0 },
+                    { Leave_S, ldloc_0_returnHResult.CreateLabel() },
+
+                    // Return the 'HRESULT' from location [0]
+                    { ldloc_0_returnHResult  },
+                    { Ret }
+                },
+                ExceptionHandlers =
+                {
+                    new CilExceptionHandler
+                    {
+                        HandlerType = CilExceptionHandlerType.Exception,
+                        TryStart = ldarg_1_tryStart.CreateLabel(),
+                        TryEnd = call_catchStartMarshalException.CreateLabel(),
+                        HandlerStart = call_catchStartMarshalException.CreateLabel(),
+                        HandlerEnd = ldloc_0_returnHResult.CreateLabel(),
+                        ExceptionType = interopReferences.Exception.Import(module)
+                    }
+                }
+            };
+
+            // Track the method for rewrite to marshal the result value
+            emitState.TrackRetValValueMethodRewrite(
+                retValType: resultType,
+                method: getResultsImplMethod,
+                marker: nop_convertToUnmanaged);
+
+            return getResultsImplMethod;
+        }
+    }
+}
