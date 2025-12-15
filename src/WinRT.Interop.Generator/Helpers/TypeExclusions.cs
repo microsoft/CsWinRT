cea2ace33f3fe8f72c386f4d50b505887cac9c40
Sergio Pedri <sergio0694@live.com>
2025-12-15 15:46:31 -0800

    Add TypeExclusions helper for interop API filtering
    Introduces a TypeExclusions helper class to centralize logic for excluding specific types, such as Task<T>, from the interop API surface. Also adds a Task1 reference to InteropReferences to support this exclusion logic.

---
+++ src/WinRT.Interop.Generator/Helpers/TypeExclusions.cs
@@ -0,0 +1,42 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using AsmResolver.DotNet;
+using AsmResolver.DotNet.Signatures;
+using WindowsRuntime.InteropGenerator.References;
+
+namespace WindowsRuntime.InteropGenerator.Helpers;
+
+/// <summary>
+/// Contains logic to handle special-cases types to exclude from the interop API surface.
+/// </summary>
+internal static class TypeExclusions
+{
+    /// <summary>
+    /// Checks whether a given type should be excluded from the interop API surface.
+    /// </summary>
+    /// <param name="type">The type to check.</param>
+    /// <param name="interopReferences">The <see cref="InteropReferences"/> instance to use.</param>
+    /// <returns>Whether <paramref name="type"/> should be excluded from the interop API surface.</returns>
+    public static bool IsExcluded(ITypeDescriptor type, InteropReferences interopReferences)
+    {
+        // If we have a constructed generic type, extract the generic type definition
+        // and use that for checking. We don't have exclusion logic for type arguments.
+        if (type is GenericInstanceTypeSignature typeSignature)
+        {
+            return IsExcluded(typeSignature.GenericType, interopReferences);
+        }
+
+        // Check if the input type matches any of our exclusions
+        foreach (TypeReference excludedType in (ReadOnlySpan<TypeReference>)[interopReferences.Task1])
+        {
+            if (SignatureComparer.IgnoreVersion.Equals(type, excludedType))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
