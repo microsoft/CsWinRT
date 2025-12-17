67e3d02495ade14895be5ae179b5d76bc09d2403
Sergio Pedri <sergio0694@live.com>
2025-12-16 20:57:45 -0800

    Add tests for GeneratedCustomPropertyProvider analyzer
    Introduces unit tests for the GeneratedCustomPropertyProviderTargetTypeAnalyzer to verify correct diagnostics for valid and invalid target types, partial type requirements, and type hierarchy scenarios.

---
+++ src/Tests/SourceGenerator2Test/Test_GeneratedCustomPropertyProviderTargetTypeAnalyzer.cs
@@ -0,0 +1,78 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WindowsRuntime.SourceGenerator.Diagnostics;
+using WindowsRuntime.SourceGenerator.Tests.Helpers;
+
+namespace WindowsRuntime.SourceGenerator.Tests;
+
+[TestClass]
+public class Test_GeneratedCustomPropertyProviderTargetTypeAnalyzer
+{
+    [TestMethod]
+    [DataRow("class")]
+    [DataRow("struct")]
+    public async Task ValidTargetType_DoesNotWarn(string modifier)
+    {
+        string source = $$"""
+            using WindowsRuntime.Xaml;
+
+            [GeneratedCustomPropertyProvider]
+            public partial {{modifier}} MyType;
+            """;
+
+        await CSharpAnalyzerTest<GeneratedCustomPropertyProviderTargetTypeAnalyzer>.VerifyAnalyzerAsync(source);
+    }
+
+    [TestMethod]
+    [DataRow("abstract class")]
+    [DataRow("static class")]
+    [DataRow("static struct")]
+    [DataRow("ref struct")]
+    public async Task InvalidTargetType_Warns(string modifiers)
+    {
+        string source = $$"""
+            using WindowsRuntime.Xaml;
+
+            [{|CSWINRT2000:GeneratedCustomPropertyProvider|}]
+            public {{modifiers}} MyType;
+            """;
+
+        await CSharpAnalyzerTest<GeneratedCustomPropertyProviderTargetTypeAnalyzer>.VerifyAnalyzerAsync(source);
+    }
+
+    [TestMethod]
+    [DataRow("class")]
+    [DataRow("struct")]
+    public async Task TypeNotPartial_Warns(string modifier)
+    {
+        string source = $$"""
+            using WindowsRuntime.Xaml;
+
+            [{|CSWINRT2001:GeneratedCustomPropertyProvider|}]
+            public {{modifier}} MyType;
+            """;
+
+        await CSharpAnalyzerTest<GeneratedCustomPropertyProviderTargetTypeAnalyzer>.VerifyAnalyzerAsync(source);
+    }
+
+    [TestMethod]
+    [DataRow("class")]
+    [DataRow("struct")]
+    public async Task TypeNotInPartialTypeHierarchy_Warns(string modifier)
+    {
+        string source = $$"""
+            using WindowsRuntime.Xaml;
+
+            public class ParentType
+            {
+                [{|CSWINRT2001:GeneratedCustomPropertyProvider|}]
+                public partial {{modifier}} MyType;
+            }
+            """;
+
+        await CSharpAnalyzerTest<GeneratedCustomPropertyProviderTargetTypeAnalyzer>.VerifyAnalyzerAsync(source);
+    }
+}
