024b3c383d4cbc9e3080f4ee2155f58fef33d761
Sergio Pedri <sergio0694@live.com>
2025-12-22 20:56:01 -0800

    Add Conv_U opcode in IEnumerator1 builder
    Inserted the Conv_U opcode in the IL instruction sequence within InteropTypeDefinitionBuilder.IEnumerator1. This change likely ensures correct type conversion for native interop scenarios.

--- src/WinRT.Interop.Generator/Builders/InteropTypeDefinitionBuilder.IEnumerator1.cs
+++ src/WinRT.Interop.Generator/Builders/InteropTypeDefinitionBuilder.IEnumerator1.cs
@@ -100,6 +100,7 @@ internal partial class InteropTypeDefinitionBuilder
                     { Stloc_1 },
                     { Ldloc_1 },
                     { Ldloca_S, loc_2_currentNative },
+                    { Conv_U },
                     { Ldloc_1 },
                     { Ldind_I },
                     { Ldfld, interopDefinitions.IEnumerator1Vftbl.GetField("get_Current"u8) },
