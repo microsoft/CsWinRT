a7e2c91bde7e2e6f6fa40846395d3b6f13db3eb9
Sergio Pedri <sergio0694@live.com>
2026-01-03 18:20:10 -0800

    Add IDictionaryAdapter and extension methods
    Introduces IDictionaryAdapter<TKey, TValue> as a stateless adapter for IDictionary to expose Windows.Foundation.Collections.IMap functionality. Adds IDictionaryAdapterExtensions for string-keyed dictionaries to optimize lookup, removal, and key existence checks using ReadOnlySpan<char> to avoid string allocations.

---
+++ src/WinRT.Runtime2/InteropServices/Collections/IDictionaryAdapterExtensions.cs
@@ -0,0 +1,149 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Frozen;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+#pragma warning disable IDE0045, IDE0046
+
+namespace WindowsRuntime.InteropServices;
+
+/// <summary>
+/// Extensions for the <see cref="IDictionaryAdapter{TKey, TValue}"/> type.
+/// </summary>
+[Obsolete(WindowsRuntimeConstants.PrivateImplementationDetailObsoleteMessage,
+    DiagnosticId = WindowsRuntimeConstants.PrivateImplementationDetailObsoleteDiagnosticId,
+    UrlFormat = WindowsRuntimeConstants.CsWinRTDiagnosticsUrlFormat)]
+[EditorBrowsable(EditorBrowsableState.Never)]
+public static class IDictionaryAdapterExtensions
+{
+    extension<TValue>(IDictionaryAdapter<string, TValue>)
+    {
+        /// <inheritdoc cref="IDictionaryAdapter{TKey, TValue}.Lookup"/>
+        /// <remarks>
+        /// This overload can be used to avoid a <see cref="string"/> allocation on the caller side.
+        /// </remarks>
+        public static TValue Lookup(IDictionary<string, TValue> dictionary, ReadOnlySpan<char> key)
+        {
+            bool found;
+            TValue? value;
+
+            // Same logic as in 'IReadOnlyDictionaryAdapterExtensions.Lookup' for trying to avoid materializing the 'string' key
+            if (dictionary.GetType() == typeof(Dictionary<string, TValue>) &&
+                Unsafe.As<Dictionary<string, TValue>>(dictionary).TryGetAlternateLookup(out Dictionary<string, TValue>.AlternateLookup<ReadOnlySpan<char>> lookup1))
+            {
+                found = lookup1.TryGetValue(key, out value);
+            }
+            else if (dictionary.GetType() == typeof(ConcurrentDictionary<string, TValue>) &&
+                     Unsafe.As<ConcurrentDictionary<string, TValue>>(dictionary).TryGetAlternateLookup(out ConcurrentDictionary<string, TValue>.AlternateLookup<ReadOnlySpan<char>> lookup2))
+            {
+                found = lookup2.TryGetValue(key, out value);
+            }
+            else if (dictionary is FrozenDictionary<string, TValue> candidate3 &&
+                     candidate3.TryGetAlternateLookup(out FrozenDictionary<string, TValue>.AlternateLookup<ReadOnlySpan<char>> lookup3))
+            {
+                found = lookup3.TryGetValue(key, out value);
+            }
+            else
+            {
+                found = dictionary.TryGetValue(key.ToString(), out value);
+            }
+
+            // Throw the correct exception if the lookup failed
+            if (!found)
+            {
+                [DoesNotReturn]
+                static void ThrowKeyNotFoundException()
+                {
+                    throw new KeyNotFoundException("Arg_KeyNotFoundWithKey")
+                    {
+                        HResult = WellKnownErrorCodes.E_BOUNDS
+                    };
+                }
+
+                ThrowKeyNotFoundException();
+            }
+
+            return value!;
+        }
+
+        /// <summary>
+        /// Determines whether the map contains the specified key.
+        /// </summary>
+        /// <param name="dictionary">The wrapped <see cref="IReadOnlyDictionary{TKey, TValue}"/> instance.</param>
+        /// <param name="key">The key to locate in the map.</param>
+        /// <returns>Whether the key was found.</returns>
+        /// <remarks>
+        /// This overload can be used to avoid a <see cref="string"/> allocation on the caller side.
+        /// </remarks>
+        /// /// <see href="https://learn.microsoft.com/uwp/api/windows.foundation.collections.imap-2.haskey"/>
+        public static bool HasKey(IDictionary<string, TValue> dictionary, ReadOnlySpan<char> key)
+        {
+            // Same logic as in 'Lookup' above for trying to avoid materializing the 'string' key
+            if (dictionary.GetType() == typeof(Dictionary<string, TValue>) &&
+                Unsafe.As<Dictionary<string, TValue>>(dictionary).TryGetAlternateLookup(out Dictionary<string, TValue>.AlternateLookup<ReadOnlySpan<char>> lookup1))
+            {
+                return lookup1.ContainsKey(key);
+            }
+
+            if (dictionary.GetType() == typeof(ConcurrentDictionary<string, TValue>) &&
+                Unsafe.As<ConcurrentDictionary<string, TValue>>(dictionary).TryGetAlternateLookup(out ConcurrentDictionary<string, TValue>.AlternateLookup<ReadOnlySpan<char>> lookup2))
+            {
+                return lookup2.ContainsKey(key);
+            }
+
+            if (dictionary is FrozenDictionary<string, TValue> candidate3 &&
+                candidate3.TryGetAlternateLookup(out FrozenDictionary<string, TValue>.AlternateLookup<ReadOnlySpan<char>> lookup3))
+            {
+                return lookup3.ContainsKey(key);
+            }
+
+            return dictionary.ContainsKey(key.ToString());
+        }
+
+        /// <inheritdoc cref="IDictionaryAdapter{TKey, TValue}.Remove"/>
+        /// <remarks>
+        /// This overload can be used to avoid a <see cref="string"/> allocation on the caller side.
+        /// </remarks>
+        public static void Remove(IDictionary<string, TValue> dictionary, ReadOnlySpan<char> key)
+        {
+            bool removed;
+
+            // Same logic as in 'IReadOnlyDictionaryAdapterExtensions.Lookup' for trying to avoid materializing the 'string' key.
+            // We don't handle 'FrozenDictionary<TKey, TValue>' since instances of that type are immutable (removing would throw).
+            if (dictionary.GetType() == typeof(Dictionary<string, TValue>) &&
+                Unsafe.As<Dictionary<string, TValue>>(dictionary).TryGetAlternateLookup(out Dictionary<string, TValue>.AlternateLookup<ReadOnlySpan<char>> lookup1))
+            {
+                removed = lookup1.Remove(key);
+            }
+            else if (dictionary.GetType() == typeof(ConcurrentDictionary<string, TValue>) &&
+                     Unsafe.As<ConcurrentDictionary<string, TValue>>(dictionary).TryGetAlternateLookup(out ConcurrentDictionary<string, TValue>.AlternateLookup<ReadOnlySpan<char>> lookup2))
+            {
+                removed = lookup2.TryRemove(key, out _);
+            }
+            else
+            {
+                removed = dictionary.Remove(key.ToString());
+            }
+
+            if (!removed)
+            {
+                [DoesNotReturn]
+                static void ThrowKeyNotFoundException()
+                {
+                    throw new KeyNotFoundException("Arg_KeyNotFoundWithKey")
+                    {
+                        HResult = WellKnownErrorCodes.E_BOUNDS
+                    };
+                }
+
+                ThrowKeyNotFoundException();
+            }
+        }
+    }
+}
