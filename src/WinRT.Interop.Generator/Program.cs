fe1d5fc63fc137cbfc8ff5f7fc79ceaae42ef9a4
Will Thant <kythant@microsoft.com>
2026-01-15 11:44:43 -0800

    Remove debugging code from Program.cs

--- src/WinRT.Interop.Generator/Program.cs
+++ src/WinRT.Interop.Generator/Program.cs
@@ -4,7 +4,5 @@
 using ConsoleAppFramework;
 using WindowsRuntime.InteropGenerator.Generation;
 
-args = [@"C:\Users\kythant\staging\debug-repro.zip"];
-
 // Run the interop generator with all parsed arguments
 ConsoleApp.Run(args, InteropGenerator.Run);
