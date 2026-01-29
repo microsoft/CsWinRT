68df9c8ccbf4b17082f87d2edb406515240f9669
Sergio Pedri <sergio0694@live.com>
2026-01-28 18:57:54 -0800

    Add WindowsRuntimeMapped attributes to DataErrorsChangedEventArgs
    Annotated DataErrorsChangedEventArgs ABI type with WindowsRuntimeMappedMetadata and WindowsRuntimeMappedType attributes to improve metadata mapping and type resolution.

--- src/WinRT.Runtime2/ABI/System/ComponentModel/DataErrorsChangedEventArgs.cs
+++ src/WinRT.Runtime2/ABI/System/ComponentModel/DataErrorsChangedEventArgs.cs
@@ -26,6 +26,8 @@ namespace ABI.System.ComponentModel;
 /// ABI type for <see cref="global::System.ComponentModel.DataErrorsChangedEventArgs"/>.
 /// </summary>
 /// <see href="https://learn.microsoft.com/windows/windows-app-sdk/api/winrt/microsoft.ui.xaml.data.dataerrorschangedeventargs"/>
+[WindowsRuntimeMappedMetadata("Microsoft.UI.Xaml.WinUIContract")]
+[WindowsRuntimeMappedType(typeof(global::System.ComponentModel.DataErrorsChangedEventArgs))]
 [DataErrorsChangedEventArgsComWrappersMarshaller]
 file static class DataErrorsChangedEventArgs;
 
