a95d7a9c8f3ec64f07379c52558b24d120c090bb
Sergio Pedri <sergio0694@live.com>
2026-01-30 14:44:12 -0800

    Make TypeKind public, obsolete, and hidden
    Change TypeKind from internal to public and mark it as an obsolete private implementation detail. Replace Windows Runtime platform/contract attributes with an Obsolete attribute (using WindowsRuntimeConstants) and EditorBrowsableState.Never to hide the type from IntelliSense. Also adjust using directives accordingly and remove Windows.Foundation/metadata/platform-specific attributes.

--- src/WinRT.Runtime2/Windows.UI.Xaml.Interop/TypeKind.cs
+++ src/WinRT.Runtime2/Windows.UI.Xaml.Interop/TypeKind.cs
@@ -1,9 +1,8 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-using System.Runtime.Versioning;
-using Windows.Foundation;
-using Windows.Foundation.Metadata;
+using System;
+using System.ComponentModel;
 using WindowsRuntime;
 
 namespace Windows.UI.Xaml.Interop;
@@ -15,10 +14,11 @@ namespace Windows.UI.Xaml.Interop;
 /// This type is required for ABI projection of the <see cref="System.Type"/> class, but marshalling it is not supported.
 /// </remarks>
 /// <see href="https://learn.microsoft.com/uwp/api/windows.ui.xaml.interop.typekind"/>
-[WindowsRuntimeMetadata("Windows.Foundation.FoundationContract")]
-[SupportedOSPlatform("Windows10.0.10240.0")]
-[ContractVersion(typeof(FoundationContract), 65536u)]
-internal enum TypeKind
+[Obsolete(WindowsRuntimeConstants.PrivateImplementationDetailObsoleteMessage,
+    DiagnosticId = WindowsRuntimeConstants.PrivateImplementationDetailObsoleteDiagnosticId,
+    UrlFormat = WindowsRuntimeConstants.CsWinRTDiagnosticsUrlFormat)]
+[EditorBrowsable(EditorBrowsableState.Never)]
+public enum TypeKind
 {
     /// <summary>
     /// The type is a language-level primitive.
