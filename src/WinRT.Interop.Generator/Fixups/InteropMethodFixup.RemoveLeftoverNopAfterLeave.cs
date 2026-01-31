6ed688a880881eb788b61097e9504df60ec3f12f
Sergio Pedri <sergio0694@live.com>
2026-01-31 10:10:33 -0800

    Move label helpers and add early exit
    Move exception/branch label validation and label-redirection helpers from InteropMethodFixup.RemoveLeftoverNopAfterLeave.cs into InteropMethodFixup.cs, adding required usings and error references. Also add an early return in RemoveLeftoverNopAfterLeave to skip work when there are no exception handlers and remove an unused using. This centralizes label-related logic and avoids unnecessary processing when no handlers are present.

--- src/WinRT.Interop.Generator/Fixups/InteropMethodFixup.RemoveLeftoverNopAfterLeave.cs
+++ src/WinRT.Interop.Generator/Fixups/InteropMethodFixup.RemoveLeftoverNopAfterLeave.cs
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-using System.Collections.Generic;
 using AsmResolver.DotNet;
 using AsmResolver.DotNet.Code.Cil;
 using AsmResolver.PE.DotNet.Cil;
@@ -48,6 +47,12 @@ internal partial class InteropMethodFixup
 
             // Ignore empty methods (they're invalid and will fail on emit anyway)
             if (instructions.Count == 0)
+            {
+                return;
+            }
+
+            // If there are no exception handlers, we know we won't have any work to do
+            if (body.ExceptionHandlers.Count == 0)
             {
                 return;
             }
@@ -118,62 +123,6 @@ internal partial class InteropMethodFixup
         }
 
         /// <summary>
-        /// Validates the exception handler labels for the specified method.
-        /// </summary>
-        /// <param name="method">The method to validate.</param>
-        /// <param name="body">The method body to validate.</param>
-        private static void ValidateExceptionHandlerLabels(MethodDefinition method, CilMethodBody body)
-        {
-            // We only support handlers that use instruction labels. We never manually patch
-            // labels that use explicit offsets, as those haven't been computed at this point.
-            // Additionally, filters should never be used by any generated interop methods.
-            foreach (CilExceptionHandler handler in body.ExceptionHandlers)
-            {
-                if (handler is not
-                    {
-                        TryStart: CilInstructionLabel { Instruction: not null },
-                        TryEnd: CilInstructionLabel { Instruction: not null },
-                        HandlerStart: CilInstructionLabel { Instruction: not null },
-                        HandlerEnd: CilInstructionLabel { Instruction: not null },
-                        FilterStart: null
-                    })
-                {
-                    throw WellKnownInteropExceptions.MethodFixupInvalidExceptionHandlerLabels(method);
-                }
-            }
-        }
-
-        /// <summary>
-        /// Validates the exception handler labels for the specified method.
-        /// </summary>
-        /// <param name="method">The method to validate.</param>
-        /// <param name="body">The method body to validate.</param>
-        private static void ValidateBranchInstructionLabels(MethodDefinition method, CilMethodBody body)
-        {
-            foreach (CilInstruction instruction in body.Instructions)
-            {
-                // Make sure that the operand of all branch instruction is a valid instruction label.
-                // This is the same exact validation we also did above for all exception handlers.
-                if (instruction.Operand is ICilLabel and not CilInstructionLabel { Instruction: not null })
-                {
-                    throw WellKnownInteropExceptions.MethodFixupInvalidBranchInstructionLabels(method);
-                }
-
-                // Handle 'switch' instructions too (their operands are multiple branch targets)
-                if (instruction.Operand is IEnumerable<ICilLabel> labels)
-                {
-                    foreach (ICilLabel label in labels)
-                    {
-                        if (label is not CilInstructionLabel { Instruction: not null })
-                        {
-                            throw WellKnownInteropExceptions.MethodFixupInvalidBranchInstructionLabels(method);
-                        }
-                    }
-                }
-            }
-        }
-
-        /// <summary>
         /// Checks whether the specified instruction does not fall through to the next instruction.
         /// </summary>
         /// <param name="instruction">The instruction to check.</param>
@@ -237,62 +186,6 @@ internal partial class InteropMethodFixup
             int endIndex = body.Instructions.ReferenceIndexOf(end.Instruction!);
 
             return instructionIndex >= startIndex && instructionIndex < endIndex;
-        }
-
-        /// <summary>
-        /// Redirects all labels pointing to the old instruction to point to the new instruction instead.
-        /// </summary>
-        /// <param name="body">The method body to update.</param>
-        /// <param name="oldInstruction">The instruction that labels currently point to.</param>
-        /// <param name="newInstruction">The instruction that labels should point to after redirection.</param>
-        private static void RedirectLabels(CilMethodBody body, CilInstruction oldInstruction, CilInstruction newInstruction)
-        {
-            ICilLabel newLabel = newInstruction.CreateLabel();
-
-            // Update exception handler labels (they've been validated before already)
-            foreach (CilExceptionHandler handler in body.ExceptionHandlers)
-            {
-                if (((CilInstructionLabel)handler.TryStart!).Instruction == oldInstruction)
-                {
-                    handler.TryStart = newLabel;
-                }
-
-                if (((CilInstructionLabel)handler.TryEnd!).Instruction == oldInstruction)
-                {
-                    handler.TryEnd = newLabel;
-                }
-
-                if (((CilInstructionLabel)handler.HandlerStart!).Instruction == oldInstruction)
-                {
-                    handler.HandlerStart = newLabel;
-                }
-
-                if (((CilInstructionLabel)handler.HandlerEnd!).Instruction == oldInstruction)
-                {
-                    handler.HandlerEnd = newLabel;
-                }
-            }
-
-            // Update branch instruction operands
-            foreach (CilInstruction instruction in body.Instructions)
-            {
-                // Handle single branch target
-                if (instruction.Operand is CilInstructionLabel label && label.Instruction == oldInstruction)
-                {
-                    instruction.Operand = newLabel;
-                }
-                // Handle switch instruction (multiple branch targets)
-                else if (instruction.Operand is System.Collections.Generic.IList<ICilLabel> labels)
-                {
-                    for (int i = 0; i < labels.Count; i++)
-                    {
-                        if (labels[i] is CilInstructionLabel switchLabel && switchLabel.Instruction == oldInstruction)
-                        {
-                            labels[i] = newLabel;
-                        }
-                    }
-                }
-            }
         }
     }
 }
