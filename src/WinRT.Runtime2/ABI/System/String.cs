2ba946be7bf9b313a6a18491d71c2d80c4d322bf
Will Thant <kythant@microsoft.com>
2026-02-02 14:12:17 -0800

    WindowsRuntimeMetadataTypeMapGroup typos for Exception.cs and String.cs

--- src/WinRT.Runtime2/ABI/System/String.cs
+++ src/WinRT.Runtime2/ABI/System/String.cs
@@ -15,7 +15,7 @@ using static System.Runtime.InteropServices.ComWrappers;
 #pragma warning disable IDE0008, IDE1006, CA1416
 
 #pragma warning disable IL2026 // Members annotated with 'RequiresUnreferencedCodeAttribute' require dynamic access otherwise can break functionality when trimming application code
-[assembly: TypeMap<WindowsRuntimeComWrappersTypeMapGroup>(
+[assembly: TypeMap<WindowsRuntimeMetadataTypeMapGroup>(
     value: "String",
     target: typeof(ABI.System.String),
     trimTarget: typeof(string))]
