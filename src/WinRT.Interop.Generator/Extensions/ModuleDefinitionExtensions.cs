796b6a20ba18c710b030de66fb64e9bad426591b
Sergio Pedri <sergio0694@live.com>
2026-02-04 14:55:29 -0800

    Discover allocated non-generic types and SZ array types in 'cswinrtgen' (#2225)
    * Enumerate types created by newobj/newarr
    
    Add two extension methods to MethodDefinitionExtensions: EnumerateNewobjTypes and EnumerateNewarrElementTypes. Each method inspects the method's CIL instructions (safely checking for a CilMethodBody) and yields the instantiated object types for Newobj instructions and the element types for Newarr instructions respectively. Also add the required AsmResolver.PE.DotNet.Cil using. These helpers simplify discovering types allocated/instantiated in a method body.
    
    * Add GetAllMethods and improve type enumeration
    
    Introduce ModuleDefinition.GetAllMethods to enumerate methods (including nested types). Refactor type-signature discovery to resolve methods once, process locals from method.CilMethodBody.LocalVariables, and use method.EnumerateNewobjTypes / EnumerateNewarrElementTypes to gather object and array element types. Add a post-pass over all non-generic methods to ensure instantiated object/array types are discovered. Remove an unused AsmResolver.PE.DotNet.Cil using and generally reduce repeated Resolve calls for improved correctness and performance.
    
    * Fix typo in comment: signatures
    
    Corrects a misspelling in ModuleDefinitionExtensions.cs ("signastures" -> "signatures") and reflows surrounding comment lines for clarity. This is a non-functional comment-only cleanup to improve readability and maintainability.
    
    * Remove GetAllMethods; inline newobj/newarr
    
    Eliminate the separate GetAllMethods helper and remove the extra loop over all declared methods. Instead, newobj and newarr types are collected inline while enumerating methods/type specs, preventing duplicate scans and ensuring object and array element types discovered in method bodies are processed without a separate pass.

--- src/WinRT.Interop.Generator/Extensions/ModuleDefinitionExtensions.cs
+++ src/WinRT.Interop.Generator/Extensions/ModuleDefinitionExtensions.cs
@@ -8,7 +8,6 @@ using AsmResolver;
 using AsmResolver.DotNet;
 using AsmResolver.DotNet.Code.Cil;
 using AsmResolver.DotNet.Signatures;
-using AsmResolver.PE.DotNet.Cil;
 using AsmResolver.PE.DotNet.Metadata.Tables;
 using WindowsRuntime.InteropGenerator.Helpers;
 using WindowsRuntime.InteropGenerator.Visitors;
@@ -185,6 +184,30 @@ internal static class ModuleDefinitionExtensions
                     yield return result;
                 }
             }
+
+            // Look for all 'newobj' instructions and gather all object types
+            foreach (ITypeDefOrRef objectType in method.EnumerateNewobjTypes())
+            {
+                foreach (TResult result in EnumerateTypeSignatures(
+                    objectType.ToTypeSignature(),
+                    results,
+                    visitor))
+                {
+                    yield return result;
+                }
+            }
+
+            // Look for all 'newarr' instructions and gather all element types
+            foreach (ITypeDefOrRef elementType in method.EnumerateNewarrElementTypes())
+            {
+                foreach (TResult result in EnumerateTypeSignatures(
+                    elementType.MakeSzArrayType(),
+                    results,
+                    visitor))
+                {
+                    yield return result;
+                }
+            }
         }
 
         // Enumerate method specifications as well. These are used to detect generic instantiations of methods being invoked
@@ -220,8 +243,14 @@ internal static class ModuleDefinitionExtensions
                 }
             }
 
-            // And process locals as well
-            foreach (CilLocalVariable localVariable in specification.Method!.Resolve()?.CilMethodBody?.LocalVariables ?? [])
+            // Resolve the method definition to be able to process its body
+            if (specification.Method!.Resolve() is not MethodDefinition method)
+            {
+                continue;
+            }
+
+            // Process all declared locals as well
+            foreach (CilLocalVariable localVariable in method.CilMethodBody?.LocalVariables ?? [])
             {
                 foreach (TResult result in EnumerateTypeSignatures(
                     localVariable.VariableType.InstantiateGenericTypes(genericContext),
@@ -232,24 +261,9 @@ internal static class ModuleDefinitionExtensions
                 }
             }
 
-            IReadOnlyList<CilInstruction> instructions = specification.Method!.Resolve()?.CilMethodBody?.Instructions ?? (IReadOnlyList<CilInstruction>)[];
-
-            // Go through instruction to look for new objects
-            foreach (CilInstruction instruction in instructions)
+            // Look for all 'newobj' instructions and instantiate the object types
+            foreach (ITypeDefOrRef objectType in method.EnumerateNewobjTypes())
             {
-                // We only care for 'newobj' instructions
-                if (instruction.OpCode != CilOpCodes.Newobj)
-                {
-                    continue;
-                }
-
-                // Check that we can retrieve the target object type
-                if (instruction.Operand is not IMethodDefOrRef { DeclaringType: ITypeDefOrRef objectType })
-                {
-                    continue;
-                }
-
-                // Instantiate the object type and enumerate all signatures
                 foreach (TResult result in EnumerateTypeSignatures(
                     objectType.ToTypeSignature().InstantiateGenericTypes(genericContext),
                     results,
@@ -259,24 +273,11 @@ internal static class ModuleDefinitionExtensions
                 }
             }
 
-            // Go through instruction to look for new arrays
-            foreach (CilInstruction instruction in instructions)
+            // Look for all 'newarr' instructions and instantiate the element types
+            foreach (ITypeDefOrRef elementType in method.EnumerateNewarrElementTypes())
             {
-                // We only care for 'newarr' instructions
-                if (instruction.OpCode != CilOpCodes.Newarr)
-                {
-                    continue;
-                }
-
-                // Check that we can retrieve the target object type
-                if (instruction.Operand is not ITypeDefOrRef arrayType)
-                {
-                    continue;
-                }
-
-                // Instantiate the object type and enumerate all signatures
                 foreach (TResult result in EnumerateTypeSignatures(
-                    arrayType.ToTypeSignature().InstantiateGenericTypes(genericContext),
+                    elementType.MakeSzArrayType().InstantiateGenericTypes(genericContext),
                     results,
                     visitor))
                 {
