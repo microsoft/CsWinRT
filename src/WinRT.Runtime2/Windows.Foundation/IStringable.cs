7676674fa63230b61780d6a96fb424b39ec72ada
Sergio Pedri <sergio0694@live.com>
2026-02-04 18:14:05 -0800

    Add Windows.Foundation.IStringable interface
    Add a new IStringable interface to src/WinRT.Runtime2/Windows.Foundation to represent objects as strings for WinRT interop. The file includes XML documentation, a Microsoft MIT license header, and WinRT metadata attributes (WindowsRuntimeMetadata, Guid, ContractVersion). It also documents that managed types should override object.ToString rather than directly implementing IStringable.

---
+++ src/WinRT.Runtime2/Windows.Foundation/IStringable.cs
@@ -0,0 +1,29 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.Runtime.InteropServices;
+using Windows.Foundation.Metadata;
+using WindowsRuntime;
+
+namespace Windows.Foundation;
+
+/// <summary>
+/// Provides a way to represent the current object as a <see cref="string"/>.
+/// </summary>
+/// <remarks>
+/// Managed types should not implement the <see cref="IStringable"/> interface. Rather, they should override the
+/// <see cref="object.ToString"/> method. When exposed to native code, they will implicitly get an implementation
+/// of <see cref="IStringable"/> that will call the managed <see cref="object.ToString"/> override.
+/// </remarks>
+[WindowsRuntimeMetadata("Windows.Foundation.FoundationContract")]
+[Guid("96369F54-8EB6-48F0-ABCE-C1B211E627C3")]
+[ContractVersion(typeof(FoundationContract), 65536u)]
+public interface IStringable
+{
+    /// <summary>
+    /// Gets a <see cref="string"/> that represents the current object.
+    /// </summary>
+    /// <returns>The <see cref="string"/> representation of the current object.</returns>
+    string ToString();
+}
