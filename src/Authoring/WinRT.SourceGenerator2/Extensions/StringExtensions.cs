c6144b97e292c61fda29488b7e6929294c856ec0
Sergio Pedri <sergio0694@live.com>
2026-02-07 19:29:38 -0800

    Optimize identifier escaping and keyword handling
    Add pragma to suppress IDE0046 and refactor string-escaping logic to avoid allocating the interpolated-string handler for already-valid identifiers. When escaping, prefix a leading '_' if the first char can't start an identifier, replace invalid identifier chars with '_', and detect keywords to prefix an underscore so the result is a valid C# identifier.

--- src/Authoring/WinRT.SourceGenerator2/Extensions/StringExtensions.cs
+++ src/Authoring/WinRT.SourceGenerator2/Extensions/StringExtensions.cs
@@ -4,6 +4,8 @@
 using System.Runtime.CompilerServices;
 using Microsoft.CodeAnalysis.CSharp;
 
+#pragma warning disable IDE0046
+
 namespace WindowsRuntime.SourceGenerator;
 
 /// <summary>
@@ -27,22 +29,41 @@ internal static class StringExtensions
                 return "_";
             }
 
+            string escapedValue;
+
             // Optimization: we can just return the name as is if it's already a valid identifier. Generally speaking this
             // should always be the case for assembly names (except if they have one or more parts separated by a '.').
             if (SyntaxFacts.IsValidIdentifier(value))
             {
-                return value;
+                escapedValue = value;
             }
-
-            DefaultInterpolatedStringHandler handler = new(literalLength: value.Length, formattedCount: 0);
-
-            // Build the escaped name by just replacing all invalid characters with '_'
-            foreach (char c in value)
+            else
             {
-                handler.AppendFormatted(SyntaxFacts.IsIdentifierPartCharacter(c) ? c : '_');
+                DefaultInterpolatedStringHandler handler = new(literalLength: value.Length, formattedCount: 0);
+
+                // Add a leading '_' if the first character is not a valid start character for an identifier
+                if (!SyntaxFacts.IsIdentifierStartCharacter(value[0]))
+                {
+                    handler.AppendFormatted('_');
+                }
+
+                // Build the escaped name by just replacing all invalid characters with '_'
+                foreach (char c in value)
+                {
+                    handler.AppendFormatted(SyntaxFacts.IsIdentifierPartCharacter(c) ? c : '_');
+                }
+
+                escapedValue = handler.ToStringAndClear();
             }
 
-            return handler.ToStringAndClear();
+            // If the resulting identifier is still not valid (eg. a keyword like 'class'),
+            // adjust it so that the final result is actually a valid identifier we can use.
+            if (SyntaxFacts.GetKeywordKind(escapedValue) is not SyntaxKind.None)
+            {
+                return $"_{escapedValue}";
+            }
+
+            return escapedValue;
         }
     }
 }
