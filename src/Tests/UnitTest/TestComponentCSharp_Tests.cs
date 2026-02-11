ca632547a45f2df4253fbc629f35d755ba15681f
Will Thant <kythant@microsoft.com>
2026-02-11 15:24:02 -0800

    Update comment and rename variable

--- src/Tests/UnitTest/TestComponentCSharp_Tests.cs
+++ src/Tests/UnitTest/TestComponentCSharp_Tests.cs
@@ -97,8 +97,9 @@ namespace UnitTest
         public void TestDelegateCallBackOnSealedType()
         {
             // In CSWinRT 2.0, this scenario would throw an exception but this is fixed in 3.0
-            SealedDelegateClassTest a = new SealedDelegateClassTest();
-            StaticDelegateClassTest.Run(a.Run);
+            SealedDelegateClassTest testType = new SealedDelegateClassTest();
+            // Passing a projected function of SealedDelegateClassTest as a delegate
+            StaticDelegateClassTest.Run(testType.Run);
         }
 
         // Test a fix for a bug in Mono.Cecil that was affecting the IIDOptimizer when it encountered long class names 
