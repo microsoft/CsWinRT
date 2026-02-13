2298df043db777e0849e3dcc70f013eb8a76b835
Sergio Pedri <sergio0694@live.com>
2026-02-12 16:38:14 -0800

    Add IInputStream ABI marshaller and IID
    Introduce ABI support for Windows.Storage.Streams.IInputStream by adding a new IInputStream.cs containing marshalling helpers, vtable layout, managed->unmanaged and unmanaged->managed call paths, and a DynamicInterfaceCastable implementation. Also add the IInputStream IID to WellKnownWindowsInterfaceIIDs.g.cs and update the IID template (WellKnownWindowsInterfaceIIDs.tt) to include the new entry so the IID can be generated.

---
+++ src/WinRT.Runtime2/ABI/Windows.Storage.Streams/IInputStream.cs
@@ -0,0 +1,197 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Windows.Foundation;
+using Windows.Storage.Streams;
+using WindowsRuntime;
+using WindowsRuntime.InteropServices;
+using WindowsRuntime.InteropServices.Marshalling;
+using static System.Runtime.InteropServices.ComWrappers;
+
+#pragma warning disable CA2256, IDE0008, IDE1006
+
+#pragma warning disable IL2026 // Members annotated with 'RequiresUnreferencedCodeAttribute' require dynamic access otherwise can break functionality when trimming application code
+[assembly: TypeMap<WindowsRuntimeMetadataTypeMapGroup>(
+    value: "Windows.Storage.Streams.IInputStream",
+    target: typeof(IInputStream),
+    trimTarget: typeof(IInputStream))]
+#pragma warning restore IL2026 // Members annotated with 'RequiresUnreferencedCodeAttribute' require dynamic access otherwise can break functionality when trimming application code
+
+[assembly: TypeMapAssociation<DynamicInterfaceCastableImplementationTypeMapGroup>(
+    source: typeof(IInputStream),
+    proxy: typeof(ABI.Windows.Storage.Streams.IInputStreamInterfaceImpl))]
+
+namespace ABI.Windows.Storage.Streams;
+
+/// <summary>
+/// Marshaller for <see cref="IInputStream"/>.
+/// </summary>
+[Obsolete(WindowsRuntimeConstants.PrivateImplementationDetailObsoleteMessage,
+    DiagnosticId = WindowsRuntimeConstants.PrivateImplementationDetailObsoleteDiagnosticId,
+    UrlFormat = WindowsRuntimeConstants.CsWinRTDiagnosticsUrlFormat)]
+[EditorBrowsable(EditorBrowsableState.Never)]
+public static unsafe class IInputStreamMarshaller
+{
+    /// <inheritdoc cref="WindowsRuntimeObjectMarshaller.ConvertToUnmanaged"/>
+    public static WindowsRuntimeObjectReferenceValue ConvertToUnmanaged(IInputStream? value)
+    {
+        return WindowsRuntimeInterfaceMarshaller<IInputStream>.ConvertToUnmanaged(value, in WellKnownWindowsInterfaceIIDs.IID_IInputStream);
+    }
+
+    /// <inheritdoc cref="WindowsRuntimeDelegateMarshaller.ConvertToManaged"/>
+    public static IInputStream? ConvertToManaged(void* value)
+    {
+        return (IInputStream?)WindowsRuntimeObjectMarshaller.ConvertToManaged(value);
+    }
+}
+
+/// <summary>
+/// Interop methods for <see cref="IInputStream"/>.
+/// </summary>
+[Obsolete(WindowsRuntimeConstants.PrivateImplementationDetailObsoleteMessage,
+    DiagnosticId = WindowsRuntimeConstants.PrivateImplementationDetailObsoleteDiagnosticId,
+    UrlFormat = WindowsRuntimeConstants.CsWinRTDiagnosticsUrlFormat)]
+[EditorBrowsable(EditorBrowsableState.Never)]
+public static unsafe class IInputStreamMethods
+{
+    /// <see cref="IInputStream.ReadAsync"/>
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public static IAsyncOperationWithProgress<IBuffer, uint> ReadAsync(
+        WindowsRuntimeObjectReference thisReference,
+        IBuffer buffer,
+        uint count,
+        InputStreamOptions options)
+    {
+        using WindowsRuntimeObjectReferenceValue thisValue = thisReference.AsValue();
+        using WindowsRuntimeObjectReferenceValue bufferValue = IBufferMarshaller.ConvertToUnmanaged(buffer);
+
+        void* thisPtr = thisValue.GetThisPtrUnsafe();
+        void* result;
+
+        HRESULT hresult = ((IInputStreamVftbl*)*(void***)thisPtr)->ReadAsync(
+            thisPtr,
+            bufferValue.GetThisPtrUnsafe(),
+            count,
+            options,
+            &result);
+
+        RestrictedErrorInfo.ThrowExceptionForHR(hresult);
+
+        try
+        {
+            [UnsafeAccessor(UnsafeAccessorKind.StaticMethod)]
+            static extern IAsyncOperationWithProgress<IBuffer, uint>? ConvertToManaged(
+                [UnsafeAccessorType("ABI.Windows.Foundation.<#CsWinRT>IAsyncOperationWithProgress'2<<#CsWinRT>Windows.Storage.Streams.IBuffer|uint>Marshaller, WinRT.Interop")] object? _,
+                void* value);
+
+            return ConvertToManaged(null, result)!;
+        }
+        finally
+        {
+            WindowsRuntimeUnknownMarshaller.Free(result);
+        }
+    }
+}
+
+/// <summary>
+/// Binding type for <see cref="IInputStream"/>.
+/// </summary>
+[StructLayout(LayoutKind.Sequential)]
+internal unsafe struct IInputStreamVftbl
+{
+    public delegate* unmanaged[MemberFunction]<void*, Guid*, void**, HRESULT> QueryInterface;
+    public delegate* unmanaged[MemberFunction]<void*, uint> AddRef;
+    public delegate* unmanaged[MemberFunction]<void*, uint> Release;
+    public delegate* unmanaged[MemberFunction]<void*, uint*, Guid**, HRESULT> GetIids;
+    public delegate* unmanaged[MemberFunction]<void*, HSTRING*, HRESULT> GetRuntimeClassName;
+    public delegate* unmanaged[MemberFunction]<void*, TrustLevel*, HRESULT> GetTrustLevel;
+    public delegate* unmanaged[MemberFunction]<void*, void*, uint, InputStreamOptions, void**, HRESULT> ReadAsync;
+}
+
+/// <summary>
+/// The <see cref="IInputStream"/> implementation.
+/// </summary>
+[Obsolete(WindowsRuntimeConstants.PrivateImplementationDetailObsoleteMessage,
+    DiagnosticId = WindowsRuntimeConstants.PrivateImplementationDetailObsoleteDiagnosticId,
+    UrlFormat = WindowsRuntimeConstants.CsWinRTDiagnosticsUrlFormat)]
+[EditorBrowsable(EditorBrowsableState.Never)]
+public static unsafe class IInputStreamImpl
+{
+    /// <summary>
+    /// The <see cref="IInputStreamVftbl"/> value for the managed <see cref="IInputStream"/> implementation.
+    /// </summary>
+    [FixedAddressValueType]
+    private static readonly IInputStreamVftbl Vftbl;
+
+    /// <summary>
+    /// Initializes <see cref="Vftbl"/>.
+    /// </summary>
+    static IInputStreamImpl()
+    {
+        *(IInspectableVftbl*)Unsafe.AsPointer(ref Vftbl) = *(IInspectableVftbl*)IInspectableImpl.Vtable;
+
+        Vftbl.ReadAsync = &ReadAsync;
+    }
+
+    /// <summary>
+    /// Gets a pointer to the managed <see cref="IInputStream"/> implementation.
+    /// </summary>
+    public static nint Vtable
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get => (nint)Unsafe.AsPointer(in Vftbl);
+    }
+
+    /// <see href="https://learn.microsoft.com/uwp/api/windows.storage.streams.IInputStream.readasync"/>
+    [UnmanagedCallersOnly(CallConvs = [typeof(CallConvMemberFunction)])]
+    private static HRESULT ReadAsync(void* thisPtr, void* buffer, uint count, InputStreamOptions options, void** result)
+    {
+        if (buffer is null || result is null)
+        {
+            return WellKnownErrorCodes.E_POINTER;
+        }
+
+        try
+        {
+            var thisObject = ComInterfaceDispatch.GetInstance<IInputStream>((ComInterfaceDispatch*)thisPtr);
+
+            IAsyncOperationWithProgress<IBuffer, uint> operation = thisObject.ReadAsync(
+                buffer: IBufferMarshaller.ConvertToManaged(buffer)!,
+                count: count,
+                options: options);
+
+            [UnsafeAccessor(UnsafeAccessorKind.StaticMethod)]
+            static extern WindowsRuntimeObjectReferenceValue ConvertToUnmanaged(
+                [UnsafeAccessorType("ABI.Windows.Foundation.<#CsWinRT>IAsyncOperationWithProgress'2<<#CsWinRT>Windows.Storage.Streams.IBuffer|uint>Marshaller, WinRT.Interop")] object? _,
+                IAsyncOperationWithProgress<IBuffer, uint>? value);
+
+            *result = ConvertToUnmanaged(null, operation).DetachThisPtrUnsafe();
+
+            return WellKnownErrorCodes.S_OK;
+        }
+        catch (Exception e)
+        {
+            return RestrictedErrorInfoExceptionMarshaller.ConvertToUnmanaged(e);
+        }
+    }
+}
+
+/// <summary>
+/// The <see cref="IDynamicInterfaceCastable"/> implementation for <see cref="IInputStream"/>.
+/// </summary>
+[DynamicInterfaceCastableImplementation]
+[Guid("00000036-0000-0000-C000-000000000046")]
+file interface IInputStreamInterfaceImpl : IInputStream
+{
+    /// <inheritdoc/>
+    IAsyncOperationWithProgress<IBuffer, uint> IInputStream.ReadAsync(IBuffer buffer, uint count, InputStreamOptions options)
+    {
+        var thisReference = ((WindowsRuntimeObject)this).GetObjectReferenceForInterface(typeof(IInputStream).TypeHandle);
+
+        return IInputStreamMethods.ReadAsync(thisReference, buffer, count, options);
+    }
+}
