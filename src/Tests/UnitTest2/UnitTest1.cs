2834fbef5b087e4b3e2b4bb573bf8f1df886c9ee
Will Thant <kythant@microsoft.com>
2026-02-13 11:53:03 -0800

    It builds

---
+++ src/Tests/UnitTest2/UnitTest1.cs
@@ -0,0 +1,12 @@
+// file: UnitTest1.cs
+namespace TestProject1;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        Assert.AreEqual(3, 3);
+    }
+}
