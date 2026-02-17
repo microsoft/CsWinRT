7809d13a518d022d79f6ded64dea07cc1c7b2554
Will Thant <kythant@microsoft.com>
2026-02-17 12:52:48 -0800

    Revert GUID Test Unintended changes

--- src/Tests/UnitTest/GuidTests.cs
+++ src/Tests/UnitTest/GuidTests.cs
@@ -66,7 +66,57 @@ namespace UnitTest
             AssertGuid<IEnumerable>("036d2c08-df29-41af-8aa2-d774be62ba6f"); // IBindableIterable
             AssertGuid<IList>("393de7de-6fd0-4c0d-bb71-47244a113e93"); // IBindableVector
 
-            // ... rest unchanged ...
+            // Generated primitive GUIDs
+            AssertGuid<bool?>("3c00fd60-2950-5939-a21a-2d12c5a01b8a");
+            AssertGuid<sbyte?>("95500129-fbf6-5afc-89df-70642d741990");
+            AssertGuid<short?>("6ec9e41b-6709-5647-9918-a1270110fc4e");
+            AssertGuid<int?>("548cefbd-bc8a-5fa0-8df2-957440fc8bf4");
+            AssertGuid<long?>("4dda9e24-e69f-5c6a-a0a6-93427365af2a");
+            AssertGuid<byte?>("e5198cc8-2873-55f5-b0a1-84ff9e4aad62");
+            AssertGuid<ushort?>("5ab7d2c3-6b62-5e71-a4b6-2d49c4f238fd");
+            AssertGuid<uint?>("513ef3af-e784-5325-a91e-97c2b8111cf3");
+            AssertGuid<ulong?>("6755e376-53bb-568b-a11d-17239868309e");
+            AssertGuid<float?>("719cc2ba-3e76-5def-9f1a-38d85a145ea8");
+            AssertGuid<double?>("2f2d6c29-5473-5f3e-92e7-96572bb990e2");
+            AssertGuid<char?>("fb393ef3-bbac-5bd5-9144-84f23576f415");
+            AssertGuid<Guid?>("7d50f649-632c-51f9-849a-ee49428933ea");
+            AssertGuid<EventRegistrationToken?>("a9b18291-ce2a-5dae-8a23-b7f7388416db");
+            AssertGuid<TimeSpan?>("604d0c4c-91de-5c2a-935f-362f13eaf800");
+            AssertGuid<DateTimeOffset?>("5541d8a7-497c-5aa4-86fc-7713adbf2a2c");
+            AssertGuid<Point?>("84f14c22-a00a-5272-8d3d-82112e66df00");
+            AssertGuid<Rect?>("80423f11-054f-5eac-afd3-63b6ce15e77b");
+            AssertGuid<Size?>("61723086-8e53-5276-9f36-2a4bb93e2b75");
+            AssertGuid<Color?>("ab8e5d11-b0c1-5a21-95ae-f16bf3a37624");
+            AssertGuid<CornerRadius?>("13e1d34b-5909-579e-9df0-b0d002d32def");
+            AssertGuid<Duration?>("031db157-bb1c-5f3b-9bd1-86b518e41838");
+            AssertGuid<GridLength?>("42caaa58-4cc0-51c0-8bb5-e03d1840af39");
+            AssertGuid<Thickness?>("62390189-15ec-5fd5-86d5-0d5321feb589");
+            AssertGuid<GeneratorPosition?>("9af7b86e-f961-5729-8bc3-8a049a324d78");
+            AssertGuid<Matrix?>("9633fa73-15ed-5161-b49f-5303116e3417");
+            AssertGuid<KeyTime?>("3272a278-58ab-5647-8b95-77ad17795919");
+            AssertGuid<RepeatBehavior?>("ac730575-6aa4-5827-8ce5-c3298584e7cf");
+            AssertGuid<Matrix3D?>("f1ba40c4-c05d-5258-8866-134054cb5c05");
+            AssertGuid<Matrix3x2?>("76358cfd-2cbd-525b-a49e-90ee18247b71");
+            AssertGuid<Matrix4x4?>("dacbffdc-68ef-5fd0-b657-782d0ac9807e");
+            AssertGuid<Plane?>("46d542a1-52f7-58e7-acfc-9a6d364da022");
+            AssertGuid<Quaternion?>("b27004bb-c014-5dce-9a21-799c5a3c1461");
+            AssertGuid<Vector2?>("48f6a69e-8465-57ae-9400-9764087f65ad");
+            AssertGuid<Vector3?>("1ee770ff-c954-59ca-a754-6199a9be282c");
+            AssertGuid<Vector4?>("a5e843c9-ed20-5339-8f8d-9fe404cf3654");
+
+            // Enums, structs, IInspectable, classes, and delegates
+            AssertGuid<PropertyType?>("ecebde54-fac0-5aeb-9ba9-9e1fe17e31d5");
+            AssertGuid<Point?>("84f14c22-a00a-5272-8d3d-82112e66df00");
+            AssertGuid<IList<object>>("b32bdca4-5e52-5b27-bc5d-d66a1a268c2a");
+            AssertGuid<IList<Uri>>("0d82bd8d-fe62-5d67-a7b9-7886dd75bc4e");
+            AssertGuid<IList<AsyncActionCompletedHandler>>("5dafe591-86dc-59aa-bfda-07f5d59fc708");
+            AssertGuid<IList<ComposedNonBlittableStruct>>("c8477314-b257-511b-a3a1-9e4eb6385152");
+            AssertGuid<IList<IDisposable>>("1bfca4f6-2c4e-5174-9869-b39d35848fcc");
+            AssertGuid<IList<IWwwFormUrlDecoderEntry>>("2f5fb6d3-231f-57a1-9f2a-daa7e43bf075");
+            AssertGuid<IList<WwwFormUrlDecoderEntry>>("1d9ba3f5-b997-5a7d-82c4-7857ecbf3a42");
+            AssertGuid<IList<Deferral>>("a3c9b753-57ad-537f-9626-4ae5785473d4");
+            AssertGuid<IList<DateTimeOffset>>("94390dc5-e442-5870-88b6-007e232f902c");
+            AssertGuid<IList<Point>>("c0d513a9-ec4a-5a5d-b6d5-b707defdb9f7");
         }
         */
     }
