15260a722b5fbe65fc873ea8a23bdc0c63f5a930
Sergio Pedri <sergio0694@live.com>
2026-02-18 11:00:46 -0800

    Validate ABI namespace for exclusiveto impl names
    Require component implementation types to be in an 'ABI.' namespace in addition to ending with 'Impl' when mapping '[exclusiveto]' interfaces to IIDs. Rename the warning helper to ComponentTypeExclusiveToInterfaceInvalidFullNameError and update its message/code to reflect an invalid full name. Remove an unused exception helper for a missing WinRT.Component.dll reference.

--- src/WinRT.Interop.Generator/Discovery/InteropTypeDiscovery.cs
+++ src/WinRT.Interop.Generator/Discovery/InteropTypeDiscovery.cs
@@ -533,9 +533,9 @@ internal static partial class InteropTypeDiscovery
 
                 // We use a naming convention to map each generated '[exclusiveto]' interface to its IID, so we need to validate
                 // that the name for this type is in fact the one we expect, before actually tracking it for code generation.
-                if (!componentType.Name!.AsSpan().EndsWith("Impl"u8))
+                if (!componentType.Namespace!.AsSpan().StartsWith("ABI."u8) || !componentType.Name!.AsSpan().EndsWith("Impl"u8))
                 {
-                    WellKnownInteropExceptions.ComponentTypeExclusiveToInterfaceInvalidNameError(componentType, typeDefinition).LogOrThrow(args.TreatWarningsAsErrors);
+                    WellKnownInteropExceptions.ComponentTypeExclusiveToInterfaceInvalidFullNameError(componentType, typeDefinition).LogOrThrow(args.TreatWarningsAsErrors);
                 }
 
                 // Try to track the current implementation type. Note that this is not actually an interface (since we don't
