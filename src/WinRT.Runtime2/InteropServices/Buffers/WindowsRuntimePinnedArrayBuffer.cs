5417a748eeb9ddf4edc4db6dea967d4db399b2f6
Sergio Pedri <sergio0694@live.com>
2026-02-18 22:41:17 -0800

    Extract WindowsRuntimePinnedArrayBuffer to file
    Move the WindowsRuntimePinnedArrayBuffer implementation out of WindowsRuntimeBuffer.cs into a new InteropServices/Buffers/WindowsRuntimePinnedArrayBuffer.cs file. This removes the duplicate class definition from WindowsRuntimeBuffer.cs, cleans up unused using directives, and updates XML docs to reference the IBuffer members explicitly. Improves code organization and separates buffer interop concerns from the factory helpers.

---
+++ src/WinRT.Runtime2/InteropServices/Buffers/WindowsRuntimePinnedArrayBuffer.cs
@@ -0,0 +1,117 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Windows.Storage.Streams;
+
+namespace WindowsRuntime.InteropServices;
+
+/// <summary>
+/// Provides a managed implementation of the <see cref="IBuffer"/> interface backed by a pinned array.
+/// </summary>
+internal sealed class WindowsRuntimePinnedArrayBuffer : IBuffer
+{
+    /// <summary>
+    /// The pinned array to use.
+    /// </summary>
+    private readonly byte[] _pinnedData;
+
+    /// <summary>
+    /// The offset in <see cref="_pinnedData"/>.
+    /// </summary>
+    private readonly int _offset;
+
+    /// <summary>
+    /// The number of bytes that can be read or written in <see cref="_pinnedData"/>.
+    /// </summary>
+    private int _length;
+
+    /// <summary>
+    /// The capacity of the buffer.
+    /// </summary>
+    private readonly int _capacity;
+
+    /// <summary>
+    /// Creates a <see cref="WindowsRuntimePinnedArrayBuffer"/> instance with the specified parameters.
+    /// </summary>
+    /// <param name="capacity">The maximum number of bytes the buffer can hold.</param>
+    /// <remarks>This constructor doesn't validate any of its parameters.</remarks>
+    public WindowsRuntimePinnedArrayBuffer(int capacity)
+    {
+        Debug.Assert(capacity >= 0);
+
+        _pinnedData = GC.AllocateArray<byte>(capacity, pinned: true);
+        _offset = 0;
+        _length = 0;
+        _capacity = capacity;
+    }
+
+    /// <summary>
+    /// Creates a <see cref="WindowsRuntimePinnedArrayBuffer"/> instance with the specified parameters.
+    /// </summary>
+    /// <param name="pinnedData">The pinned array to wrap.</param>
+    /// <param name="offset">The offset in <paramref name="pinnedData"/>.</param>
+    /// <param name="length">The number of bytes.</param>
+    /// <param name="capacity">The maximum number of bytes the buffer can hold.</param>
+    /// <remarks>This constructor doesn't validate any of its parameters.</remarks>
+    public WindowsRuntimePinnedArrayBuffer(byte[] pinnedData, int offset, int length, int capacity)
+    {
+        Debug.Assert(pinnedData is not null);
+        Debug.Assert(offset >= 0);
+        Debug.Assert(length >= 0);
+        Debug.Assert(capacity >= 0);
+        Debug.Assert(pinnedData.Length - offset >= length);
+        Debug.Assert(pinnedData.Length - offset >= capacity);
+        Debug.Assert(capacity >= length);
+
+        _pinnedData = pinnedData;
+        _offset = offset;
+        _length = length;
+        _capacity = capacity;
+    }
+
+    /// <inheritdoc/>
+    public uint Capacity
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get => (uint)_capacity;
+    }
+
+    /// <inheritdoc/>
+    public uint Length
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get => (uint)_length;
+        set
+        {
+            if (value > Capacity)
+            {
+                ArgumentOutOfRangeException ex = new(nameof(value), global::Windows.Foundation.SR.Argument_BufferLengthExceedsCapacity)
+                {
+                    HResult = WellKnownErrorCodes.E_BOUNDS
+                };
+
+                throw ex;
+            }
+
+            // Capacity is ensured to not exceed Int32.MaxValue, so Length is within this limit and this cast is safe:
+            Debug.Assert(Capacity <= int.MaxValue);
+            _length = unchecked((int)value);
+        }
+    }
+
+    internal unsafe byte* Buffer
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get => &((byte*)Unsafe.AsPointer(ref MemoryMarshal.GetArrayDataReference(_pinnedData)))[(uint)_offset];
+    }
+
+    internal void GetUnderlyingData(out byte[] underlyingDataArray, out int underlyingDataArrayStartOffset)
+    {
+        underlyingDataArray = _pinnedData;
+        underlyingDataArrayStartOffset = _offset;
+    }
+}
--- src/WinRT.Runtime2/Windows.Storage.Streams/Buffers/WindowsRuntimeBuffer.cs
+++ src/WinRT.Runtime2/Windows.Storage.Streams/Buffers/WindowsRuntimeBuffer.cs
@@ -2,9 +2,6 @@
 // Licensed under the MIT License.
 
 using System;
-using System.Diagnostics;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 using Windows.Storage.Streams;
 using WindowsRuntime.InteropServices;
 
@@ -19,7 +16,7 @@ public static class WindowsRuntimeBuffer
     /// Creates an empty <see cref="IBuffer"/> instance with a given maximum capacity, and its contents set to zero.
     /// </summary>
     /// <param name="capacity">The maximum number of bytes the buffer can hold.</param>
-    /// <returns>The resulting <see cref="IBuffer"/> instance (with the specified capacity and <see cref="Length"/> equal to <c>0</c>).</returns>
+    /// <returns>The resulting <see cref="IBuffer"/> instance (with the specified capacity and <see cref="IBuffer.Length"/> equal to <c>0</c>).</returns>
     /// <exception cref="ArgumentOutOfRangeException">Thrown if <paramref name="capacity"/> is less than <c>0</c>.</exception>
     public static IBuffer Create(int capacity)
     {
@@ -32,7 +29,7 @@ public static class WindowsRuntimeBuffer
     /// Creates an <see cref="IBuffer"/> instance that contains a specified range of bytes copied from an input <see cref="ReadOnlySpan{T}"/> value.
     /// </summary>
     /// <param name="data">The <see cref="ReadOnlySpan{T}"/> value to copy from.</param>
-    /// <returns>The resulting <see cref="IBuffer"/> instance (with <see cref="Capacity"/> and <see cref="Length"/> equal to the length of <paramref name="data"/>).</returns>
+    /// <returns>The resulting <see cref="IBuffer"/> instance (with <see cref="IBuffer.Capacity"/> and <see cref="IBuffer.Length"/> equal to the length of <paramref name="data"/>).</returns>
     public static IBuffer Create(ReadOnlySpan<byte> data)
     {
         byte[] pinnedData = GC.AllocateArray<byte>(data.Length, pinned: true);
@@ -95,112 +92,5 @@ public static class WindowsRuntimeBuffer
             length: length);
 
         return new WindowsRuntimePinnedArrayBuffer(pinnedData, offset: 0, length, capacity);
-    }
-}
-
-/// <summary>
-/// Provides a managed implementation of the <see cref="IBuffer"/> interface backed by a pinned array.
-/// </summary>
-internal sealed class WindowsRuntimePinnedArrayBuffer : IBuffer
-{
-    /// <summary>
-    /// The pinned array to use.
-    /// </summary>
-    private readonly byte[] _pinnedData;
-
-    /// <summary>
-    /// The offset in <see cref="_pinnedData"/>.
-    /// </summary>
-    private readonly int _offset;
-
-    /// <summary>
-    /// The number of bytes that can be read or written in <see cref="_pinnedData"/>.
-    /// </summary>
-    private int _length;
-
-    /// <summary>
-    /// The capacity of the buffer.
-    /// </summary>
-    private readonly int _capacity;
-
-    /// <summary>
-    /// Creates a <see cref="WindowsRuntimePinnedArrayBuffer"/> instance with the specified parameters.
-    /// </summary>
-    /// <param name="capacity">The maximum number of bytes the buffer can hold.</param>
-    /// <remarks>This constructor doesn't validate any of its parameters.</remarks>
-    public WindowsRuntimePinnedArrayBuffer(int capacity)
-    {
-        Debug.Assert(capacity >= 0);
-
-        _pinnedData = GC.AllocateArray<byte>(capacity, pinned: true);
-        _offset = 0;
-        _length = 0;
-        _capacity = capacity;
-    }
-
-    /// <summary>
-    /// Creates a <see cref="WindowsRuntimePinnedArrayBuffer"/> instance with the specified parameters.
-    /// </summary>
-    /// <param name="pinnedData">The pinned array to wrap.</param>
-    /// <param name="offset">The offset in <paramref name="pinnedData"/>.</param>
-    /// <param name="length">The number of bytes.</param>
-    /// <param name="capacity">The maximum number of bytes the buffer can hold.</param>
-    /// <remarks>This constructor doesn't validate any of its parameters.</remarks>
-    public WindowsRuntimePinnedArrayBuffer(byte[] pinnedData, int offset, int length, int capacity)
-    {
-        Debug.Assert(pinnedData is not null);
-        Debug.Assert(offset >= 0);
-        Debug.Assert(length >= 0);
-        Debug.Assert(capacity >= 0);
-        Debug.Assert(pinnedData.Length - offset >= length);
-        Debug.Assert(pinnedData.Length - offset >= capacity);
-        Debug.Assert(capacity >= length);
-
-        _pinnedData = pinnedData;
-        _offset = offset;
-        _length = length;
-        _capacity = capacity;
-    }
-
-    /// <inheritdoc/>
-    public uint Capacity
-    {
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        get => (uint)_capacity;
-    }
-
-    /// <inheritdoc/>
-    public uint Length
-    {
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        get => (uint)_length;
-        set
-        {
-            if (value > Capacity)
-            {
-                ArgumentOutOfRangeException ex = new(nameof(value), global::Windows.Foundation.SR.Argument_BufferLengthExceedsCapacity)
-                {
-                    HResult = WellKnownErrorCodes.E_BOUNDS
-                };
-
-                throw ex;
-            }
-
-            // Capacity is ensured to not exceed Int32.MaxValue, so Length is within this limit and this cast is safe:
-            Debug.Assert(Capacity <= int.MaxValue);
-            _length = unchecked((int)value);
-        }
-    }
-
-    internal unsafe byte* Buffer
-    {
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        get => &((byte*)Unsafe.AsPointer(ref MemoryMarshal.GetArrayDataReference(_pinnedData)))[(uint)_offset];
-    }
-
-    internal void GetUnderlyingData(out byte[] underlyingDataArray, out int underlyingDataArrayStartOffset)
-    {
-        underlyingDataArray = _pinnedData;
-        underlyingDataArrayStartOffset = _offset;
     }
 }
