519cdfe09b6d2f650972a4f56f2f10e855a70ba8
Sergio Pedri <sergio0694@live.com>
2026-02-18 22:56:40 -0800

    Add IBufferByteAccessVftbl interop vtable
    Introduce an internal unsafe struct IBufferByteAccessVftbl that binds the IBufferByteAccess COM vtable for WinRT interop. Defines unmanaged MemberFunction pointers for QueryInterface, AddRef, Release and get_Buffer, and provides an AggressiveInlining helper get_BufferUnsafe. Marked with StructLayout.Sequential and includes doc comments and Microsoft MIT license header.

---
+++ src/WinRT.Runtime2/InteropServices/Vtables/IBufferByteAccessVftbl.cs
@@ -0,0 +1,37 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+#pragma warning disable IDE1006
+
+namespace WindowsRuntime.InteropServices;
+
+/// <summary>
+/// Binding type for the <c>IBufferByteAccess</c> interface vtable.
+/// </summary>
+/// <see href="https://learn.microsoft.com/windows/win32/api/robuffer/ns-robuffer-ibufferbyteaccess"/>
+[StructLayout(LayoutKind.Sequential)]
+internal unsafe struct IBufferByteAccessVftbl
+{
+    public delegate* unmanaged[MemberFunction]<void*, Guid*, void**, int> QueryInterface;
+    public delegate* unmanaged[MemberFunction]<void*, uint> AddRef;
+    public delegate* unmanaged[MemberFunction]<void*, uint> Release;
+    public delegate* unmanaged[MemberFunction]<void*, byte**, int> get_Buffer;
+
+    /// <summary>
+    /// Gets the array of bytes in the buffer.
+    /// </summary>
+    /// <param name="thisPtr">The target COM object.</param>
+    /// <param name="result">The byte array.</param>
+    /// <returns>If this method succeeds, it returns <c>S_OK</c>. Otherwise, it returns an <c>HRESULT</c> error code.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static int get_BufferUnsafe(
+        void* thisPtr,
+        byte** result)
+    {
+        return ((IBufferByteAccessVftbl*)*(void***)thisPtr)->get_Buffer(thisPtr, result);
+    }
+}
