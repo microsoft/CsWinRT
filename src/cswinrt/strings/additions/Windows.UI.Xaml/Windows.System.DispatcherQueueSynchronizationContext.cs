587da4f05f6a10622e375f78ee0e55cafef45867
Sergio Pedri <sergio0694@live.com>
2026-02-21 13:20:19 -0800

    Convert namespace to block form for sync context
    Replace file-scoped namespaces with block-style namespaces and move the DispatcherQueueSynchronizationContext class declaration inside the namespace braces. Reorganize XML doc comments and member ordering for readability; implementation and behavior are unchanged. Changes applied to both Microsoft.UI.Dispatching and Windows.System variants.

--- src/cswinrt/strings/additions/Windows.UI.Xaml/Windows.System.DispatcherQueueSynchronizationContext.cs
+++ src/cswinrt/strings/additions/Windows.UI.Xaml/Windows.System.DispatcherQueueSynchronizationContext.cs
@@ -1,53 +1,54 @@
 #nullable enable
 
-namespace Windows.System;
-
-/// <summary>
-/// The <see cref="DispatcherQueueSynchronizationContext"/> type allows developers to await calls and get back onto
-/// the UI thread. Needs to be installed on the UI thread through <see cref="SynchronizationContext.SetSynchronizationContext"/>.
-/// </summary>
-public sealed class DispatcherQueueSynchronizationContext : global::System.Threading.SynchronizationContext
+namespace Windows.System
 {
     /// <summary>
-    /// The <see cref="WindowsRuntime.InteropServices.DispatcherQueueSynchronizationContext"/> instance to use.
+    /// The <see cref="DispatcherQueueSynchronizationContext"/> type allows developers to await calls and get back onto
+    /// the UI thread. Needs to be installed on the UI thread through <see cref="SynchronizationContext.SetSynchronizationContext"/>.
     /// </summary>
-    private readonly WindowsRuntime.InteropServices.DispatcherQueueSynchronizationContext _innerContext;
-
-    /// <summary>
-    /// Creates a new <see cref="DispatcherQueueSynchronizationContext"/> instance with the specified parameters.
-    /// </summary>
-    /// <param name="dispatcherQueue">The target <see cref="global::Windows.System.DispatcherQueue"/> instance.</param>
-    /// <exception cref="ArgumentNullException">Thrown if <paramref name="dispatcherQueue"/> is <see langword="null"/>.</exception>
-    public DispatcherQueueSynchronizationContext(global::Windows.System.DispatcherQueue dispatcherQueue)
+    public sealed class DispatcherQueueSynchronizationContext : global::System.Threading.SynchronizationContext
     {
-        _innerContext = new WindowsRuntime.InteropServices.DispatcherQueueSynchronizationContext(dispatcherQueue);
-    }
+        /// <summary>
+        /// The <see cref="WindowsRuntime.InteropServices.DispatcherQueueSynchronizationContext"/> instance to use.
+        /// </summary>
+        private readonly WindowsRuntime.InteropServices.DispatcherQueueSynchronizationContext _innerContext;
 
-    /// <summary>
-    /// Creates a new <see cref="DispatcherQueueSynchronizationContext"/> instance with the specified parameters.
-    /// </summary>
-    /// <param name="innerContext">The <see cref="WindowsRuntime.InteropServices.DispatcherQueueSynchronizationContext"/> instance for the target dispatcher queue.</param>
-    private DispatcherQueueSynchronizationContext(WindowsRuntime.InteropServices.DispatcherQueueSynchronizationContext innerContext)
-    {
-        _innerContext = innerContext;
-    }
+        /// <summary>
+        /// Creates a new <see cref="DispatcherQueueSynchronizationContext"/> instance with the specified parameters.
+        /// </summary>
+        /// <param name="dispatcherQueue">The target <see cref="global::Windows.System.DispatcherQueue"/> instance.</param>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="dispatcherQueue"/> is <see langword="null"/>.</exception>
+        public DispatcherQueueSynchronizationContext(global::Windows.System.DispatcherQueue dispatcherQueue)
+        {
+            _innerContext = new WindowsRuntime.InteropServices.DispatcherQueueSynchronizationContext(dispatcherQueue);
+        }
 
-    /// <inheritdoc/>
-    public override void Post(global::System.Threading.SendOrPostCallback d, object? state)
-    {
-        _innerContext.Post(d, state);
-    }
+        /// <summary>
+        /// Creates a new <see cref="DispatcherQueueSynchronizationContext"/> instance with the specified parameters.
+        /// </summary>
+        /// <param name="innerContext">The <see cref="WindowsRuntime.InteropServices.DispatcherQueueSynchronizationContext"/> instance for the target dispatcher queue.</param>
+        private DispatcherQueueSynchronizationContext(WindowsRuntime.InteropServices.DispatcherQueueSynchronizationContext innerContext)
+        {
+            _innerContext = innerContext;
+        }
 
-    /// <inheritdoc/>
-    public override void Send(global::System.Threading.SendOrPostCallback d, object? state)
-    {
-        _innerContext._Send(d, state);
-    }
+        /// <inheritdoc/>
+        public override void Post(global::System.Threading.SendOrPostCallback d, object? state)
+        {
+            _innerContext.Post(d, state);
+        }
 
-    /// <inheritdoc/>
-    public override global::System.Threading.SynchronizationContext CreateCopy()
-    {
-        return new DispatcherQueueSynchronizationContext(_innerContext);
+        /// <inheritdoc/>
+        public override void Send(global::System.Threading.SendOrPostCallback d, object? state)
+        {
+            _innerContext._Send(d, state);
+        }
+
+        /// <inheritdoc/>
+        public override global::System.Threading.SynchronizationContext CreateCopy()
+        {
+            return new DispatcherQueueSynchronizationContext(_innerContext);
+        }
     }
 }
 
