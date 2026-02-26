28706d2fdd96908ee461a8b9519001787e45b030
Sergio Pedri <sergio0694@live.com>
2026-02-25 16:49:59 -0800

    Remove unused usings in buffer extensions
    Remove unused using directives System.Runtime.CompilerServices and System.Runtime.InteropServices from WindowsRuntimeBufferExtensions.cs to clean up imports. No functional changes; reduces compiler warnings for unused namespaces.

--- src/WinRT.Runtime2/Windows.Storage/Buffers/WindowsRuntimeBufferExtensions.cs
+++ src/WinRT.Runtime2/Windows.Storage/Buffers/WindowsRuntimeBufferExtensions.cs
@@ -5,8 +5,6 @@ using System;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 using Windows.Storage.Streams;
 using WindowsRuntime;
 using WindowsRuntime.InteropServices;
