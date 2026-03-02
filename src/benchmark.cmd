5b9f7f9e1abe203ad2da749f9643dc01041239d2
Manodasan Wignarajah <mawign@microsoft.com>
2026-03-01 22:49:37 -0800

    restore

--- src/benchmark.cmd
+++ src/benchmark.cmd
@@ -1,5 +1,5 @@
 nuget restore TestWinRT\Test.sln
-msbuild -t:restore -t:Benchmarks /p:platform=x64 /p:configuration=release /p:solutiondir=%~dp0 /p:IsDotnetBuild=false %this_dir%cswinrt.slnx
+msbuild -t:restore -t:Benchmarks /p:RestorePackagesConfig=true /p:platform=x64 /p:configuration=release /p:solutiondir=%~dp0 /p:IsDotnetBuild=false %this_dir%cswinrt.slnx
 dotnet %~dp0Benchmarks\bin\x64\Release\net10.0\Benchmarks.dll -filter * --runtimes net10.0
 nuget restore Perf\ResultsComparer\ResultsComparer.sln
 msbuild Perf\ResultsComparer\ResultsComparer.sln -t:restore -t:build /p:platform="Any CPU" /p:configuration=release
