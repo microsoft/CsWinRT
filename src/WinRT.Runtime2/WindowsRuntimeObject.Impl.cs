681851d4a9c345884b58ffef7bb198040b6c2490
Sergio Pedri <sergio0694@live.com>
2026-07-22 10:23:17 -0700

    Pass iid to ActivateInstanceUnsafe (fix from #2476)
    Forward the 'iid' parameter into WindowsRuntimeActivationHelper.ActivateInstanceUnsafe. This ensures the activation helper receives the requested interface IID (used for composable/activation scenarios) instead of omitting it, fixing incorrect/default behavior when selecting the default interface during activation.

--- src/WinRT.Runtime2/WindowsRuntimeObject.Impl.cs
+++ src/WinRT.Runtime2/WindowsRuntimeObject.Impl.cs
@@ -77,6 +77,7 @@ public unsafe partial class WindowsRuntimeObject
         // See additional notes in the overload below for more details about how and when that parameter is necessary.
         WindowsRuntimeActivationHelper.ActivateInstanceUnsafe(
             activationFactoryObjectReference: activationFactoryObjectReference,
+            iid: in iid,
             defaultInterface: out void* defaultInterface);
 
         // The inner interface pointer isn't used for non-composable types, so we just pass 'null'
