8a300d29e92bddd3af2f6c7ec21cb07ac5afa339
Sergio Pedri <sergio0694@live.com>
2026-07-28 09:41:29 -0700

    Update license header in GenerateCsWinRTStubExes.cs

--- src/WinRT.Generator.Tasks/GenerateCsWinRTStubExes.cs
+++ src/WinRT.Generator.Tasks/GenerateCsWinRTStubExes.cs
@@ -1,5 +1,5 @@
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
 
 using System.Diagnostics;
 using System.Text;
